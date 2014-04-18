86c25325a82bb828154f5744f14979f79303d9aa
Adam Brill <adambrill@gmail.com>
2014-04-18 15:42:01 -0500

    Fixed the Grid Promos module to work properly.

--- Website/layouts/Modules/Grid Promos.ascx.cs
+++ Website/layouts/Modules/Grid Promos.ascx.cs
@@ -10,42 +10,47 @@ namespace Website.layouts.Modules
 {
     public partial class GridPromos : BaseModule
     {
-        protected List<object> PromoItems
-        {
-            get
-            {
-                var ret = new List<object>();
-
-                if (!string.IsNullOrEmpty(CurrentContextItem.Fields["Promo One Header"].Value))
-                {
-                    ret.Add(new
-                    {
-                        Image = FieldRenderer.Render(CurrentContextItem, "Promo One Image"),
-                        Header = FieldRenderer.Render(CurrentContextItem, "Promo One Header"),
-                        Content = FieldRenderer.Render(CurrentContextItem, "Promo One Content"),
-                        Button = FieldRenderer.Render(CurrentContextItem, "Promo One Button"),
-                        Accent = FieldRenderer.Render(CurrentContextItem, "Promo One Accent"),
-                    });
-                }
-
-                if (!string.IsNullOrEmpty(CurrentContextItem.Fields["Promo Two Header"].Value))
-                {
-                    ret.Add(new
-                    {
-                        Image = FieldRenderer.Render(CurrentContextItem, "Promo One Image"),
-                        Header = FieldRenderer.Render(CurrentContextItem, "Promo One Header"),
-                        Content = FieldRenderer.Render(CurrentContextItem, "Promo One Content"),
-                        Button = FieldRenderer.Render(CurrentContextItem, "Promo One Button"),
-                        Accent = FieldRenderer.Render(CurrentContextItem, "Promo One Accent"),
-                    });
-                }
-
-                return ret;
-            }
-        }
         protected void Page_Load(object sender, EventArgs e)
         {
+            var promoItems = new List<object>();
+            var colors = Sitecore.Context.Database.GetItem("/sitecore/content/Data/Taxonomy/Modules/Colors").Children;
 
+            foreach (var num in new string[] { "Promo One", "Promo Two", "Promo Three", "Promo Four" })
+            {
+                if (!string.IsNullOrEmpty(CurrentContextItem.Fields[num + " Header"].Value))
+                {
+
+                    var classes = new List<string>();
+
+                    var accentColorField = CurrentContextItem.Fields[num + " Accent"];
+
+                    if (colors.Any(c => c.Name == accentColorField.Value))
+                    {
+                        classes.Add("accent-" + colors.First(c => c.Name == accentColorField.Value).Fields["CSS Class"].Value);
+                    }
+
+                    promoItems.Add(new
+                    {
+                        Image = FieldRenderer.Render(CurrentContextItem, num + " Image"),
+                        Header = FieldRenderer.Render(CurrentContextItem, num + " Header"),
+                        Content = FieldRenderer.Render(CurrentContextItem, num + " Content"),
+                        Button = FieldRenderer.Render(CurrentContextItem, num + " Button"),
+                        CssClasses = string.Join(" ", classes),
+                    });
+                }
+            }
+
+            if (promoItems.Count == 3)
+            {
+                divGrid.Attributes["class"] += " three";
+            }
+            else if (promoItems.Count == 4)
+            {
+                divGrid.Attributes["class"] += " four";
+            }
+            
+            rptPromoItems.DataSource = promoItems;
+            rptPromoItems.DataBind();
         }
     }
 }
