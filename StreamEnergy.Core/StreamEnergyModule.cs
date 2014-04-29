41f6aa72cf1bd29e2b54be98c033c02ba6da8db1
Matt DeKrey <mattdekrey@gmail.com>
2014-04-29 12:11:56 -0400

    Create a StreamEnergyModule that sets up Unity with the MVC framework. Also, added in a few testing classes/files for Sitecore MVC testing, but didn't get far.

---
+++ StreamEnergy.Core/StreamEnergyModule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace StreamEnergy
+{
+    class StreamEnergyModule : IHttpModule
+    {
+        static StreamEnergyModule instance = null;
+
+        void IHttpModule.Dispose()
+        {
+            if (instance == this)
+            {
+                instance = null;
+            }
+        }
+
+        void IHttpModule.Init(HttpApplication context)
+        {
+            if (instance == null)
+            {
+                instance = this;
+
+                StreamEnergy.Unity.Container.Instance.Initialize(/* TODO */);
+
+                var container = StreamEnergy.Unity.Container.Instance.Unity;
+
+                DependencyResolver.SetResolver(new global::Unity.Mvc5.UnityDependencyResolver(container));
+                GlobalConfiguration.Configuration.DependencyResolver = new global::Unity.WebApi.UnityDependencyResolver(container);
+            }
+        }
+    }
+}
---
+++ Website/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Website
+{
+    public class Global : Sitecore.Web.Application
+    {
+        private static bool initialized;
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("");
+            routes.MapRoute(
+                "Default",
+                "HelloWorld/{action}/{id}",
+                new { controller = "HelloWorld", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+        public override void Init()
+        {
+            if (!initialized)
+            {
+                initialized = true;
+                AreaRegistration.RegisterAllAreas();
+
+                RegisterGlobalFilters(GlobalFilters.Filters);
+                RegisterRoutes(RouteTable.Routes);
+            }
+            base.Init();
+        }
+    }
+}
