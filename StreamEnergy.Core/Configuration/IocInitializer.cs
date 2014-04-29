83af38c61d995a9662cd54333084deb5dd0e964c
Matt DeKrey <mattdekrey@gmail.com>
2014-04-29 13:53:00 -0400

    Added config section for the initializers

---
+++ StreamEnergy.Core/Configuration/IocInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreamEnergy.Configuration
+{
+    class IocInitializer : System.Configuration.ConfigurationElement
+    {
+        internal class KeyGenerator : IKeyGenerator<IocInitializer>
+        {
+            object IKeyGenerator<IocInitializer>.GetKey(IocInitializer element)
+            {
+                return element.TypeName;
+            }
+        }
+
+
+        [System.Configuration.ConfigurationProperty("type", IsRequired = true)]
+        public string TypeName
+        {
+            get
+            {
+                return (string)base["type"];
+            }
+            set
+            {
+                base["type"] = value;
+            }
+        }
+
+        public Unity.IContainerSetupStrategy Build()
+        {
+            try
+            {
+                var type = Type.GetType(TypeName);
+                return Activator.CreateInstance(type) as Unity.IContainerSetupStrategy;
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error("Could not build Unity.IContainerSetupStrategy of type " + TypeName, ex, this);
+                return null;
+            }
+        }
+    }
+}
--- StreamEnergy.Core/StreamEnergyModule.cs
+++ StreamEnergy.Core/StreamEnergyModule.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -29,7 +30,19 @@ namespace StreamEnergy
             {
                 instance = this;
 
-                StreamEnergy.Unity.Container.Instance.Initialize(/* TODO */);
+                var configuration = WebConfigurationManager.OpenWebConfiguration("~").GetSection("streamEnergy") as Configuration.ConfigurationSection;
+
+                if (configuration == null)
+                {
+                    Sitecore.Diagnostics.Log.SingleError("Could not find \"streamEnergy\" configuration section.", this);
+                }
+                else
+                {
+                    StreamEnergy.Unity.Container.Instance.Initialize((from iocInitializer in configuration.InversionOfControlInitializers
+                                                                      let value = iocInitializer.Build()
+                                                                      where value != null
+                                                                      select value).ToArray());
+                }
 
                 var container = StreamEnergy.Unity.Container.Instance.Unity;
 
