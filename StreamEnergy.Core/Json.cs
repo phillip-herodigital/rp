9f392ad504b8feadaf5e766f4c35d54bb210b283
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-02 08:47:05 -0400

    Upgrade Newtonsoft.Json and add an HtmlHelper to JsonStringify.

---
+++ StreamEnergy.Core/Json.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamEnergy
+{
+    public static class Json
+    {
+        private static readonly JsonSerializerSettings standardFormatting;
+
+        public static string Stringify(object target)
+        {
+            return JsonConvert.SerializeObject(target, StandardFormatting);
+        }
+
+        public static JsonSerializerSettings StandardFormatting
+        {
+            get
+            {
+                return new JsonSerializerSettings
+                    {
+                        ContractResolver = new CamelCasePropertyNamesContractResolver()
+                    };
+            }
+        }
+    }
+}
