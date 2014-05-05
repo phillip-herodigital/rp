f2a1a7638bff4eeefed79e3e777a5023bf211171
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-05 11:16:22 -0400

    Create a "StreamEnergy.DomainModel" library and reference it from Website. Include a basic Address model.

---
+++ StreamEnergy.DomainModel/DomainModels/Address.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamEnergy.DomainModels
+{
+    public class Address : ISanitizable
+    {
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string UnitNumber { get; set; }
+        public string City { get; set; }
+        public string StateAbbreviation { get; set; }
+        public string PostalCode5 { get; set; }
+        public string PostalCodePlus4 { get; set; }
+
+        void ISanitizable.Sanitize()
+        {
+            if (AddressLine1 != null)
+                AddressLine1 = AddressLine1.Trim();
+
+            if (AddressLine2 != null) 
+                AddressLine2 = AddressLine2.Trim();
+
+            if (UnitNumber != null) 
+                UnitNumber = UnitNumber.Trim();
+
+            if (City != null) 
+                City = City.Trim();
+
+            if (StateAbbreviation != null) 
+                StateAbbreviation = StateAbbreviation.Trim();
+
+            if (PostalCode5 != null) 
+                PostalCode5 = PostalCode5.Trim();
+
+            if (PostalCodePlus4 != null) 
+                PostalCodePlus4 = PostalCodePlus4.Trim();
+        }
+    }
+}
