6099ed9e6d9e41690359ee92b8970bae4ba4d464
Ryan Brill <ryanbrill@gmail.com>
2014-05-06 15:20:23 -0700

    Working on floatLabel directive

--- Website/frontend/assets/js/directives/float-labels.js
+++ Website/frontend/assets/js/directives/float-labels.js
@@ -1,9 +1,48 @@
 // Float Labels
-ngApp.directive('floatLabels', [function () {
+ngApp.directive('floatLabel', ['$timeout', '$parse', 'breakpoint', function ($timeout, $parse, breakpoint) {
 	return {
-		restrict: 'A',
+	    restrict: 'A',
+        scope: true,
+        template: '<div ng-class="{\'fl-enabled\': isEnabled, \'fl-active\': isActive}" ng-transclude></div>',
+	    transclude: true,
+        replace: true,
 		link: function(scope, element, attrs) {
-			
+		    var formEl = angular.element(element[0].querySelectorAll('input,select,textarea')[0]); // Get first form input
+
+		    formEl.on('propertychange keyup input paste', function () {
+		        if (formEl.val() === '') {
+		            scope.isActive = false;
+		            scope.$apply();
+		        } else {
+		            scope.isActive = true;
+		            scope.$apply();
+		        }
+		    });
+
+            // Check if a model is bound to element, and bind watch
+		    var modelValue = $parse(formEl.attr('ng-model'));
+		    scope.$watch(modelValue, function (newValue, oldValue) {
+		        if (newValue === '') {
+		            scope.isActive = false;
+		        } else {
+		            scope.isActive = true;
+		        }
+		    });
+
+            // Watch the breakpoint for updates
+		    scope.$watch(breakpoint.getBreakpoint, function (newValue, oldValue) {
+		        if (newValue == 'phone') {
+		            scope.isEnabled = true;
+		        } else {
+		            scope.isEnabled = false;
+		        }
+		    });
+
+		    $timeout(function () {
+                // Wait for a model to be bound
+		        formEl.triggerHandler('keyup');
+		    });
+            
 		}
 	};
 }]);
---
+++ Website/frontend/assets/js/services/breakpoint.js
@@ -0,0 +1,44 @@
+﻿ngApp.factory("breakpoint", ['$window', '$rootScope', function ($window, $rootScope) {
+
+    var sizes = [
+         {
+             'name': 'phone',
+             'dimensions': [0, 767]
+         },
+         {
+             'name': 'tablet',
+             'dimensions': [768, 1024]
+         },
+         {
+             'name': 'desktop',
+             'dimensions': [1025, 9999]
+         }
+    ],
+    selectedSize = null,
+    setBreakpoint = function (winWidth) {
+        for (var i = 0, len = sizes.length; i < len; i++) {
+            if (winWidth > sizes[i].dimensions[0] && winWidth < sizes[i].dimensions[1]) {
+                selectedSize = sizes[i].name;
+            }
+        }
+    };
+
+    setBreakpoint($window.innerWidth);
+
+    $rootScope.$watch(function () {
+        return $window.innerWidth;
+    }, function (newValue, oldValue) {
+        setBreakpoint(newValue);
+    });
+
+    window.onresize = function () {
+        $rootScope.$apply();
+    };
+
+    return {
+       getBreakpoint: function () {
+            return selectedSize;
+        }
+    }
+
+}]);
