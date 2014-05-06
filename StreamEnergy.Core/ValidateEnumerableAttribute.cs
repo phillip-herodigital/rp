e9362fd6a0d56d0c152eba079fd235137d4d39ec
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-06 14:46:40 -0400

    Add the ability to validate child properties (only) if necessary

--- StreamEnergy.Core/ValidateEnumerableAttribute.cs
+++ StreamEnergy.Core/ValidateEnumerableAttribute.cs
@@ -1,43 +1,31 @@
-﻿using System;
+﻿using StreamEnergy.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using StreamEnergy.Extensions;
 
 namespace StreamEnergy
 {
-    public class ValidateEnumerableAttribute : ValidationAttribute
+    public class ValidateEnumerableAttribute : CompositeValidationAttribute
     {
-        public ValidateEnumerableAttribute()
-        {
-            PrefixMembers = true;
-        }
-
-        public bool PrefixMembers { get; set; }
-        public string ErrorMessagePrefix { get; set; }
-
-
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var results = from entry in ((System.Collections.IEnumerable)value).OfType<object>().Select((obj, index) => new { obj, index })
-                          let context = new ValidationContext(entry.obj)
-                          let innerResults = new HashSet<ValidationResult>()
-                          where !Validator.TryValidateObject(entry.obj, context, innerResults, true)
-                          from result in innerResults.Flatten(result => result as IEnumerable<ValidationResult>, leafNodesOnly: true)
-                          select new ValidationResult(result.ErrorMessage.Prefix(ErrorMessagePrefix),
-                                                             from member in result.MemberNames
-                                                             select PrefixMembers ? member.Prefix(validationContext.MemberName + "[" + entry.index + "]") : member);
-            results = results.ToArray();
-
-            if (results.Any())
+            if (value != null)
             {
-                return new CompositeValidationResult(results, ErrorMessageString, new[] { validationContext.MemberName });
+                var memberName = validationContext.MemberName;
+                var results = from entry in ((System.Collections.IEnumerable)value).OfType<object>().Select((obj, index) => new { obj, index })
+                              let context = new ValidationContext(entry.obj)
+                              let innerResults = new HashSet<ValidationResult>()
+                              where !Validator.TryValidateObject(entry.obj, context, innerResults, true)
+                              from innerResult in innerResults.Flatten(result => result as IEnumerable<ValidationResult>, leafNodesOnly: true)
+                              select Tuple.Create(memberName +  "[" + entry.index + "]", innerResult);
+
+                return ResultFromInnerResults(results, memberName);
             }
 
             return ValidationResult.Success;
         }
-
     }
 }
---
+++ StreamEnergy.Core/ValidationService.cs
@@ -0,0 +1,63 @@
+﻿using StreamEnergy.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamEnergy
+{
+    internal class ValidationService : IValidationService
+    {
+        public IEnumerable<ValidationResult> CompleteValidate<T>(T target)
+        {
+            var validationContext = new ValidationContext(target);
+            var validations = new HashSet<ValidationResult>();
+
+            Validator.TryValidateObject(target, validationContext, validations, true);
+
+            return validations.Flatten(result => result as IEnumerable<ValidationResult>, leafNodesOnly: true);
+        }
+
+        public IEnumerable<ValidationResult> PartialValidate<T>(T target, params Expression<Func<T, object>>[] properties)
+        {
+            var validationContext = new ValidationContext(target);
+            var validations = new List<ValidationResult>();
+            foreach (var property in from v in properties
+                                     let property = (v.RemoveLambdaBody().RemoveCast() as MemberExpression)
+                                     let propertyChain = UnrollPropertyChain(property)
+                                     select new
+                                     {
+                                         messagePrefix = string.Join("", (from p in propertyChain
+                                                                          where p != property.Member
+                                                                          from a in p.GetCustomAttributes().OfType<CompositeValidationAttribute>()
+                                                                          select a.ErrorMessagePrefix)),
+                                         name = string.Join(".", propertyChain.Select(mi => mi.Name)),
+                                         value = v.CachedCompile<Func<T, object>>()(target),
+                                         attrs = property.Member.GetCustomAttributes(true).OfType<ValidationAttribute>()
+                                     })
+            {
+                validationContext.MemberName = property.name;
+                var innerValidations = new HashSet<ValidationResult>();
+                Validator.TryValidateValue(property.value, validationContext, innerValidations, property.attrs);
+                validations.AddRange(from v in innerValidations.Flatten(v => v as IEnumerable<ValidationResult>, leafNodesOnly: true)
+                                     select new ValidationResult(v.ErrorMessage.Prefix(property.messagePrefix), v.MemberNames));
+            }
+            return validations.Flatten(result => result as IEnumerable<ValidationResult>, leafNodesOnly: true);
+        }
+
+        private IEnumerable<MemberInfo> UnrollPropertyChain(MemberExpression expression)
+        {
+            var members = new Stack<MemberInfo>();
+            while (expression != null)
+            {
+                members.Push(expression.Member);
+                expression = expression.Expression as MemberExpression;
+            }
+            return members.ToArray();
+        }
+    }
+}
