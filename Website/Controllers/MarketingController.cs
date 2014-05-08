0909c354958bf66a3d33002a3758bc2149891bde
Adam Brill <adambrill@gmail.com>
2014-05-08 10:30:29 -0700

    Added the Marketing Controller and hooked it up to the HomeLife Services view

---
+++ Website/Controllers/MarketingController.cs
@@ -0,0 +1,44 @@
+﻿using StreamEnergy.MyStream.Models.Marketing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreamEnergy.MyStream.Controllers
+{
+    public class MarketingController : Controller
+    {
+        public ActionResult HomeLifeServices(string mock)
+        {
+            HomeLifeServices model = null;
+
+            if (mock == "test")
+            {
+                model = new HomeLifeServices()
+                {
+                    PostUrl = "/",
+                    ClientID = "ClientID",
+                    CampaignName = "CampaignName",
+                    CustomerNumber = "CustomerNumber",
+                    FirstName = "FirstName",
+                    LastName = "LastName",
+                    Address1 = "Address1",
+                    Address2 = "Address2",
+                    City = "City",
+                    State = "State",
+                    ZipCode = "ZipCode",
+                    Phone1 = "Phone1",
+                    Phone2 = "Phone2",
+                    Phone3 = "Phone3",
+                    RepId = "RepId",
+                    RepFirstName = "RepFirstName",
+                    RepLastName = "RepLastName",
+                    RepEmail = "RepEmail",
+                };
+            }
+
+            return View("~/Views/Pages/Marketing/Services/HomeLife Services.cshtml", model);
+        }
+	}
+}
