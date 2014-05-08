7b72937dfb4c1d017525456b12e0abce7e73d1c5
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-08 09:01:42 -0400

    Setup Lambda mocks (no parameter testing yet).

--- StreamEnergy.StreamCommons/Services/Clients/ClientContainerSetup.cs
+++ StreamEnergy.StreamCommons/Services/Clients/ClientContainerSetup.cs
@@ -19,17 +19,29 @@ namespace StreamEnergy.Services.Clients
 
         protected override IEnumerable<Type> GetParentTypes(Type instanceType)
         {
-            return instanceType.GetInterfaces().Where(t => t.Assembly == instanceType.Assembly);
+            return instanceType.GetInterfaces().Where(t => t.Assembly == instanceType.Assembly).Except(new[]
+            {
+                typeof(IServiceMockResolver)
+            });
         }
 
         protected override void AdditionalSetup(IUnityContainer unityContainer)
         {
             unityContainer.RegisterType<ServiceMockResolver>(new ContainerControlledLifetimeManager());
-            unityContainer.Resolve<ServiceMockResolver>().MockResolvers.Add(new EmbeddedResourceMockResolver(this.GetType().Assembly));
+            SetupMockResolvers(unityContainer, unityContainer.Resolve<ServiceMockResolver>());
 
             unityContainer.RegisterInstance(WrapService<Sample.Temperature.TempConvertSoap>(unityContainer,
                 new Sample.Temperature.TempConvertSoapClient(new System.ServiceModel.BasicHttpBinding(), new System.ServiceModel.EndpointAddress("http://www.w3schools.com/webservices/tempconvert.asmx"))
             ));
+        }
+
+        private void SetupMockResolvers(IUnityContainer unityContainer, ServiceMockResolver mockResolver)
+        {
+            mockResolver.MockResolvers.Add(new EmbeddedResourceMockResolver(this.GetType().Assembly));
+
+            var temp = unityContainer.Resolve<LambdaToResourceMockResolver>(new DependencyOverride(typeof(System.Reflection.Assembly), this.GetType().Assembly));
+            temp.Register<Sample.Temperature.TempConvertSoap>(s => s.CelsiusToFahrenheit(null), nvc => true, "StreamEnergy.Services.Clients.Mocks.CelsiusToFahrenheit_Response.soap");
+            mockResolver.MockResolvers.Add(temp);
         }
 
         private TInterface WrapService<TInterface>(IUnityContainer unityContainer, TInterface soapClient)
---
+++ StreamEnergy.StreamCommons/Services/Clients/LambdaToResourceMockResolver.cs
@@ -0,0 +1,65 @@
+﻿using StreamEnergy.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamEnergy.Services.Clients
+{
+    public class LambdaToResourceMockResolver : IServiceMockResolver
+    {
+        private struct ResponseTest
+        {
+            public Predicate<NameValueCollection> Test;
+            public string Response;
+        }
+
+        private System.Reflection.Assembly sourceAsm;
+        private readonly Dictionary<MethodInfo, List<ResponseTest>> envelopes = new Dictionary<MethodInfo, List<ResponseTest>>();
+
+        public LambdaToResourceMockResolver(System.Reflection.Assembly sourceAsm)
+        {
+            this.sourceAsm = sourceAsm;
+        }
+
+        public void Register<T>(Expression<Func<T, object>> service, Predicate<NameValueCollection> match, string resource)
+        {
+            var method = service.SimpleMethodCall();
+
+            if (!envelopes.ContainsKey(method))
+            {
+                envelopes.Add(method, new List<ResponseTest>());
+            }
+
+            using (var stream = sourceAsm.GetManifestResourceStream(resource))
+            using (var sr = new StreamReader(stream))
+            {
+                envelopes[method].Add(new ResponseTest
+                {
+                    Test = match,
+                    Response = sr.ReadToEnd()
+                });
+            }
+        }
+
+        bool IServiceMockResolver.ApplyMock(Castle.DynamicProxy.IInvocation invocation)
+        {
+            if (envelopes.ContainsKey(invocation.Method))
+            {
+                var mockParameters = new NameValueCollection(); // TODO - get these from somewhere
+                var result = envelopes[invocation.Method].FirstOrDefault(m => m.Test(mockParameters));
+                if (result.Response != null)
+                {
+                    invocation.ReturnValue = SoapConverter.FromSoap(result.Response, invocation.Method.ReturnType);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
