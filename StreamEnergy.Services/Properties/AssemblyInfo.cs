15bc4f5b66a13f73a1597fbc366d682ea24820dd
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-08 13:46:46 -0400

    Rename StreamEnergy.StreamCommons library to StreamEnergy.Services.

--- StreamEnergy.Services/Properties/AssemblyInfo.cs
+++ StreamEnergy.Services/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StreamEnergy.StreamCommons")]
+[assembly: AssemblyTitle("StreamEnergy.Services")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StreamEnergy.StreamCommons")]
+[assembly: AssemblyProduct("StreamEnergy.Services")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
