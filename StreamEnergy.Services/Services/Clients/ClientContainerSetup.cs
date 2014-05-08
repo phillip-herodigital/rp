76a30cb65e1d99c7dac252058594884430e88ebd
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-08 14:38:02 -0400

    Clean up the container setups for the services.

--- StreamEnergy.Services/Services/Clients/ClientContainerSetup.cs
+++ StreamEnergy.Services/Services/Clients/ClientContainerSetup.cs
@@ -27,35 +27,14 @@ namespace StreamEnergy.Services.Clients
 
         protected override void AdditionalSetup(IUnityContainer unityContainer)
         {
-            unityContainer.RegisterType<ServiceMockResolver>(new ContainerControlledLifetimeManager());
-            SetupMockResolvers(unityContainer, unityContainer.Resolve<ServiceMockResolver>());
-
-            unityContainer.RegisterInstance(WrapService<Sample.Temperature.TempConvertSoap>(unityContainer,
-                new Sample.Temperature.TempConvertSoapClient(new System.ServiceModel.BasicHttpBinding(), new System.ServiceModel.EndpointAddress("http://www.w3schools.com/webservices/tempconvert.asmx"))
-            ));
-
-            unityContainer.RegisterInstance(WrapService<Sample.Commons.SampleStreamCommonsSoap>(unityContainer,
-                new Sample.Commons.SampleStreamCommonsSoapClient(new System.ServiceModel.BasicHttpBinding(), new System.ServiceModel.EndpointAddress("http://www.example.com/webservices"))
-            ));
+            RegisterService<Sample.Temperature.TempConvertSoap>(unityContainer, new Sample.Temperature.TempConvertSoapClient(new System.ServiceModel.BasicHttpBinding(), new System.ServiceModel.EndpointAddress("http://www.w3schools.com/webservices/tempconvert.asmx")));
+            RegisterService<Sample.Commons.SampleStreamCommonsSoap>(unityContainer, new Sample.Commons.SampleStreamCommonsSoapClient(new System.ServiceModel.BasicHttpBinding(), new System.ServiceModel.EndpointAddress("http://www.example.com/webservices")));
         }
 
-        private void SetupMockResolvers(IUnityContainer unityContainer, ServiceMockResolver mockResolver)
-        {
-            mockResolver.MockResolvers.Add(new EmbeddedResourceMockResolver(this.GetType().Assembly));
-
-            var temp = unityContainer.Resolve<LambdaToResourceMockResolver>(new DependencyOverride(typeof(System.Reflection.Assembly), this.GetType().Assembly));
-            
-            temp.Register<Sample.Temperature.TempConvertSoap>(s => s.CelsiusToFahrenheit(null), mockParams => mockParams.Contains("nice"), "StreamEnergy.Services.Clients.Mocks.CelsiusToFahrenheit_Response.soap");
-            temp.Register<Sample.Commons.SampleStreamCommonsSoap>(s => s.GetInvoices(null), mockParams => mockParams.Contains("new"), "StreamEnergy.Services.Clients.Mocks.GetInvoices_New_Response.soap");
-            temp.Register<Sample.Commons.SampleStreamCommonsSoap>(s => s.GetInvoices(null), mockParams => true, "StreamEnergy.Services.Clients.Mocks.GetInvoices_Response.soap");
-
-            mockResolver.MockResolvers.Add(temp);
-        }
-
-        private TInterface WrapService<TInterface>(IUnityContainer unityContainer, TInterface soapClient)
+        private void RegisterService<TInterface>(IUnityContainer unityContainer, TInterface soapClient)
             where TInterface : class
         {
-            return proxyGenerator.CreateInterfaceProxyWithTarget(soapClient, unityContainer.Resolve<ServiceMockInterceptor>());
+            unityContainer.RegisterType<TInterface>(new InjectionFactory(uc => proxyGenerator.CreateInterfaceProxyWithTarget(soapClient, uc.Resolve<ServiceMockInterceptor>())));
         }
     }
 }
