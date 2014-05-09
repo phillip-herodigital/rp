e9a8eefdfada354a0cc4d83655c59911ffef89c6
Adam Brill <adambrill@gmail.com>
2014-05-09 09:10:12 -0700

    Fixed spacing

--- Website/Models/Marketing/HomeLifeServices.cs
+++ Website/Models/Marketing/HomeLifeServices.cs
@@ -11,20 +11,20 @@ namespace StreamEnergy.MyStream.Models.Marketing
         public string ClientID { get; set; }
 
         public string CampaignName { get; set; }
-		public string CustomerNumber { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string Address1 { get; set; }
-		public string Address2 { get; set; }
-		public string City { get; set; }
-		public string State { get; set; }
-		public string ZipCode { get; set; }
-		public string Phone1 { get; set; }
-		public string Phone2 { get; set; }
-		public string Phone3 { get; set; }
-		public string RepId { get; set; }
-		public string RepFirstName { get; set; }
-		public string RepLastName { get; set; }
+        public string CustomerNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone1 { get; set; }
+        public string Phone2 { get; set; }
+        public string Phone3 { get; set; }
+        public string RepId { get; set; }
+        public string RepFirstName { get; set; }
+        public string RepLastName { get; set; }
         public string RepEmail { get; set; }
     }
 }
