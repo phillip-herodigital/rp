b692cc304236723fbaeefd46f9cee3af1314044f
Adam Brill <adambrill@gmail.com>
2014-05-13 14:00:38 -0700

    Worked on the GeoProvider

--- StreamEnergy.Core/Providers/GeoProvider.cs
+++ StreamEnergy.Core/Providers/GeoProvider.cs
@@ -6,32 +6,36 @@ using System.Threading.Tasks;
 using Sitecore.Analytics.Lookups;
 using MaxMind.GeoIP2;
 using System.Configuration;
+using System.Collections.Specialized;
 
 namespace StreamEnergy.Providers
 {
-    public class GeoProvider : Sitecore.Analytics.Lookups.MaxMindProvider
+    public class GeoProvider : LookupProviderBase
     {
+        public GeoProvider() : base()
+        {
+            
+        }
         public override WhoIsInformation GetInformationByIp(string ip)
         {
             var whois = new WhoIsInformation();
-            var dbLocations = ConfigurationManager.AppSettings["GeoIP2.DbLocation"];
+            var dbLocations = Sitecore.Configuration.Settings.GetSetting("GeoIP2.DbLocation", null);
             if (!string.IsNullOrEmpty(dbLocations))
             {
-                var reader = new DatabaseReader(dbLocations);
-                var omni = reader.Omni("128.101.101.101");
-
-                Console.WriteLine(omni.Country.IsoCode); // 'US'
-                Console.WriteLine(omni.Country.Name); // 'United States'
-
-                Console.WriteLine(omni.MostSpecificSubdivision.Name); // 'Minnesota'
-                Console.WriteLine(omni.MostSpecificSubdivision.IsoCode); // 'MN'
-
-                Console.WriteLine(omni.City.Name); // 'Minneapolis'
-
-                Console.WriteLine(omni.Postal.Code); // '55455'
-
-                Console.WriteLine(omni.Location.Latitude); // 44.9733
-                Console.WriteLine(omni.Location.Longitude); // -93.2323
+                var reader = new DatabaseReader(System.Web.HttpContext.Current.Server.MapPath(dbLocations));
+                var omni = reader.Omni(ip);
+                //whois.AreaCode = "";
+                whois.BusinessName = omni.Traits.Organization;
+                whois.City = omni.City.Name;
+                whois.Country = omni.Country.Name;
+                //whois.Dns = "";
+                whois.Isp = omni.Traits.Isp;
+                whois.Latitude = omni.Location.Latitude.ToString();
+                whois.Longitude = omni.Location.Longitude.ToString();
+                whois.MetroCode = omni.Location.MetroCode.ToString();
+                whois.PostalCode = omni.Postal.Code;
+                whois.Region = omni.MostSpecificSubdivision.Name;
+                whois.Url = omni.Traits.Domain;
             }
             return whois;
         }
