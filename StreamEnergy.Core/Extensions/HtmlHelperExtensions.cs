e79a1d808cf17963ca153c1a33d6a03445c267b0
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-13 12:35:19 -0400

    Work on adapting MVC validations to use Sitecore. Still needs jquery validation.

--- StreamEnergy.Core/Extensions/HtmlHelperExtensions.cs
+++ StreamEnergy.Core/Extensions/HtmlHelperExtensions.cs
@@ -6,6 +6,7 @@ using Sitecore.Resources.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -52,6 +53,29 @@ namespace StreamEnergy.Extensions
         public static string JsonStringify(this HtmlHelper htmlHelper, object target)
         {
             return Json.Stringify(target);
+        }
+
+        public static IHtmlString ValidationAttributes<T, U>(this HtmlHelper<T> html, Expression<Func<T, U>> model, Item translateFrom = null)
+        {
+            var temp = model.RemoveLambdaBody().RemoveCast();
+
+            var propertyChain = StreamEnergy.CompositeValidationAttribute.UnrollPropertyChain(temp as MemberExpression);
+            var prefix = StreamEnergy.CompositeValidationAttribute.GetPrefix(propertyChain);
+
+            var metadata = ModelMetadata.FromLambdaExpression(model, html.ViewData);
+
+            var clientRules = StreamEnergy.CompositeValidationAttribute.FilterClientRules(propertyChain, ModelValidatorProviders.Providers.GetValidators(metadata, html.ViewContext).SelectMany(v => v.GetClientValidationRules())).ToArray();
+            foreach (var rule in clientRules)
+            {
+                rule.ErrorMessage = (prefix + rule.ErrorMessage).RenderFieldFrom(translateFrom ?? (Item)html.ViewBag.ValidationMessagesItem ?? html.Sitecore().CurrentItem, true);
+            }
+
+            var dictionary = new Dictionary<string, object>();
+            UnobtrusiveValidationAttributesGenerator.GetValidationAttributes(clientRules, dictionary);
+            dictionary["data-val-name"] = StreamEnergy.CompositeValidationAttribute.GetPathedName(propertyChain);
+
+            return html.Raw(string.Join(" ", from attr in dictionary
+                                             select attr.Key + "=\"" + attr.Value + "\""));
         }
     }
 }
