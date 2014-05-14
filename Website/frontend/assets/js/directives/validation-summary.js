60a7ad9a02d4d62a16b0b6344d7d16aad3f17995
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-14 12:38:51 -0400

    Add in MVC Validation Summary support.

---
+++ Website/frontend/assets/js/directives/validation-summary.js
@@ -0,0 +1,24 @@
+﻿ngApp.directive('valmsgSummary', ['validation', function (validation) {
+
+    return {
+        restrict: 'A',
+        scope: {},
+        template: '<ul>' +
+            '<li data-ng-repeat="err in validationSummary" data-ng-bind-html="err"></span>' +
+            '</ul>',
+        link: function (scope) {
+            console.log('linked');
+            scope.validationSummary = [];
+            scope.$watchCollection(function () { return scope.$parent[validation.messageArray]; }, function (newValue) {
+                var merged = [];
+                var obj = scope.$parent[validation.messageArray];
+                for (key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        merged = merged.concat(obj[key]);
+                    }
+                }
+                scope.validationSummary = merged;
+            });
+        }
+    };
+}]);
