9acd8275490a09962178402c16830457aed4dd28
Adam Powell <adam.powell@responsivepath.com>
2014-05-14 12:59:29 -0700

    adding EmailService  to StreamEnergy.Services

---
+++ StreamEnergy.Services/Services/Clients/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using SendGridMail;
+
+namespace StreamEnergy.Services.Clients
+{
+    class EmailService : IEmailService
+    {   
+        string IEmailService.SendEmail(MailMessage message)
+        {
+            // Create network credentials to access your SendGrid account.
+            var username = Sitecore.Configuration.Settings.GetSetting("SendGrid.username", null);;
+            var pswd = Sitecore.Configuration.Settings.GetSetting("SendGrid.password", null);;
+
+            var credentials = new NetworkCredential(username, pswd);
+
+            // Create the email object first, then add the properties.
+            SendGrid myMessage = SendGrid.GetInstance();
+            myMessage.AddTo("adam.powell@responsivepath.com");
+            myMessage.From = new MailAddress("adam.powell@responsivepath.com", "Adam Powell");
+            myMessage.Subject = "Testing the SendGrid Library";
+            myMessage.Text = "Hello World!";
+
+            // Create an Web transport for sending email.
+            var transportWeb = SendGridMail.Web.GetInstance(credentials);
+
+            // Send the email.
+            try 
+            {
+                transportWeb.Deliver(myMessage);
+                return "Success";
+            }
+            catch
+            {
+                return "Email Failed";
+            }
+            
+
+        }
+    }
+}
---
+++ Website/Controllers/TestController.cs
@@ -0,0 +1,64 @@
+﻿using StreamEnergy.DomainModels;
+using StreamEnergy.Services.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreamEnergy.MyStream.Controllers
+{
+    public class TestController : Controller
+    {
+        private IEmailService emailService;
+        public TestController (IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+        
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            return View("~/Views/Pages/Marketing/Contact/Contact.cshtml");
+        }
+
+        //
+        // POST: /Test/Create
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            try
+            {
+                var FirstName = collection["firstName"];
+                var LastName = collection["lastName"];
+                var AddressLine1 = collection["address"];
+                var City = collection["city"];
+                var StateAbbreviation = collection["state"];
+                var PostalCode5 = collection["zipCode"];
+                var Phone = collection["phone"];
+                var Email = collection["email"];
+                var Reason = collection["reason"];
+                var Comments = collection["comments"];
+
+                var Message = new MailMessage();
+                Message.Body = Comments;
+
+                // TODO - Validate form data
+
+                // TODO - Send the email
+                var result = this.emailService.SendEmail(Message);
+                // TODO - Send the success message or load a new view
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("~/Views/Pages/Marketing/Contact/Contact.cshtml");
+            }
+        }
+    }
+}
