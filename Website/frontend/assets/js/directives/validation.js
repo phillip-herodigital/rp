b112b172532a3c5b32da7f3ab0fd4911c2afecf2
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-14 11:28:30 -0400

    More work on validations - hooking into Angular validation logic.

--- Website/frontend/assets/js/directives/validation.js
+++ Website/frontend/assets/js/directives/validation.js
@@ -9,7 +9,7 @@
         var validators = {};
         for (var index in keys) {
             var key = keys[index];
-            if (key == 'val' || key == 'valName' || !startsWith(key, 'val'))
+            if (key == 'val' || key == 'valName' || key == 'valIf' || !startsWith(key, 'val'))
                 continue;
             var handled = false;
             for (var validator in validators) {
@@ -40,49 +40,57 @@
     };
 
     // Attribute to run validation on an element
-    var link = function (scope, element, attrs) {
+    var link = function (scope, element, attrs, ngModelController) {
+        // TODO - consider $observe-ing the attributes
         if (attrs['val'] != 'true')
             return;
 
         var validationFor = attrs['valName'];
-        var validators = buildValidatorsFromAttributes(attrs);
+        scope.$$validation = scope.$$validation || {};
 
-        scope.$watch(
-            function () { return element.val() },
-            function (newValue) {
-                scope.$$validation.data[validationFor] = newValue;
-                scope.$$validation.errs[validationFor] = [];
-                for (var key in validators)
-                {
-                    if (!validators[key].validate(newValue, validators[key].params))
-                    {
-                        scope.$$validation.errs[validationFor].push($sce.trustAsHtml(validators[key].message));
-                    }
+        var allowValidation = false;
+
+        var validators = buildValidatorsFromAttributes(attrs);
+        var runValidations = function (newValue) {
+            scope.$$validation[validationFor] = [];
+            for (var key in validators) {
+                if (allowValidation && !validators[key].validate(newValue, validators[key].params)) {
+                    ngModelController.$setValidity(key, false);
+                    scope.$$validation[validationFor].push($sce.trustAsHtml(validators[key].message));
                 }
-            });
+                else {
+                    ngModelController.$setValidity(key, true);
+                }
+            }
+        };
+
+        ngModelController.$parsers.unshift(runValidations);
+
+        scope.$watch(attrs['valIf'], function (newValue, oldValue) {
+            allowValidation = !!newValue;
+            runValidations(element.val());
+        });
+
+        element.on('$destroy', function () {
+            scope.$$validation[validationFor] = undefined;
+        });
     };
 
     return {
         restrict: 'A',
-        compile: function (compilingElement, compileAttrs) {
-            if (compileAttrs['ngModel'])
-                return {
-                    post: link
-                };
-
-            compilingElement.attr('data-ng-model', '$$validation.data["' + compileAttrs['valName'] + '"]');
-
-            return {
-                pre: function preLink(scope, iElement, iAttrs, controller) {
-                    scope.$$validation = scope.$$validation || {};
-                    scope.$$validation.data = scope.$$validation.data || {};
-                    scope.$$validation.errs = scope.$$validation.errs || {};
-                    scope.$$validation.data[compileAttrs['valName']] = iElement.val();
-                },
-                post: function postLink(scope, iElement, iAttrs, controller) {
-                    $compile(iElement)(scope);
-                }
-            };
-        }
+        require: 'ngModel',
+        link: link
     };
 }]);
+
+ngApp.directive('form', [function () {
+
+    return {
+        restrict: 'E',
+        link: function (scope) {
+            // add the $$validation object at the form level so that we don't end up adding it
+            // at an inner level, such as an ng-if
+            scope.$$validation = {};
+        }
+    };
+}])
