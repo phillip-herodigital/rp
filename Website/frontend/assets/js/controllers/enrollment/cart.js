2f8c109deda7f7c6d0bae39ca241eb027d10f4e8
Brandon Quintana <bquintana@brandonquintanaconsulting.com>
2014-05-20 15:56:08 -0700

    Progress on enrollment JS.

---
+++ Website/frontend/assets/js/controllers/enrollment/cart.js
@@ -0,0 +1,8 @@
+﻿/* Enrollment Cart Controller
+ *
+ * This is used to control aspects of the cart on enrollment page.
+ */
+ngApp.controller('EnrollmentCartCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+
+
+}]);
---
+++ Website/frontend/assets/js/controllers/enrollment/lets-get-started.js
@@ -0,0 +1,8 @@
+﻿/* Enrollment Let's Get Started Controller
+ *
+ * This is used to control aspects of let's get started on enrollment page.
+ */
+ngApp.controller('EnrollmentLetsGetStartedCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+
+
+}]);
