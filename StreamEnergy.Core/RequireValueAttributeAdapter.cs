4a1e35847b37bbec0f555f021bcee45083d716e4
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-20 14:47:39 -0400

    Add client-side validation for custom attribute.

---
+++ StreamEnergy.Core/RequireValueAttributeAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace StreamEnergy
+{
+    class RequireValueAttributeAdapter : DataAnnotationsModelValidator<RequireValueAttribute>
+    {
+        public RequireValueAttributeAdapter(ModelMetadata metadata, ControllerContext context, RequireValueAttribute attribute)
+            : base(metadata, context, attribute)
+        {
+        }
+
+        public override IEnumerable<ModelClientValidationRule> GetClientValidationRules()
+        {
+            yield return new ModelClientValidationRule 
+            { 
+                ErrorMessage = base.ErrorMessage, 
+                ValidationParameters = 
+                { 
+                    { "value", Json.Stringify(base.Attribute.Value) }
+                },
+                ValidationType = "exactvalue"
+            };
+        }
+    }
+}
