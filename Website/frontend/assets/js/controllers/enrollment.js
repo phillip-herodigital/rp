3b8f748f04de8d25f246872034c2822e9d4e0aa9
Brandon Quintana <bquintana@brandonquintanaconsulting.com>
2014-05-20 15:24:16 -0700

    Starting initial JS for enrollment.

---
+++ Website/frontend/assets/js/controllers/enrollment.js
@@ -0,0 +1,8 @@
+/* Enrollment Controller
+ *
+ * This is used to control aspects of the overall enrollment page.
+ */
+ngApp.controller('EnrollmentCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+
+
+}]);
