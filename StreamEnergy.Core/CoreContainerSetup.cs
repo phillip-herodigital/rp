371ebf9ece236a2908c80f20cbffaede37a08f27
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-21 09:38:24 -0400

    Remove the ISitecoreContext because it really wasn't necessary.

--- StreamEnergy.Core/CoreContainerSetup.cs
+++ StreamEnergy.Core/CoreContainerSetup.cs
@@ -19,6 +19,8 @@ namespace StreamEnergy
             }
             unityContainer.RegisterInstance(environment);
 
+            unityContainer.RegisterType<ISettings, SitecoreSettings>();
+
             var typeIndicatorJsonConverter = new TypeIndicatorJsonConverter();
             unityContainer.RegisterInstance(typeIndicatorJsonConverter);
             Json.AdditionalConverters.Add(typeIndicatorJsonConverter);
--- StreamEnergy.Core/SitecoreSettings.cs
+++ StreamEnergy.Core/SitecoreSettings.cs
@@ -7,16 +7,14 @@ using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
 using Sitecore.SecurityModel;
 
-namespace StreamEnergy.Mvc.Sitecore
+namespace StreamEnergy
 {
     class SitecoreSettings : ISettings
     {
-        private readonly ISitecoreContext sitecoreContext;
         private readonly string environment;
 
-        public SitecoreSettings(ISitecoreContext sitecoreContext, EnvironmentCategory environment)
+        public SitecoreSettings(EnvironmentCategory environment)
         {
-            this.sitecoreContext = sitecoreContext;
             this.environment = environment.ToString();
         }
 
@@ -46,7 +44,7 @@ namespace StreamEnergy.Mvc.Sitecore
             // we don't need to check security settings for reading the item
             using (new SecurityDisabler())
             {
-                var baseItem = sitecoreContext.Database.SelectSingleItem("fast://sitecore/content/data/Settings/" + relativePath);
+                var baseItem = Sitecore.Context.Database.SelectSingleItem("fast://sitecore/content/data/Settings/" + relativePath);
                 if (baseItem == null)
                     yield break;
                 var child = baseItem.Axes.GetChild(environment);
