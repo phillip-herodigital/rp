5b60f242ad76a039aa29db5ee527198761a6e845
Adam Brill <adambrill@gmail.com>
2014-05-22 10:54:52 -0700

    Testing code - probably should be reverted.

---
+++ StreamEnergy.Core/Pipelines/UpdateGeoIpData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamEnergy.Pipelines
+{
+    public class UpdateGeoIpData : Sitecore.Analytics.Pipelines.CreateVisits.UpdateGeoIpData
+    {
+        public UpdateGeoIpData(): base()
+        {
+
+        }
+        public override void Process(Sitecore.Analytics.Pipelines.CreateVisits.CreateVisitArgs args)
+        {
+            base.Process(args);
+        }
+    }
+}
