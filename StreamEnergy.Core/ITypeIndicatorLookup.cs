46481e5f901f6b44bde0a443cd9550525b4f5452
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-22 15:27:39 -0400

    Clean up how the Polymorphic Serialization stuff works.

---
+++ StreamEnergy.Core/ITypeIndicatorLookup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace StreamEnergy
+{
+    public interface ITypeIndicatorLookup
+    {
+        Type SuperType { get; }
+        Type FindMatch(JObject data);
+    }
+}
--- StreamEnergy.DomainModel/DomainModels/PolymorphicSerializationContainerSetup.cs
+++ StreamEnergy.DomainModel/DomainModels/PolymorphicSerializationContainerSetup.cs
@@ -12,25 +12,27 @@ namespace StreamEnergy.DomainModels
     {
         void IContainerSetupStrategy.SetupUnity(IUnityContainer unityContainer)
         {
-            unityContainer.Resolve<TypeIndicatorJsonConverter>().TypeIndicators.Add(new TypeIndicatorLookup<IServiceCapability, SampleServiceCapability, TexasServiceCapability>
-                {
-                    IsMatch = serviceCapability => serviceCapability.CapabilityType == TexasServiceCapability.Qualifier
-                });
+            unityContainer.Resolve<TypeIndicatorJsonConverter>().TypeIndicators.Add(new TypeIndicatorLookup<IServiceCapability, SampleServiceCapability>(sc => sc.CapabilityType)
+            {
+                SupportedTypes = {
+                    { TexasServiceCapability.Qualifier, typeof(TexasServiceCapability) },
+                    { Enrollments.ServiceStatusCapability.Qualifier, typeof(Enrollments.ServiceStatusCapability) }
+                }
+            });
 
-            unityContainer.Resolve<TypeIndicatorJsonConverter>().TypeIndicators.Add(new TypeIndicatorLookup<IServiceCapability, SampleServiceCapability, Enrollments.ServiceStatusCapability>
-                {
-                    IsMatch = serviceCapability => serviceCapability.CapabilityType == Enrollments.ServiceStatusCapability.Qualifier
-                });
+            unityContainer.Resolve<TypeIndicatorJsonConverter>().TypeIndicators.Add(new TypeIndicatorLookup<Enrollments.IOfferOption, Enrollments.SampleOfferOption>(sc => sc.OptionType)
+            {
+                SupportedTypes = {
+                    { Enrollments.TexasElectricityOfferOption.Qualifier, typeof(Enrollments.TexasElectricityOfferOption) }
+                }
+            });
 
-            unityContainer.Resolve<TypeIndicatorJsonConverter>().TypeIndicators.Add(new TypeIndicatorLookup<Enrollments.IOfferOption, Enrollments.SampleOfferOption, Enrollments.TexasElectricityOfferOption>
-                {
-                    IsMatch = serviceCapability => serviceCapability.OptionType == Enrollments.TexasElectricityOfferOption.Qualifier
-                });
-
-            unityContainer.Resolve<TypeIndicatorJsonConverter>().TypeIndicators.Add(new TypeIndicatorLookup<Payments.IPaymentInfo, Payments.SamplePaymentInfo, Payments.TokenizedCard>
-                {
-                    IsMatch = serviceCapability => serviceCapability.PaymentType == Payments.TokenizedCard.Qualifier
-                });
+            unityContainer.Resolve<TypeIndicatorJsonConverter>().TypeIndicators.Add(new TypeIndicatorLookup<Payments.IPaymentInfo, Payments.SamplePaymentInfo>(sc => sc.PaymentType)
+            {
+                SupportedTypes = {
+                    { Payments.TokenizedCard.Qualifier, typeof(Payments.TokenizedCard) }
+                }
+            });
         }
     }
 }
