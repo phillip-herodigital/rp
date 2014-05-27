2e30d6e141d60aa0959c2fa7b85a13d2ab64be00
Adam Brill <adambrill@gmail.com>
2014-05-27 16:10:38 -0700

    Check in WIP code

--- StreamEnergy.Core/Providers/GeoProvider.cs
+++ StreamEnergy.Core/Providers/GeoProvider.cs
@@ -22,20 +22,28 @@ namespace StreamEnergy.Providers
             var dbLocations = Sitecore.Configuration.Settings.GetSetting("GeoIP2.DbLocation", null);
             if (!string.IsNullOrEmpty(dbLocations))
             {
-                var reader = new DatabaseReader(System.Web.HttpContext.Current.Server.MapPath(dbLocations));
-                var omni = reader.Omni(ip);
-                //whois.AreaCode = "";
-                whois.BusinessName = omni.Traits.Organization;
-                whois.City = omni.City.Name;
-                whois.Country = omni.Country.Name;
-                //whois.Dns = "";
-                whois.Isp = omni.Traits.Isp;
-                whois.Latitude = omni.Location.Latitude.ToString();
-                whois.Longitude = omni.Location.Longitude.ToString();
-                whois.MetroCode = omni.Location.MetroCode.ToString();
-                whois.PostalCode = omni.Postal.Code;
-                whois.Region = omni.MostSpecificSubdivision.Name;
-                whois.Url = omni.Traits.Domain;
+                try
+                {
+                    using(var reader = new DatabaseReader(System.Web.HttpContext.Current.Server.MapPath(dbLocations)))
+                    {
+                        var omni = reader.Omni(ip);
+                        //whois.AreaCode = "";
+                        whois.BusinessName = omni.Traits.Organization;
+                        whois.City = omni.City.Name;
+                        whois.Country = omni.Country.Name;
+                        //whois.Dns = "";
+                        whois.Isp = omni.Traits.Isp;
+                        whois.Latitude = omni.Location.Latitude.ToString();
+                        whois.Longitude = omni.Location.Longitude.ToString();
+                        whois.MetroCode = omni.Location.MetroCode.ToString();
+                        whois.PostalCode = omni.Postal.Code;
+                        whois.Region = omni.MostSpecificSubdivision.Name;
+                        whois.Url = omni.Traits.Domain;
+                    }
+                }
+                catch(Exception)
+                {
+                }
             }
             return whois;
         }
