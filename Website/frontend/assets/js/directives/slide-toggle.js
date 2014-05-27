6113b50858137475717534a15d53a7837c65ef66
Justin Myers <justin.myers@responsivepath.com>
2014-05-27 17:01:54 -0500

    Simplified slide-toggle directive. Updating margin on answer section to match question margin

--- Website/frontend/assets/js/directives/slide-toggle.js
+++ Website/frontend/assets/js/directives/slide-toggle.js
@@ -5,23 +5,7 @@ ngApp.directive('slideToggle', [function () {
 		scope: true,
 		// The linking function will add behavior to the template
 		link: function(scope, element, attrs) {
-			var anchorElement = element.find('a');
-
-			//Add class collapsed by default
-			anchorElement.addClass('collapsed');
-
-			if (attrs.openPane) {
-				scope.pane = attrs.openPane;
-			}
-			scope.togglePane = function(pane) {
-				if(scope.pane === pane) {
-					scope.pane = '';
-					anchorElement.addClass('collapsed');
-				} else {
-					scope.pane = pane;
-					anchorElement.removeClass('collapsed');
-				}
-			};
+			scope.isOpen = false;
 		}
 	};
 }]);
