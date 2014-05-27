9d09832b229b213aafaecad056fbd7f7213c6ae7
Justin Myers <justin.myers@responsivepath.com>
2014-05-27 13:56:44 -0500

    Added functionlity for scroll after state selection on mobile

--- Website/frontend/assets/js/directives/state-tabs.js
+++ Website/frontend/assets/js/directives/state-tabs.js
@@ -1,19 +1,31 @@
 // State Tabs Selector
-ngApp.directive('stateTabs', [function () {
+ngApp.directive('stateTabs', ['breakpoint', function (breakpoint) {
 	return {
 		restrict: 'A',
 		scope: true,
 		// The linking function will add behavior to the template
 		link: function(scope, element, attrs) {
-			
+			var isMobile = false;
+
 			if (attrs.stateTabs) {
 				scope.pane = attrs.stateTabs;
 			}
 
 			scope.selectPane = function(pane) {
 				scope.pane = pane;
+
+				//Calculate the height of the tab items and window scroll offset also accounting for the fixed nav height
+				if(isMobile) {
+					window.scrollTo(0, element.children()[0].getBoundingClientRect().top + window.pageYOffset + element.children()[0].offsetHeight - 57);	
+				}
 			};
 
+			//Only use scroll functionality for mobile views
+		    scope.$watch(function () {
+		        return breakpoint.breakpoint.name
+		    }, function (newValue, oldValue) {
+		    	isMobile = newValue == 'phone' ? true : false;
+		    }, true);
 		}
 	};
 }]);
