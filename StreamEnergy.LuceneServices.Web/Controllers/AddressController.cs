7a7b6bed996fff2cd4503b896e2134dfd098d977
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-28 13:47:54 -0400

    Add JsonP capabilities to the address lookup

--- StreamEnergy.LuceneServices.Web/Controllers/AddressController.cs
+++ StreamEnergy.LuceneServices.Web/Controllers/AddressController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 
 namespace StreamEnergy.LuceneServices.Web.Controllers
@@ -20,6 +21,16 @@ namespace StreamEnergy.LuceneServices.Web.Controllers
         public IEnumerable<DomainModels.Enrollments.Location> Lookup(string state, string query)
         {
             return searcher.Search(state, query);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Lookup(string state, string query, string callback)
+        {
+            var result = Lookup(state, query);
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(callback + "(" + StreamEnergy.Json.Stringify(result) + ")", Encoding.UTF8, "text/javascript")
+            };
         }
     }
 }
