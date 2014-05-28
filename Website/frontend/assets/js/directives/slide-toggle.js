ad652b2e5e10eddafb0fa04f7eed9ca84efb47b4
Justin Myers <justin.myers@responsivepath.com>
2014-05-28 12:51:14 -0500

    Updated slideToggle directive to animate using jquery slideToggle. Updated FAQ listng and Career listing to use newest directive code Small styling updates in _marketing-site.scss

--- Website/frontend/assets/js/directives/slide-toggle.js
+++ Website/frontend/assets/js/directives/slide-toggle.js
@@ -1,11 +1,25 @@
 // Slide Toggle
-ngApp.directive('slideToggle', [function () {
+ngApp.directive('slideToggle', ['jQuery', function ($) {
 	return {
 		restrict: 'A',
 		scope: true,
-		// The linking function will add behavior to the template
 		link: function(scope, element, attrs) {
-			scope.isOpen = false;
+			var $slidePanel = element.find('.slide-toggle');
+
+			scope.isOpen = attrs.slideToggle == 'true' ? true : false;
+
+			//Hide the slidePanel if true is not passed in
+			if(!scope.isOpen) {
+				$slidePanel.hide();
+			}
+
+			//Toggle the slidePanel visibility
+			scope.toggleSlide = function() {
+				$slidePanel.slideToggle(300, function() {
+					scope.isOpen = !scope.isOpen;
+					scope.$apply();
+				});
+			};		
 		}
 	};
 }]);
