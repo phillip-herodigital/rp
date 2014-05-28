b857f627950a80f1713f628030b98dbc5934e327
Justin Myers <justin.myers@responsivepath.com>
2014-05-28 18:52:33 -0500

    Updated state-tabs directive with better breakpoint code and jQuery functions.

--- Website/frontend/assets/js/directives/state-tabs.js
+++ Website/frontend/assets/js/directives/state-tabs.js
@@ -1,11 +1,11 @@
 // State Tabs Selector
-ngApp.directive('stateTabs', ['breakpoint', function (breakpoint) {
+ngApp.directive('stateTabs', ['breakpoint', 'jQuery', function (breakpoint, $) {
 	return {
 		restrict: 'A',
 		scope: true,
 		// The linking function will add behavior to the template
 		link: function(scope, element, attrs) {
-			var isMobile = false;
+			var $tabs = element.find('.tabs-nav');
 
 			if (attrs.stateTabs) {
 				scope.pane = attrs.stateTabs;
@@ -15,20 +15,10 @@ ngApp.directive('stateTabs', ['breakpoint', function (breakpoint) {
 				scope.pane = pane;
 
 				//Calculate the height of the tab items and window scroll offset also accounting for the fixed nav height
-				if(isMobile) {
-					window.scrollTo(0, element.children()[0].getBoundingClientRect().top
-									   + window.pageYOffset
-									   + element.children()[0].offsetHeight
-									   - document.querySelector('.site-header').offsetHeight);
+				if(breakpoint.breakpoint.name == 'phone') {
+					window.scrollTo(0, $tabs.offset().top + $tabs.height() - $('.site-header').height());
 				}
 			};
-
-			//Only use scroll functionality for mobile views
-			scope.$watch(function () {
-				return breakpoint.breakpoint.name
-			}, function (newValue, oldValue) {
-				isMobile = newValue == 'phone' ? true : false;
-			}, true);
 		}
 	};
 }]);
