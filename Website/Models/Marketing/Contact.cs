7e05acf4a271c7ca872775ce3a685b7ff6a807a1
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-05-29 16:00:01 -0400

    Corrections to server-side parsing of objects where the assumption is that the object is not created when no values are passed.

--- Website/Models/Marketing/Contact.cs
+++ Website/Models/Marketing/Contact.cs
@@ -11,17 +11,29 @@ namespace StreamEnergy.MyStream.Models.Marketing
     {
         public bool ShowSuccessMessage { get; set; }
 
+        [Required]
         [ValidateObject(ErrorMessagePrefix = "")]
         public Name ContactName { get; set; }
 
         [ValidateObject(ErrorMessagePrefix = "")]
         public Phone ContactPhone { get; set; }
 
+        [Required]
         [ValidateObject(ErrorMessagePrefix = "")]
         public Email ContactEmail { get; set; }
 
-        [ValidateObject(ErrorMessagePrefix = "")]
-        public Address ContactAddress { get; set; }
+        public string AddressLine1 { get; set; }
+
+        [Required(ErrorMessage = "City Required")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "State Required")]
+        public string StateAbbreviation { get; set; }
+
+        [Required(ErrorMessage = "Postal Code Required")]
+        [RegularExpression("^[0-9]{5}$", ErrorMessage = "Postal Code Invalid")]
+        public string PostalCode5 { get; set; }
+
 
         [Required(ErrorMessage = "Reason Required")]
         [StringLength(50, MinimumLength = 1)]
