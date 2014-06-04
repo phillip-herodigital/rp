eb93d305b890a072ea5064776e3f8ffbbb7bd6d9
Justin Myers <justin.myers@responsivepath.com>
2014-06-04 12:28:11 -0500

    Added one time payment cshtml files Updated paymentInfo controller to not conflict with value.js directive

--- Website/frontend/assets/js/controllers/payments/paymentInfo.js
+++ Website/frontend/assets/js/controllers/payments/paymentInfo.js
@@ -2,5 +2,7 @@
 	Payments - One Time Payment Controller
  */
 ngApp.controller('PaymentsCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
-
+	//Had to use the following since ng-value is used to not conflict with the value.js directive
+	$scope.creditCard = 'credit-card';
+	$scope.bankAccount = 'bank-account';
 }]);
