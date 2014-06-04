63cda7d00ed213ae09ed6989d4e18f39f8522a02
Brandon Quintana <bquintana@brandonquintanaconsulting.com>
2014-06-03 20:30:10 -0700

    Completes button selector.

--- Website/frontend/assets/js/directives/button-selector.js
+++ Website/frontend/assets/js/directives/button-selector.js
@@ -4,13 +4,15 @@ ngApp
         unselectedText: 'Select',
         selectedText: 'Selected',
         selectedIcon: '<i class="icon-check"></i>',
-        selectedClass: 'active'
+        selectedClass: 'selected',
+        toggleEvent: 'click'
     })
     .controller('ButtonSelectorController', ['buttonConfig', function (buttonConfig) {
         this.unselectedText = buttonConfig.unselectedText || 'Select';
         this.selectedText = buttonConfig.selectedText || 'Selected';
         this.selectedIcon = buttonConfig.selectedIcon || '<i class="icon-check"></i>';
         this.selectedClass = buttonConfig.selectedClass || 'selected';
+        this.toggleEvent = buttonConfig.toggleEvent || 'click';
     }])
     .directive('buttonSelector', [function () {
         return {
@@ -19,8 +21,29 @@ ngApp
             link: function (scope, element, attrs, ctrls) {
                 var buttonsCtrl = ctrls[0], ngModelCtrl = ctrls[1];
 
+                //model -> UI
+                ngModelCtrl.$render = function () {
+                    element.toggleClass(buttonsCtrl.selectedClass, angular.equals(ngModelCtrl.$modelValue, scope.$eval(attrs.buttonSelector)));
+
+                    var isSelected = element.hasClass(buttonsCtrl.selectedClass);
+
+                    if (isSelected) {
+                        element.html(buttonsCtrl.selectedIcon + ' ' + buttonsCtrl.selectedText);
+                    } else {
+                        element.html(buttonsCtrl.unselectedText);
+                    }
+                };
+
+                //ui->model
                 element.bind('click', function () {
-                    console.log(element);
+                    var isSelected = element.hasClass(buttonsCtrl.selectedClass);
+
+                    if (!isSelected) {
+                        scope.$apply(function () {
+                            ngModelCtrl.$setViewValue(isSelected ? null : scope.$eval(attrs.buttonSelector));
+                            ngModelCtrl.$render();
+                        });
+                    }
                 });
             }
         };
