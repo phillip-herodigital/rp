6b9c3d19f18ebed53723c2ea15520e93be3278cf
Justin Myers <justin.myers@responsivepath.com>
2014-06-04 17:54:13 -0500

    Updated with requested fixes

--- Website/frontend/assets/js/controllers/payments/paymentInfo.js
+++ Website/frontend/assets/js/controllers/payments/paymentInfo.js
@@ -5,4 +5,9 @@ ngApp.controller('PaymentsCtrl', ['$scope', '$rootScope', function ($scope, $roo
 	//Had to use the following since ng-value is used to not conflict with the value.js directive
 	$scope.creditCard = 'credit-card';
 	$scope.bankAccount = 'bank-account';
+	$scope.activeState = 'step1';
+
+	$scope.continue = function() {
+		$scope.activeState = 'step2';
+	}
 }]);
