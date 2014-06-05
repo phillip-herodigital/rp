ee5fe3d2cc7cf1f6d9b99495f87f08d6345affe0
Adam Powell <adam.powell@responsivepath.com>
2014-06-05 10:25:53 -0700

    Added component controller and view validation

---
+++ Website/Controllers/Components/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreamEnergy.MyStream.Controllers.Components
+{
+    public class AuthenticationController : Controller
+    {
+        public ActionResult LoginIndex()
+        {
+            var model = new StreamEnergy.MyStream.Models.Authentication.LoginRequest();
+
+            return View("~/Views/Components/Authentication/My Stream Account.cshtml", model);
+        }
+
+        [HttpPost]
+        [Mvc.ErrorSitecoreTranslation]
+        public ActionResult LoginIndex(StreamEnergy.MyStream.Models.Authentication.LoginResponse response)
+        {
+            // Validate form data
+            if (ModelState.IsValid)
+            {
+                // Go to the /account page
+                var ReturnURL = new RedirectResult("~/account");
+                return ReturnURL;
+            }
+            else
+            {
+                // Return validation errors
+                return View("~/Views/Components/Authentication/My Stream Account.cshtml", response);
+            }
+        }
+    }
+}
