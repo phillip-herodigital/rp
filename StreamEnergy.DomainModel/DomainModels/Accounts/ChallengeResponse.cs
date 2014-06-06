119e7c6186e588e44ba3b620aa715d833bb5635c
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-06-06 11:27:09 -0400

    Make sure the ChallengeResponse is serializable using our chosen method. (Oops.)

--- StreamEnergy.DomainModel/DomainModels/Accounts/ChallengeResponse.cs
+++ StreamEnergy.DomainModel/DomainModels/Accounts/ChallengeResponse.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace StreamEnergy.DomainModels.Accounts
 {
+    [Serializable]
     public class ChallengeResponse
     {
         public string QuestionKey { get; set; }
