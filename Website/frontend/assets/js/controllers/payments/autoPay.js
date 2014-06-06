a805bbe5746c06135d49a61777bacdf71f2d44aa
Justin Myers <justin.myers@responsivepath.com>
2014-06-06 16:47:12 -0500

    Updated datepicker styling Updated styling of autopay page Added functionality to date picker to allow for hiding on resize Added datepicker functionality to autopay page

--- Website/frontend/assets/js/controllers/payments/autoPay.js
+++ Website/frontend/assets/js/controllers/payments/autoPay.js
@@ -4,17 +4,16 @@
 ngApp.controller('AutoPayCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
 	//Currently set autoPay status to false, will eventually set according to the account
 	$scope.autoPay = {
-		status:'autoPayOn'
+		status:'autoPayOn',
+		dt: new Date()
 	};
 	$scope.autoPayOn = 'autoPayOn';
 	$scope.autoPayOff = 'autoPayOff';
+	$scope.minDate = new Date();
 
    // Disable weekend selection
     $scope.disableWeekends = function (date, mode) {
         return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
     };
-    $scope.minDate = new Date();
-    $scope.form = {
-         dt: new Date()
-    };
+
 }]);
--- Website/frontend/assets/js/directives/datepicker.js
+++ Website/frontend/assets/js/directives/datepicker.js
@@ -1,6 +1,6 @@
 ﻿// This extends the Angular UI Bootstrap datepicker-popup directive
 // https://github.com/angular/angular.js/wiki/Understanding-Directives#extending-directives
-ngApp.directive('datepickerPopup', [function () {
+ngApp.directive('datepickerPopup', ['$window', 'breakpoint', function ($window, breakpoint) {
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {
@@ -12,6 +12,19 @@ ngApp.directive('datepickerPopup', [function () {
                 scope.datePickerOpened = true;
             };
 
+            /*
+            Close the datepicker on mobile resize, otherwise it gets out of alignment
+            with the element that it's positioned with. 
+            */
+            if(attrs.datepickerCloseOnResize == '') {
+                angular.element($window).bind('resize', function() {
+                    if(scope.datePickerOpened && breakpoint.breakpoint.name == 'phone')
+                    {
+                        scope.datePickerOpened = false;
+                        scope.$apply();
+                    }
+                });
+            }
         }
     };
 }]);
