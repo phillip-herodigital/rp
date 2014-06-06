6cc1c6b2c7df74904b3aea2f72f1d46da0ee9a26
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-06-06 12:47:44 -0400

    More work on the Verify User state - needs to actually do the processing, though.

--- StreamEnergy.DomainModel/DomainModels/Accounts/ResetPassword/VerifyUserState.cs
+++ StreamEnergy.DomainModel/DomainModels/Accounts/ResetPassword/VerifyUserState.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,39 @@ namespace StreamEnergy.DomainModels.Accounts.ResetPassword
             this.container = container;
         }
 
-        public override IEnumerable<System.ComponentModel.DataAnnotations.ValidationResult> AdditionalValidations(ResetPasswordContext context, object internalContext)
+        public override IEnumerable<ValidationResult> AdditionalValidations(ResetPasswordContext context, object internalContext)
         {
             var profile = UserProfile.Locate(container, context.Username);
 
+            if (!profile.ChallengeQuestions.Select(q => q.QuestionKey).OrderBy(guid => guid).SequenceEqual(context.ChallengeQuestions.Where(q => !string.IsNullOrEmpty(q.Value)).Select(q => q.Key).OrderBy(guid => guid)))
+            {
+                yield return new ValidationResult("All Questions Required", new[] { "ChallengeQuestions" });
+            }
+            if ((from answer in context.ChallengeQuestions
+                 join originalResponse in profile.ChallengeQuestions on answer.Key equals originalResponse.QuestionKey
+                 select originalResponse.IsCorrect(answer.Value)).Any(isCorrect => !isCorrect))
+            {
+                yield return new ValidationResult("Incorrect Response", new[] { "ChallengeQuestions" });
+            }
             throw new NotImplementedException();
+        }
+
+        protected override Type InternalProcess(ResetPasswordContext context, object internalContext)
+        {
+            var passwordResetToken = GeneratePasswordResetToken();
+
+            // TODO - set the password reset token with expiration... somewhere
+
+            // TODO - send the email
+
+            return base.InternalProcess(context, internalContext);
+        }
+
+        private static string GeneratePasswordResetToken()
+        {
+            byte[] array = new byte[8];
+            new System.Security.Cryptography.RNGCryptoServiceProvider().GetBytes(array);
+            return Convert.ToBase64String(array);
         }
     }
 }
