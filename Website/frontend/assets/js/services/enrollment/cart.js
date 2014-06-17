35196931ce7839c590a7a815dcc43409bdc87ff5
Justin Myers <justin.myers@responsivepath.com>
2014-06-17 14:01:41 -0500

    First stab at shopping cart implementation

--- Website/frontend/assets/js/services/enrollment/cart.js
+++ Website/frontend/assets/js/services/enrollment/cart.js
@@ -1,12 +1,26 @@
 ngApp.factory('enrollmentCartService', ['enrollmentStepsService', 'utilityProductsService', 'accountInformationService', function (enrollmentStepsService, utilityProductsService, accountInformationService) {
 
 	return {
+		getPlans: utilityProductsService.getSelectedPlans,
+		/**
+		 * Return the number of items in the cart
+		 * @return {[type]} [description]
+		 */
 		getCartCount: function() {
 			return utilityProductsService.addresses.length;
 		},
+		/**
+		 * Currently only offering utilityServices so we're simply returning addresses
+		 * Eventually return all products here
+		 * @return {[type]} [description]
+		 */
 		getCartItems: function() {
-			return utilityProductsService.getSelectedPlansByLocation;
+			return utilityProductsService.addresses;
 		},
+		/**
+		 * Return the total cost of all cart items
+		 * @return {[type]} [description]
+		 */
 		getCartTotal: function() {
 
 		}
