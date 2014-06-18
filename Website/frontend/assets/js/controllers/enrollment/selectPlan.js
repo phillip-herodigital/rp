d0991ea08acd994dcf42680bfbe707be2e3f5176
Brandon Quintana <bquintana@brandonquintanaconsulting.com>
2014-06-18 12:30:51 -0700

    Styling slider.

--- Website/frontend/assets/js/controllers/enrollment/selectPlan.js
+++ Website/frontend/assets/js/controllers/enrollment/selectPlan.js
@@ -3,13 +3,32 @@
  * This is used to control aspects of the select plan on enrollment page.
  */
 ngApp.controller('SelectPlanCtrl', ['$scope', '$rootScope', 'enrollmentService', function ($scope, $rootScope, enrollmentService) {
-    $scope.slider1 = {
-        'options': {
-            start: function (event, ui) { $log.info('Slider start'); },
-            stop: function (event, ui) { $log.info('Slider stop'); }
-    $scope.slider2 = {
-	'options': {
-        start: function (event, ui) { $log.info('Slider start'); },
-        stop: function (event, ui) { $log.info('Slider stop'); }
+    $scope.sliderValues = {
+        slider1: '',
+        slider2: ''
+    };
 
+    $scope.sliders = [
+        {
+            'options': {
+                min: 0,
+                max: 3,
+                step: 1,
+                range: 'min',
+                slide: function (event, ui) {
+                    console.log(ui);
+                }
+            }
+        },{
+            'options': {
+                min: 0,
+                max: 3,
+                step: 1,
+                range: 'min',
+                slide: function (event, ui) {
+
+                }
+            }
+        }
+    ];
 }]);
