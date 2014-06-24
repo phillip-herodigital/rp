2cb74bbc84ba9525e654e78b43acaad61f127026
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-06-24 13:42:19 -0400

    Add a scroll anchor directive

---
+++ Website/frontend/assets/js/directives/scroll-anchor.js
@@ -0,0 +1,21 @@
+﻿ngApp.directive('scrollAnchor', ['jQuery', '$window', '$timeout', '$parse', function (jQuery, window, $timeout, $parse) {
+    // angular's $window is not a jqLite or jQuery window - it is the actual window object.
+    $window = jQuery(window);
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            // element is already a jQuery object, so we can just use the jQuery functions automatically
+            var scrollTop;
+            var anchorTop;
+
+            var model = $parse(attrs.scrollAnchor);
+            model.assign(scope, function () {
+                scrollTop = $window.scrollTop();
+                anchorTop = element.offset().top;
+                $timeout(function () {
+                    $window.scrollTop(scrollTop - anchorTop + element.offset().top);
+                }, 0, false);
+            });
+        }
+    };
+}]);
