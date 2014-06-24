0420bfbe0615236277d33f07dc03d79a6201743f
Adam Powell <adam.powell@responsivepath.com>
2014-06-24 16:43:24 -0700

    Added modal for removing enrolled account

--- Website/frontend/assets/js/controllers/account/enrolledAccounts.js
+++ Website/frontend/assets/js/controllers/account/enrolledAccounts.js
@@ -13,33 +13,14 @@ ngApp.controller('AcctEnrolledAccountsCtrl', ['$scope', '$rootScope', '$http', '
 		});
 	});
 
-	$scope.open = function (accountNumber, modalTitle, modalBody, cancelButton, removeButton ) {
-		/*
-        var modalInstance = modalFactory.open({
-            //templateUrl: 'myModalContent.html',
-            controller: 'AcctEnrolledAccountsCtrl',
-            modalContent: {
-                title: modalTitle,
-                body: modalBody,
-                footer: '<button class="secondary" ng-click="close()">' + cancelButton + '</button>' +
-                	'    <button ng-click="removeEnrolledAccount(' + accountNumber +')">' + removeButton + '</button>'
-            },
-            resolve: {
-                data: function () {
-                    return {
-                        items: $scope.items
-                    };
-                }
-            }
-        });
-		*/
-    };
+	$scope.open = function (accountNumber) {
+		$scope.currentAccount = accountNumber;
 
-    $scope.close = function (result) {
-        //$modalInstance.close(result);
-        $rootScope.modalInstance.dismiss('Cancel');
-    };
-
+		var modalInstance = modalFactory.open({
+			templateUrl: 'removeAccount.html',
+			scope: $scope
+		});	
+	};
 
 	// remove an enrolled account
 	$scope.removeEnrolledAccount = function (accountNumber) {
@@ -47,28 +28,25 @@ ngApp.controller('AcctEnrolledAccountsCtrl', ['$scope', '$rootScope', '$http', '
 		var requestData = {};
 		requestData.accountNumber = accountNumber;
 
-		if (confirm('Are you sure that you want to remove this account?')){
-			// sent the post
-			$http({
-				method  : 'POST',
-				url     : '/api/account/removeEnrolledAccount',
-				data    : requestData,
-				headers : { 'Content-Type': 'application/JSON' } 
-			})
-				.success(function (data, status, headers, config) {
-					if (!data.success) {
-				        // if not successful, show an error
+		$http({
+			method  : 'POST',
+			url     : '/api/account/removeEnrolledAccount',
+			data    : requestData,
+			headers : { 'Content-Type': 'application/JSON' } 
+		})
+			.success(function (data, status, headers, config) {
+				if (!data.success) {
+					// if not successful, show an error
 
-					} else {
-						// if successful, remove the row
-						for (var i = 0, len = $scope.formData.enrolledAccounts.length; i < len; i += 1) {
-					        if ($scope.formData.enrolledAccounts[i] && $scope.formData.enrolledAccounts[i].accountNumber === accountNumber) {
-					            $scope.formData.enrolledAccounts.splice(i, 1);
-					        }
-					    } 
-					}
-				});
-		}	
+				} else {
+					// if successful, remove the row
+					for (var i = 0, len = $scope.formData.enrolledAccounts.length; i < len; i += 1) {
+						if ($scope.formData.enrolledAccounts[i] && $scope.formData.enrolledAccounts[i].accountNumber === accountNumber) {
+							$scope.formData.enrolledAccounts.splice(i, 1);
+						}
+					} 
+				}
+			});
 	};
 
 	// send letter of residency
@@ -86,7 +64,7 @@ ngApp.controller('AcctEnrolledAccountsCtrl', ['$scope', '$rootScope', '$http', '
 		})
 			.success(function (data, status, headers, config) {
 				if (!data.success) {
-			        // if not successful, show an error
+					// if not successful, show an error
 
 				} else {
 					// if successful, alert the user
