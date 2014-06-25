c83a1e63b54c24445e3fbc6ea26a7e80e988520b
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-06-25 15:14:29 -0400

    Add a DataController for sitecore taxonomies. Switch the usStates variable to use data from DataController.

---
+++ Website/Controllers/DataController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreamEnergy.MyStream.Controllers
+{
+    [ChildActionOnly]
+    public class DataController : Controller
+    {
+
+        public ActionResult States()
+        {
+            var item = Sitecore.Context.Database.GetItem("/sitecore/content/Data/Taxonomy/State Abbreviations/State Abbreviations");
+            var nameValues = ((Sitecore.Data.Fields.NameValueListField)item.Fields["State Abbreviations"]).NameValues;
+
+            var data = nameValues.AllKeys.Select(key => new { abbreviation = key, display = nameValues[key] });
+
+            return this.Content(StreamEnergy.Json.Stringify(data));
+        }
+
+    }
+}
