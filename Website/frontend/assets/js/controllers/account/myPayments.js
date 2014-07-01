6c483e93f74118a996d69bb9b9a55de2c33fc2aa
Adam Powell <adam.powell@responsivepath.com>
2014-07-01 15:33:20 -0700

    Created dynamic filters

--- Website/frontend/assets/js/controllers/account/myPayments.js
+++ Website/frontend/assets/js/controllers/account/myPayments.js
@@ -2,59 +2,34 @@
  *
  */
 ngApp.controller('AcctMyPaymentsCtrl', ['$scope', '$rootScope', '$http', '$filter', '$timeout', '$sce', 'jQuery', function ($scope, $rootScope, $http, $filter, $timeout, $sce, jQuery) {
-	// create a blank object to hold the information
+	// create  blank objects to hold the information
 	$scope.paymentsTable = {};
 	$scope.paymentsTable.columnList = [];
 	$scope.paymentsTable.values = [];
 	$scope.isLoading = true;
 	$scope.showExpand = true;
+	$scope.filters = {};
+	$scope.filtersList = {};
+	$scope.filtersList.serviceType = [];
+	$scope.filtersList.accountNumber = [];
+	$scope.filtersList.status = [];
 
 	$timeout(function() {
 		$http.get('/api/account/getPayments').success(function (data, status, headers, config) {
 			$scope.paymentsTable = data.payments;
 			$scope.paymentsTableOriginal = angular.copy($scope.paymentsTable);
+
+			// filters
+			$scope.serviceTypes = _.pluck(_.uniq($scope.paymentsTable.values, 'serviceType'),'serviceType');
+			$scope.accountNumbers = _.pluck(_.uniq($scope.paymentsTable.values, 'accountNumber'),'accountNumber');
+			$scope.statuses = _.pluck(_.uniq($scope.paymentsTable.values, 'status'),'status');
+			_.forEach($scope.serviceTypes,function(type) { $scope.filtersList.serviceType.push({ 'name' : type, 'value' : type }) }); 
+			_.forEach($scope.accountNumbers,function(num) { $scope.filtersList.accountNumber.push({ 'name' : num, 'value' : num }) }); 
+			_.forEach($scope.statuses,function(status) { $scope.filtersList.status.push({ 'name' : status, 'value' : status }) }); 
+			
 			$scope.isLoading = false;
 		});
 	}, 800);
-
-	// filters
-	$scope.filters = {};
-	$scope.filtersList = {
-		"serviceType": [
-			{
-				"name": "HomeLife Services",
-				"value": "HomeLife Services"
-			},
-			{
-				"name": "Utility",
-				"value": "Utility"
-			}
-		],
-		"accountNumber": [
-			{
-				"name": "1197015532",
-				"value": "1197015532"
-			},
-			{
-				"name": "219849302",
-				"value": "219849302"
-			},
-			{
-				"name": "194829927",
-				"value": "194829927"
-			}
-		],
-		"isPaid": [
-			{
-				"name": "Paid",
-				"value": true
-			},
-			{
-				"name": "Unpaid",
-				"value": false
-			}
-		]
-	};
 
 	// methods
 	$scope.resetFilters = function() {
