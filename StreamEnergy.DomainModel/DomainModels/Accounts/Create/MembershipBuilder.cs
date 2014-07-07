3a42c3aea812fa0a8867271a494af5d93d71a7e2
Matt DeKrey <matt.dekrey@responsivepath.com>
2014-07-07 16:10:53 -0400

    Refactor membership building to share with enrollments.

---
+++ StreamEnergy.DomainModel/DomainModels/Accounts/Create/MembershipBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+using Microsoft.Practices.Unity;
+
+namespace StreamEnergy.DomainModels.Accounts.Create
+{
+    public class MembershipBuilder
+    {
+        private readonly MembershipProvider membershipProvider;
+        private readonly IUnityContainer unityContainer;
+
+        public MembershipBuilder(MembershipProvider membershipProvider, IUnityContainer unityContainer)
+        {
+            this.membershipProvider = membershipProvider;
+            this.unityContainer = unityContainer;
+        }
+
+        internal UserProfile CreateUser(string username, string password, Dictionary<Guid, string> challengeAnswers = null)
+        {
+            var user = Membership.CreateUser(username, password);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            var profile = UserProfile.Locate(unityContainer, username);
+            profile.ChallengeQuestions = (from entry in challengeAnswers ?? new Dictionary<Guid, string>()
+                                          select ChallengeResponse.Create(entry.Key, entry.Value)).ToArray();
+
+            profile.Save();
+
+            return profile;
+        }
+    }
+}
