e997805392f303cd23e427a7cdc31a55072aee7c
Matt DeKrey <mattdekrey@gmail.com>
2014-07-10 12:32:24 -0400

    More work on Make a Payment display.

--- Website/frontend/assets/js/controllers/account/makePayment.js
+++ Website/frontend/assets/js/controllers/account/makePayment.js
@@ -3,6 +3,18 @@
  */
 ngApp.controller('MakePaymentCtrl', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
 
+    var ctrl = this;
+    this.invoices = null;
+    this.paymentMethods = null;
+    this.selectedAccounts = [];
+    this.total = 0;
+
+    $scope.$watch(function () { return _.pluck(ctrl.invoices.values, 'selected'); }, function (newValue) {
+        ctrl.selectedAccounts = _.where(ctrl.invoices.values, { 'selected': true, 'canMakeOneTimePayment': true });
+        ctrl.total = _.reduce(ctrl.selectedAccounts, function (a, b) { return a + parseFloat(b.invoiceAmount); }, 0);
+        ctrl.paymentAmount = ctrl.total;
+    }, true);
+
     // Disable weekends selection
     $scope.disableWeekends = function (date, mode) {
         return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
