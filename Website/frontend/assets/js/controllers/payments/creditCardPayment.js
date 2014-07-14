ed0806f82ce42645b778208721e8ddc5b182a49b
Matt DeKrey <mattdekrey@gmail.com>
2014-07-14 10:23:33 -0400

    Some work on the Tokenized Card stuff.

--- Website/frontend/assets/js/controllers/payments/creditCardPayment.js
+++ Website/frontend/assets/js/controllers/payments/creditCardPayment.js
@@ -1,3 +1,29 @@
 ﻿ngApp.controller('CreditCardPaymentCtrl', ['$scope', function ($scope) {
+    var ctrl = this;
+    this.tokenizedCard = function () {
+        return {
+            paymentType: 'TokenizedCard',
+            cardToken: ctrl.cardToken(),
+            expirationDate: new Date(ctrl.expirationYear, ctrl.expirationMonth),
+            billingZipCode: ctrl.billingZip,
+            securityCode: ctrl.securityCode
+        };
+    };
 
+    var currentYear = new Date().getFullYear();
+
+    $scope.$watch(function () { return ctrl.expirationYear }, function (newValue) {
+        if (newValue == currentYear) {
+            ctrl.expirationMonths = _.range(new Date().getMonth() + 1, 13).map(function (n) { return String("0" + n).slice(-2); });
+        } else {
+            ctrl.expirationMonths = _.range(1, 13).map(function (n) { return String("0" + n).slice(-2); });
+        }
+    });
+    $scope.$watch(function () { return ctrl.expirationMonth }, function (newValue) {
+        if (newValue && newValue <= new Date().getMonth()) {
+            ctrl.expirationYears = _.range(currentYear + 1, currentYear + 20);
+        } else {
+            ctrl.expirationYears = _.range(currentYear, currentYear + 20);
+        }
+    });
 }]);
---
+++ Website/frontend/assets/js/directives/tokenize-card.js
@@ -0,0 +1,34 @@
+﻿ngApp.directive('tokenizeCard', ['$http', function ($http) {
+    return {
+        require: 'ngModel',
+        link: function ($scope, element, attrs, ctrl) {
+
+            function getTokenizedCard()
+            {
+                var rawCard = ctrl.$viewValue;
+                // TODO - tokenize it
+                return rawCard;
+            }
+
+            ctrl.$parsers.push(function (inputValue) {
+                return getTokenizedCard;
+            });
+
+            ctrl.$formatters.push(function (modelValue) {
+                if (modelValue)
+                {
+                    if (typeof modelValue == 'function') {
+                        return ctrl.$viewValue;
+                    }
+                    // TODO - this should be the value of the token from the server... We should probably convert it to a redacted value
+                    console.log(ctrl);
+                    return modelValue;
+                }
+                else
+                {
+                    return '';
+                }
+            });
+        }
+    };
+}]);
