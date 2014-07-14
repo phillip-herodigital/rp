732249052385c0866052f11d2fdfcc1f1bb02443
Matt DeKrey <mattdekrey@gmail.com>
2014-07-14 12:22:50 -0400

    Work on the BankAccount payment method.

---
+++ Website/frontend/assets/js/controllers/payments/bankAccountPayment.js
@@ -0,0 +1,11 @@
+﻿ngApp.controller('BankAccountPaymentCtrl', ['$scope', function ($scope) {
+    var ctrl = this;
+    this.bankAccount = function () {
+        return {
+            paymentType: 'BankPaymentMethod',
+            category: ctrl.category,
+            routingNumber: ctrl.routingNumber,
+            accountNumber: ctrl.accountNumber
+        };
+    };
+}]);
