0578ecd33ffea494d5ad7f746a7d2a446d80d8a9
Adam Powell <adam.powell@responsivepath.com>
2014-07-14 16:10:19 -0700

    Added background to chart

--- Website/frontend/assets/js/controllers/account/myEnergyUsage.js
+++ Website/frontend/assets/js/controllers/account/myEnergyUsage.js
@@ -7,43 +7,27 @@ ngApp.controller('AcctMyEnergyUsageCtrl', ['$scope', '$rootScope', '$http', '$ti
 		lineMode: "cardinal",
 		tension: 0.7,
 		axes: {
-		x: {type: "linear", key: "month", 
-				labelFunction: function (v) {
-					switch (v) {
-						case 1: return 'JAN';
-						case 2: return 'FEB';
-						case 3: return 'MAR';
-						case 4: return 'APR';
-						case 5: return 'MAY';
-						case 6: return 'JUN';
-						case 7: return 'JUL';
-						case 8: return 'AUG';
-						case 9: return 'SEP';
-						case 10: return 'OCT';
-						case 11: return 'NOV';
-						case 12: return 'DEC';
-						default: return '';
+			x: {type: "linear", key: "month", 
+					labelFunction: function (v) {
+						switch (v) {
+							case 1: return 'JAN';
+							case 2: return 'FEB';
+							case 3: return 'MAR';
+							case 4: return 'APR';
+							case 5: return 'MAY';
+							case 6: return 'JUN';
+							case 7: return 'JUL';
+							case 8: return 'AUG';
+							case 9: return 'SEP';
+							case 10: return 'OCT';
+							case 11: return 'NOV';
+							case 12: return 'DEC';
+							default: return '';
+						}
 					}
-				}
-			},
-		y: {type: "linear", min: 0, max: 250,
-				labelFunction: function (v) {
-					if (v/20 % 2) {
-						return ""
-					} else {
-						return v;
-					}
-				}
-			},
-		y2: {type: "linear", min: 0, max: 100,
-				labelFunction: function (v) {
-					if (v/2 % 2) {
-						return ""
-					} else {
-						return v;
-					}
-				}
-			}
+				},
+			y: {type: "linear", min: 0, max: 250},
+			y2: {type: "linear", min: 0, max: 100}
 		},
 		drawLegend: false,
 		drawDots: true,
