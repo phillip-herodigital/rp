e509c4609336a8c4797cbf9529f0832c37dfe005
Matt DeKrey <mattdekrey@gmail.com>
2014-07-15 14:46:59 -0400

    Updates to currency input to allow > 1000 payments.

--- Website/frontend/assets/js/directives/currencyInput.js
+++ Website/frontend/assets/js/directives/currencyInput.js
@@ -1,16 +1,20 @@
 ngApp.directive('currencyInput', ['$filter', function($filter) {
     return {
-      require: '?ngModel',
-      link: function($scope, element, attrs, ctrl) {
+        require: '?ngModel',
+        link: function($scope, element, attrs, ctrl) {
 
-        ctrl.$formatters.push(function(inputValue) {
-          return $filter('currency')(inputValue, '');
-        });
+            ctrl.$formatters.push(function(inputValue) {
+                return $filter('currency')(inputValue, '');
+            });
 
-        element.bind('blur', function() {
-          ctrl.$setViewValue($filter('currency')(element[0].value, ''));
-          ctrl.$render();
-        });
-      }
+            ctrl.$parsers.push(function (viewValue) {
+                return viewValue.replace(/,/g, '');
+            });
+
+            element.bind('blur', function () {
+                ctrl.$setViewValue($filter('currency')(ctrl.$modelValue, ''));
+                ctrl.$render();
+            });
+        }
     };
 }]);
