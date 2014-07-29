0ab31e84a4ce09be4512d3de34e84e33ee45242b
Matt DeKrey <mattdekrey@gmail.com>
2014-07-29 14:22:10 -0400

    Ensure user is logged out on authentication page. Display different error message after multiple attempts.

--- Website/Models/Authentication/LoginRequest.cs
+++ Website/Models/Authentication/LoginRequest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
@@ -34,7 +35,29 @@ namespace StreamEnergy.MyStream.Models.Authentication
             }
             else if (!Membership.ValidateUser(Domain.AccountPrefix + Username, Password))
             {
-                yield return new ValidationResult("Error Text", new[] { "Username", "Password" });
+                string connectionString = Sitecore.Configuration.Settings.GetConnectionString("core");
+
+                using (var connection = new SqlConnection(connectionString))
+                using (var command = new SqlCommand(@"
+SELECT FailedPasswordAttemptCount
+FROM [MyStreamSitecore_Core].[dbo].[aspnet_Membership]
+JOIN [MyStreamSitecore_Core].[dbo].[aspnet_Users] ON [aspnet_Membership].UserId = [aspnet_Users].UserId
+WHERE UserName=@username
+", connection))
+                {
+                    connection.Open();
+                    command.Parameters.Add(new SqlParameter("@username", System.Data.SqlDbType.VarChar) { Value = Domain.AccountPrefix + Username });
+
+                    var count = Convert.ToInt32(command.ExecuteScalar());
+                    if (count >= 2)
+                    {
+                        yield return new ValidationResult("Repeated Error Text", new[] { "Username", "Password" });
+                    }
+                    else
+                    {
+                        yield return new ValidationResult("Error Text", new[] { "Username", "Password" });
+                    }
+                }
             }
         }
     }
