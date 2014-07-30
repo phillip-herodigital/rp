d9d1de4035e39f4d9db2f75f7ccf8fb5395992f6
Matt DeKrey <mattdekrey@gmail.com>
2014-07-30 09:43:44 -0400

    Add locked-out error messages.

--- Website/Models/Authentication/LoginRequest.cs
+++ Website/Models/Authentication/LoginRequest.cs
@@ -37,27 +37,39 @@ namespace StreamEnergy.MyStream.Models.Authentication
             }
             else if (!Membership.ValidateUser(Domain.AccountPrefix + Username, Password))
             {
-                string connectionString = Sitecore.Configuration.Settings.GetConnectionString("core");
+                var user = Membership.GetUser(Domain.AccountPrefix + Username);
+                if (user.IsLockedOut)
+                {
+                    yield return new ValidationResult("Soft Locked Out Error Text", new[] { "Username", "Password" });
+                }
+                else if (!user.IsApproved)
+                {
+                    yield return new ValidationResult("Hard Locked Out Error Text", new[] { "Username", "Password" });
+                }
+                else
+                {
+                    string connectionString = Sitecore.Configuration.Settings.GetConnectionString("core");
 
-                using (var connection = new SqlConnection(connectionString))
-                using (var command = new SqlCommand(@"
+                    using (var connection = new SqlConnection(connectionString))
+                    using (var command = new SqlCommand(@"
 SELECT FailedPasswordAttemptCount
 FROM [MyStreamSitecore_Core].[dbo].[aspnet_Membership]
 JOIN [MyStreamSitecore_Core].[dbo].[aspnet_Users] ON [aspnet_Membership].UserId = [aspnet_Users].UserId
 WHERE UserName=@username
 ", connection))
-                {
-                    connection.Open();
-                    command.Parameters.Add(new SqlParameter("@username", System.Data.SqlDbType.VarChar) { Value = Domain.AccountPrefix + Username });
+                    {
+                        connection.Open();
+                        command.Parameters.Add(new SqlParameter("@username", System.Data.SqlDbType.VarChar) { Value = Domain.AccountPrefix + Username });
 
-                    var count = Convert.ToInt32(command.ExecuteScalar());
-                    if (count >= 2)
-                    {
-                        yield return new ValidationResult("Repeated Error Text", new[] { "Username", "Password" });
-                    }
-                    else
-                    {
-                        yield return new ValidationResult("Error Text", new[] { "Username", "Password" });
+                        var count = Convert.ToInt32(command.ExecuteScalar());
+                        if (count >= 2)
+                        {
+                            yield return new ValidationResult("Repeated Error Text", new[] { "Username", "Password" });
+                        }
+                        else
+                        {
+                            yield return new ValidationResult("Error Text", new[] { "Username", "Password" });
+                        }
                     }
                 }
             }
