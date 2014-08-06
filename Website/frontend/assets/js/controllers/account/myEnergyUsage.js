ef5daf60604ccde64b288eacea545b25a6371179
Adam Powell <adam.powell@responsivepath.com>
2014-08-06 16:16:32 -0700

    Fix for issure SMYS-383

--- Website/frontend/assets/js/controllers/account/myEnergyUsage.js
+++ Website/frontend/assets/js/controllers/account/myEnergyUsage.js
@@ -5,48 +5,55 @@ ngApp.controller('AcctMyEnergyUsageCtrl', ['$scope', '$rootScope', '$http', 'bre
 	// set the graph options
 	var monthNames = ['','January','February','March','April','May','June','July','August','September','October','November','December']
 	var monthAbbreviations = ['','JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
-	$scope.options = {
-		lineMode: 'cardinal',
-		tension: 0.7,
-		axes: {
-			x: {type: 'linear', key: 'month', 
-					labelFunction: function (v) {
-						return monthAbbreviations[v];
+	var setOptions = function (utilityType) {
+		$scope.options = {
+			lineMode: 'cardinal',
+			tension: 0.7,
+			axes: {
+				x: {type: 'linear', key: 'month', 
+						labelFunction: function (v) {
+							return monthAbbreviations[v];
+						}
+					},
+				y: {type: 'linear', min: 0}//,
+				//y2: {type: 'linear', min: 0, max: 100}
+			},
+			drawLegend: false,
+			drawDots: true,
+			series: [
+				{
+					y: 'usage',
+					label: 'Usage',
+					type: 'column',
+					color: '#77bd43',
+					axis: 'y',
+					visible: true
+				}/*,
+				{
+					y: 'gas',
+					label: 'Gas',
+					color: '#2a93c7',
+					axis: 'y2',
+					type: 'line',
+					thickness: '3px',
+					visible: true
+				}*/
+			],
+			tooltip: {mode: 'scrubber', interpolate: false,
+				formatter: function (x, y, series) {
+					var month = monthNames[x];
+					var units = (utilityType == 'Electric') ? 'kWh' : 'Therms';
+					var tooltip = y + ' ' + units;
+					if (breakpoint.breakpoint.name != 'phone') {
+							tooltip = month + ' ' + series.label + ': ' + tooltip;
 					}
-				},
-			y: {type: 'linear', min: 0}//,
-			//y2: {type: 'linear', min: 0, max: 100}
-		},
-		drawLegend: false,
-		drawDots: true,
-		series: [
-			{
-				y: 'usage',
-				label: 'Usage',
-				type: 'column',
-				color: '#77bd43',
-				axis: 'y',
-				visible: true
-			}/*,
-			{
-				y: 'gas',
-				label: 'Gas',
-				color: '#2a93c7',
-				axis: 'y2',
-				type: 'line',
-				thickness: '3px',
-				visible: true
-			}*/
-		],
-		tooltip: {mode: 'scrubber', interpolate: false,
-			formatter: function (x, y, series) {
-				var month = monthNames[x];
-				var units = (series.label == 'Electricity') ? 'kWh' : 'Therms';
-				var tooltip = month + ' ' + series.label + ': ' + y + ' ' + units;
-				return tooltip;
+					return tooltip;
+				}
 			}
-		}
-	};
+		};
+
+	}
+	
 
 	// get the account selector data
 	$http.get('/api/account/getAccounts').success(function (data, status, headers, config) {
@@ -55,7 +62,6 @@ ngApp.controller('AcctMyEnergyUsageCtrl', ['$scope', '$rootScope', '$http', 'bre
 		$scope.currentAccount = $scope.accounts[0];
 		$scope.currentSubAccount = $scope.currentAccount.subAccounts[0];
 		$scope.updateSelectedAccount($scope.currentAccount.accountNumber, $scope.currentAccount.subAccountLabel, $scope.currentSubAccount);
-
 	});
 
 	$scope.updateSelectedAccount = function(accountNumber, subAccountLabel, subaccount) {
@@ -80,7 +86,7 @@ ngApp.controller('AcctMyEnergyUsageCtrl', ['$scope', '$rootScope', '$http', 'bre
 				.success(function (data, status, headers, config) {
 					$scope.energyUsage = data.energyUsage;
 					// if the window is sized to mobie, only load 4 columns on the graph
-					if(breakpoint.breakpoint.name == 'phone') {
+					if (breakpoint.breakpoint.name == 'phone') {
 						$scope.energyUsage = $scope.energyUsage.slice(-4);
 						monthAbbreviations[8] = '';
 					}
@@ -89,6 +95,7 @@ ngApp.controller('AcctMyEnergyUsageCtrl', ['$scope', '$rootScope', '$http', 'bre
 					$scope.startMonth = monthNames[$scope.energyUsage[0].month];
 					$scope.endMonth = monthNames[$scope.energyUsage[lastItem].month];
 					$scope.endYear = $scope.energyUsage[lastItem].year;
+					setOptions(data.utilityType);
 					$scope.isLoading = false;
 				});
 		}
