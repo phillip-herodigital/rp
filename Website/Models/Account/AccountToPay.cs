073e27c09073986ca7c875c54b6b7ced20206f09
Justin Myers <justin.myers@responsivepath.com>
2014-08-12 16:48:18 -0500

    Updated Make a Payment table for responsive view

--- Website/Models/Account/AccountToPay.cs
+++ Website/Models/Account/AccountToPay.cs
@@ -16,10 +16,10 @@ namespace StreamEnergy.MyStream.Models.Account
         [ColumnSchema("Account Number", DeviceType.Phone)]
         public string AccountNumber { get; set; }
 
-        [ColumnSchema("Invoice Number", DeviceType.Tablet, DeviceType.Phone)]
+        [ColumnSchema("Invoice Number")]
         public string InvoiceAmount { get; set; }
 
-        [ColumnSchema("Due Date", DeviceType.Tablet, DeviceType.Phone)]
+        [ColumnSchema("Due Date")]
         public string DueDate { get; set; }
 
         public decimal AccountBalance { get; set; }
@@ -30,7 +30,7 @@ namespace StreamEnergy.MyStream.Models.Account
 
         public string UtilityProvider { get; set; }
 
-        [ColumnSchema("Details")]
+        [ColumnSchema("Details", DeviceType.Tablet, DeviceType.Phone)]
         public Dictionary<string, string> Actions { get; private set; }
 
     }
