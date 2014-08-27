221839625aa582923a1550524725db44171e689f
Justin Myers <justin.myers@responsivepath.com>
2014-08-27 18:13:45 -0500

    Added togglePane directive for complex hide/show functionality Updated subpage to use new togglePane directive Updated styles

---
+++ Website/frontend/assets/js/directives/togglePane.js
@@ -0,0 +1,18 @@
+
+/* 
+    Lets take any sort of elements in a list and allow them
+    to be toggleable
+*/
+ngApp.directive('togglePane', [function() {
+    return {
+        restrict: 'E',
+        scope: {
+            value: '='
+        },
+        transclude: true,
+        template: '<div class="pane-item" ng-show="value == name" ng-transclude></div>',
+        link: function(scope, element, attrs) {
+            scope.name = attrs.name;
+        }
+    };
+}]);
