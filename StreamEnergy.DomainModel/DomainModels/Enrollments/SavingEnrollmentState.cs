9f82552832f5c8118594503c063b785484643db3
Matt DeKrey <mattdekrey@gmail.com>
2014-09-08 14:00:35 -0400

    Log enrollment errors.

--- StreamEnergy.DomainModel/DomainModels/Enrollments/SavingEnrollmentState.cs
+++ StreamEnergy.DomainModel/DomainModels/Enrollments/SavingEnrollmentState.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StreamEnergy.Logging;
 using StreamEnergy.Processes;
 
 namespace StreamEnergy.DomainModels.Enrollments
