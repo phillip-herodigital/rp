1d62a8caee9fa0ab8d326ddd99391fcd9587bff8
Matt DeKrey <mattdekrey@gmail.com>
2014-09-09 15:04:23 -0400

    Add Maintenance Mode checks.

---
+++ Website/Conditions/MaintenanceModeCondition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Practices.Unity;
+using Sitecore.Rules.Conditions;
+
+namespace StreamEnergy.MyStream.Conditions
+{
+    public class MaintenanceModeCondition<T> : WhenCondition<T>
+        where T : Sitecore.Rules.RuleContext
+    {
+        private readonly Injection dependencies;
+
+        public class Injection
+        {
+            [Dependency]
+            public ISettings Settings { get; set; }
+        }
+        
+        public MaintenanceModeCondition()
+        {
+            dependencies = StreamEnergy.Unity.Container.Instance.Unity.Resolve<Injection>();
+        }
+
+        public MaintenanceModeCondition(Injection injectedValue)
+        {
+            dependencies = injectedValue;
+        }
+
+        protected override bool Execute(T ruleContext)
+        {
+            var isMaintenanceMode = !string.IsNullOrEmpty(dependencies.Settings.GetSettingsValue("Maintenance Mode", "Maintenance Mode"));
+
+            return isMaintenanceMode;
+        }
+    }
+}
