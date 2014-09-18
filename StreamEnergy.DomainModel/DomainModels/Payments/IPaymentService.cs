1e1a140520f28706a2cc4ec8a40063e9cf8548da
Matt DeKrey <mattdekrey@gmail.com>
2014-09-18 16:40:38 -0400

    Add payment service interface.

---
+++ StreamEnergy.DomainModel/DomainModels/Payments/IPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamEnergy.DomainModels.Payments
+{
+    public interface IPaymentService
+    {
+        Task<IEnumerable<SavedPaymentInfo>> GetSavedPaymentMethods(Guid globalCustomerId);
+        Task<Guid> SavePaymentMethod(Guid globalCustomerId, IPaymentInfo paymentInfo);
+        Task<bool> DeletePaymentMethod(Guid globalCustomerId, Guid paymentMethodId);
+    }
+}
