c8113ba5e269ae5b513b5b1247ed30edc55cd76f
Justin Myers <justin.myers@responsivepath.com>
2014-09-18 11:39:41 -0500

    Converting indentation to spaces for consistency

--- Website/frontend/assets/js/controllers/mobileEnrollment.js
+++ Website/frontend/assets/js/controllers/mobileEnrollment.js
@@ -2,89 +2,89 @@
     //Set the data in a service
     this.mobileService = mobileEnrollmentService;
 
-	//Choose Network Settings
-	this.phoneFilters = {
-		phoneTypeTab: "new",
-		zipCode: "",
-		selectedPhone: "",
-		selectedNetwork: "att",
-		condition: "",
-		brand: [],
-		os: []
-	};
+    //Choose Network Settings
+    this.phoneFilters = {
+        phoneTypeTab: "new",
+        zipCode: "",
+        selectedPhone: "",
+        selectedNetwork: "att",
+        condition: "",
+        brand: [],
+        os: []
+    };
 
-	this.phoneOptions = {
-		color: "",
-		size: "",
-		condition: "",
-		warranty: "",
-		number: ""
-	};
+    this.phoneOptions = {
+        color: "",
+        size: "",
+        condition: "",
+        warranty: "",
+        number: ""
+    };
 
-	this.displayFilters = false;
+    this.displayFilters = false;
 
-	//Set the default options when a new phone is selected
-	this.setSelectedPhone = function(item) {
-		this.phoneFilters.selectedPhone = item.name;
-		this.phoneOptions.color = item.colors[0].color;
-		this.phoneOptions.size = item.models[0].size;
-	};
-	
+    //Set the default options when a new phone is selected
+    this.setSelectedPhone = function(item) {
+        this.phoneFilters.selectedPhone = item.name;
+        this.phoneOptions.color = item.colors[0].color;
+        this.phoneOptions.size = item.models[0].size;
+    };
+    
     /* Change the ordering of the phones */
     this.setPhoneOrder = function(value) {
 
     };
 
     this.clearPhoneSelection = function() {
-    	this.phoneFilters.selectedPhone = "";
-		this.phoneOptions = {
-			color: "",
-			size: "",
-			condition: "",
-			warranty: "",
-			number: ""
-		};
+        this.phoneFilters.selectedPhone = "";
+        this.phoneOptions = {
+            color: "",
+            size: "",
+            condition: "",
+            warranty: "",
+            number: ""
+        };
     };
 
     this.toggleBrands = function(brand) {
-	    var idx = this.phoneFilters.brand.indexOf(brand);
+        var idx = this.phoneFilters.brand.indexOf(brand);
 
-	    if (idx > -1) {
-	    	this.phoneFilters.brand.splice(idx, 1);
-	    } else {
-	    	this.phoneFilters.brand.push(brand);
-	    }
-  	};
+        if (idx > -1) {
+            this.phoneFilters.brand.splice(idx, 1);
+        } else {
+            this.phoneFilters.brand.push(brand);
+        }
+    };
 
      this.toggleOSs = function(os) {
-	    var idx = this.phoneFilters.os.indexOf(os);
+        var idx = this.phoneFilters.os.indexOf(os);
 
-	    if (idx > -1) {
-	    	this.phoneFilters.os.splice(idx, 1);
-	    } else {
-	    	this.phoneFilters.os.push(os);
-	    }
-  	};
+        if (idx > -1) {
+            this.phoneFilters.os.splice(idx, 1);
+        } else {
+            this.phoneFilters.os.push(os);
+        }
+    };
 
 }]);
 
 ngApp.filter('phoneFilter', function() {
   return function(phones, filters) {
-	//Filter by brand, OS, Condition
-	var filteredElements = phones;
-	
-	if(filters.brand.length) {
-		filteredElements =  _.filter(filteredElements, function(item) {
-			return _.contains(filters.brand, item.brand);
-		});
-	}
+    //Filter by brand, OS, Condition
+    var filteredElements = phones;
+    
+    if(filters.brand.length) {
+        filteredElements =  _.filter(filteredElements, function(item) {
+            return _.contains(filters.brand, item.brand);
+        });
+    }
 
-	if(filters.os.length) {
-		filteredElements =  _.filter(filteredElements, function(item) {
-			return _.contains(filters.os, item.os);
-		});
-	}
+    if(filters.os.length) {
+        filteredElements =  _.filter(filteredElements, function(item) {
+            return _.contains(filters.os, item.os);
+        });
+    }
 
-	return filteredElements;
+    return filteredElements;
   }
 });
--- Website/frontend/assets/js/services/mobileEnrollment.js
+++ Website/frontend/assets/js/services/mobileEnrollment.js
@@ -1,9 +1,9 @@
 ngApp.factory('mobileEnrollmentService', ['$rootScope', function ($rootScope) {
-	var service = {},
-		mobileNetworks = [],
-		phones = [];
+    var service = {},
+        mobileNetworks = [],
+        phones = [];
 
-	service.setNetworkData = function (data) {
+    service.setNetworkData = function (data) {
         mobileNetworks = data;
     };
 
@@ -12,51 +12,51 @@ ngApp.factory('mobileEnrollmentService', ['$rootScope', function ($rootScope) {
     };
 
     service.getPhones = function() {
-    	return phones;
+        return phones;
     }
 
     service.getNetworks = function() {
-    	return mobileNetworks;
+        return mobileNetworks;
     }
 
-	service.getItemPrice = function(priceObject) {
-		//I'm assuming we always want the new price if it exists
-		if(_.where(priceObject, { condition: "New" })) {
-			return _.where(priceObject, { condition: "New" })[0].value;
-		} else {
-			return _.where(priceObject, { condition: "Refurbished" })[0].value;
-		}
-	};
+    service.getItemPrice = function(priceObject) {
+        //I'm assuming we always want the new price if it exists
+        if(_.where(priceObject, { condition: "New" })) {
+            return _.where(priceObject, { condition: "New" })[0].value;
+        } else {
+            return _.where(priceObject, { condition: "Refurbished" })[0].value;
+        }
+    };
 
-	service.getPricesForSize = function(item, size) {
-		return _.where(item.models, { size: size });
-	};
+    service.getPricesForSize = function(item, size) {
+        return _.where(item.models, { size: size });
+    };
 
-	service.getConditionPrice = function(item, condition) {
-		var models =  _.where(item.models, { condition: condition });
+    service.getConditionPrice = function(item, condition) {
+        var models =  _.where(item.models, { condition: condition });
 
-		//Return the lowest price for the condition
-		return _.min(models, function(model){ return model.price; }).price;
-	};
+        //Return the lowest price for the condition
+        return _.min(models, function(model){ return model.price; }).price;
+    };
 
-	service.getPhoneSizes = function(item) {
-		var sizes =  _.uniq(_.pluck(item.models, 'size'));
-		return _.sortBy(sizes, function(size) {
-			return size;
-		})
-	};
+    service.getPhoneSizes = function(item) {
+        var sizes =  _.uniq(_.pluck(item.models, 'size'));
+        return _.sortBy(sizes, function(size) {
+            return size;
+        })
+    };
 
-	service.getItemConditions = function(priceObject) {
-		return _.pluck(priceObject, 'condition');
-	};
+    service.getItemConditions = function(priceObject) {
+        return _.pluck(priceObject, 'condition');
+    };
 
-	service.getBrands = function() {
-		return _.uniq(_.pluck(phones, "brand"));
-	};
+    service.getBrands = function() {
+        return _.uniq(_.pluck(phones, "brand"));
+    };
 
-	service.getOSs = function() {
-		return _.uniq(_.pluck(phones, "os"));
-	};
+    service.getOSs = function() {
+        return _.uniq(_.pluck(phones, "os"));
+    };
 
     return service;
 }]);
