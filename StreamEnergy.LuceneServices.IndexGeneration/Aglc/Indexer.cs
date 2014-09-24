91b68f3853b2346c20ba52148a4ae7120883dde4
Matt DeKrey <mattdekrey@gmail.com>
2014-09-24 14:41:27 -0400

    More work on the AGLC typeahead

--- StreamEnergy.LuceneServices.IndexGeneration/Aglc/Indexer.cs
+++ StreamEnergy.LuceneServices.IndexGeneration/Aglc/Indexer.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StreamEnergy.DomainModels;
+using StreamEnergy.DomainModels.Enrollments;
 using SmartyStreets = StreamEnergy.Services.Clients.SmartyStreets;
 
 namespace StreamEnergy.LuceneServices.IndexGeneration.Aglc
@@ -31,6 +33,8 @@ namespace StreamEnergy.LuceneServices.IndexGeneration.Aglc
             public const string PremiseTypeCommercial = "B";
             public const string PremiseTypeResidential = "I";
             public static readonly ColumnDefinition AglAccountNumber = new ColumnDefinition { Start = 57, Length = 25 };
+            public static readonly ColumnDefinition ZipCode = new ColumnDefinition { Start = 148, Length = 5 };
+            public static readonly ColumnDefinition City = new ColumnDefinition { Start = 127, Length = 19 };
         }
 
         public Indexer(int reportEvery, int maxTasks)
@@ -42,6 +46,7 @@ namespace StreamEnergy.LuceneServices.IndexGeneration.Aglc
 
         public async Task AddAddresses(Options options, IndexBuilder indexBuilder, SmartyStreets.SmartyStreetService streetService)
         {
+            var zipCodes = new HashSet<string>();
             int counter = 0;
             using (var reader = new System.IO.StreamReader(Path.Combine(options.Source, "custdata.txt")))
             {
@@ -55,9 +60,28 @@ namespace StreamEnergy.LuceneServices.IndexGeneration.Aglc
                             continue;
 
                         var aglAccountNumber = Configuration.AglAccountNumber.GetValue(entry);
+                        var city = Configuration.City.GetValue(entry);
+                        var zipCode = Configuration.ZipCode.GetValue(entry);
 
+                        if (!zipCodes.Contains(zipCode))
+                        {
+                            await WriteZipCodeLocation(indexBuilder, customerType.Value, zipCode);
+                            zipCodes.Add(zipCode);
+                        }
 
+                        var resultLocation = new Location
+                        {
+                            Address = new Address 
+                            {
+                                // TODO
+                                City = city,
+                                StateAbbreviation = "GA", 
+                                PostalCode5 = zipCode 
+                            },
+                            Capabilities = GetServiceCapabilities(aglAccountNumber, zipCode)
+                        };
 
+                        await indexBuilder.WriteLocation(resultLocation, customerType.Value, "AGLC", true);
                         counter++;
                         if (counter % reportEvery == 0)
                             Console.WriteLine(counter.ToString().PadLeft(11));
@@ -66,7 +90,32 @@ namespace StreamEnergy.LuceneServices.IndexGeneration.Aglc
             }
         }
 
-        private DomainModels.Enrollments.EnrollmentCustomerType? GetCustomerType(string line)
+        private Task<bool> WriteZipCodeLocation(IndexBuilder indexBuilder, EnrollmentCustomerType customerType, string zipCode)
+        {
+            return indexBuilder.WriteLocation(new Location
+            {
+                Address = new Address
+                {
+                    StateAbbreviation = "GA",
+                    PostalCode5 = zipCode
+                },
+                Capabilities = GetServiceCapabilities(null, zipCode)
+            }, customerType, "AGLC", true);
+        }
+
+        private IEnumerable<DomainModels.IServiceCapability> GetServiceCapabilities(string aglAccountNumber, string zipCode)
+        {
+            return new IServiceCapability[]
+                {
+                    new GeorgiaGasServiceCapability
+                    {
+                        AglAccountNumber = aglAccountNumber,
+                        Zipcode = zipCode
+                    }
+                };
+        }
+
+        private EnrollmentCustomerType? GetCustomerType(string line)
         {
             switch (Configuration.PremiseType.GetValue(line))
             {
