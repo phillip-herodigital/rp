a5254fe2bd455c9d533adce8011ea0fe6349ec2f
Adam Brill <adambrill@gmail.com>
2014-10-09 09:57:59 -0700

    Updated the cmd to be the correct path

--- Website/installStreamEnergy.RenderingService.cmd
+++ Website/installStreamEnergy.RenderingService.cmd
@@ -1,3 +1,3 @@
 IF "%Install.StreamEnergy.Services%" == "true" (
-   C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe .\bin\StreamEnergy.RenderingService.exe
+   D:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe .\bin\StreamEnergy.RenderingService.exe
 )
