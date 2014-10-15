e48387527521fb55b642fc3ed84b5e0335c5e1a9
Adam Brill <adambrill@gmail.com>
2014-10-15 16:03:47 -0700

    Removed the collapsible from the log viewer

--- Website/frontend/assets/js/controllers/debugging/logViewer.js
+++ Website/frontend/assets/js/controllers/debugging/logViewer.js
@@ -1,15 +1,15 @@
 ﻿ngApp.controller('LogViewerCtrl', ['$scope', '$window', function ($scope, $window) {
     $scope.logs = [];
-    $scope.expand = [];
+    //$scope.expand = [];
     $scope.form = {};
     $scope.setServerData = function (data, sessionID) {
         $scope.logs = data;
         $scope.form.sessionID = sessionID;
     }
 
-    $scope.expandInnerTable = function (index) {
+    /*$scope.expandInnerTable = function (index) {
         $scope.expand[index] = !$scope.expand[index];
-    }
+    }*/
     $scope.update = function () {
         $window.location.href = '?sessionID=' + $scope.form.sessionID;
     }
