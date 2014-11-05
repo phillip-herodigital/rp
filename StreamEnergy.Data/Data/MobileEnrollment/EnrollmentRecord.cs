709a2ef06ecab535fbfc7bff1642ef4db56e1000
Matt DeKrey <mattdekrey@gmail.com>
2014-11-05 13:43:29 -0500

    Correct git includes

---
+++ StreamEnergy.Data/Data/MobileEnrollment/EnrollmentRecord.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using StreamEnergy.DomainModels;
+
+namespace StreamEnergy.Data.MobileEnrollment
+{
+    public class EnrollmentRecord
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name Required")]
+        public Name Name { get; set; }
+
+        [Required(ErrorMessage = "Phone Required")]
+        public TypedPhone Phone { get; set; }
+
+        [Required(ErrorMessage = "Email Required")]
+        public Email Email { get; set; }
+
+        [Required(ErrorMessage = "Billing Address Required")]
+        public Address BillingAddress { get; set; }
+
+        [Required(ErrorMessage = "Shipping Address Required")]
+        public Address ShippingAddress { get; set; }
+
+        [Required(ErrorMessage = "Business Address Required")]
+        public Address BusinessAddress { get; set; }
+
+        [Required]
+        public string BusinessInformationName { get; set; }
+
+        public string BusinessName { get; set; }
+        [Required]
+        public W9BusinessClassification TaxClassification { get; set; }
+        public string AdditionalTaxClassification { get; set; }
+        [Required]
+        public string ExemptCode { get; set; }
+        [Required]
+        public string FatcaCode { get; set; }
+        [Required]
+        public bool BusinessAddressSame { get; set; }
+
+        public string CurrentAccountNumbers { get; set; }
+        [Required]
+        public DateTimeOffset CustomerCertification { get; set; }
+        [Required]
+        public string CustomerSignature { get; set; }
+        [Required]
+        public bool SignatureConfirmation { get; set; }
+        [Required]
+        public string SignatoryName { get; set; }
+        [Required]
+        public string SignatoryRelation { get; set; }
+
+        [RequireValue(true, ErrorMessage = "Must Agree To Terms")]
+        public DateTimeOffset AgreeToTerms { get; set; }
+
+        public bool TcpaPreference { get; set; }
+
+        public byte[] PdfGen { get; set; }
+
+        public string AssociateId { get; set; }
+        public string SourceId { get; set; }
+        public string OrderId { get; set; }
+    }
+}
---
+++ StreamEnergy.Data/Data/MobileEnrollment/MobileEnrollmentService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StreamEnergy.DomainModels;
+using StreamEnergy.DomainModels.MobileEnrollment;
+
+namespace StreamEnergy.Data.MobileEnrollment
+{
+    class MobileEnrollmentService : IMobileEnrollmentService
+    {
+        private readonly DataContext dataContext;
+
+        public MobileEnrollmentService(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+
+        async Task<Guid> IMobileEnrollmentService.RecordEnrollment(UserContext data, byte[] w9Pdf)
+        {
+            var record = new EnrollmentRecord
+                {
+                    Name = data.ContactInfo.Name,
+                    Phone = (TypedPhone)data.ContactInfo.Phone.First(),
+                    Email = data.ContactInfo.Email,
+                    BillingAddress = data.BillingAddress,
+                    ShippingAddress = data.ShippingAddress,
+                    BusinessAddress = data.BusinessAddress,
+                    BusinessInformationName = data.BusinessInformationName,
+                    BusinessName = data.BusinessName,
+                    TaxClassification = data.BusinessTaxClassification,
+                    AdditionalTaxClassification = data.AdditionalTaxClassification,
+                    ExemptCode = data.ExemptCode,
+                    FatcaCode = data.FATCACode,
+                    BusinessAddressSame = data.BusinessAddressSame,
+                    CurrentAccountNumbers = data.CurrentAccountNumbers,
+                    CustomerCertification = DateTimeOffset.Now,
+                    CustomerSignature = data.CustomerSignature,
+                    SignatureConfirmation = data.SignatureConfirmation,
+                    SignatoryName = data.SignatoryName,
+                    SignatoryRelation = data.SignatoryRelation,
+                    AgreeToTerms = DateTimeOffset.Now,
+                    TcpaPreference = data.TcpaPreference,
+                    PdfGen = w9Pdf,
+                    AssociateId = null,
+                    SourceId = null,
+                    OrderId = null,
+                };
+            dataContext.EnrollmentRecords.Add(record);
+
+            await dataContext.SaveChangesAsync();
+
+            return record.Id;
+        }
+
+        async Task<byte[]> IMobileEnrollmentService.RetrievePdf(Guid mobileEnrollmentId)
+        {
+            var record = await dataContext.EnrollmentRecords.FindAsync(mobileEnrollmentId);
+            
+            return record.PdfGen;
+        }
+
+    }
+}
