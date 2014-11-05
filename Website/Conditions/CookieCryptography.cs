1c19ea93df41037c9ae0afbc642b6e232a482bdb
Matt DeKrey <mattdekrey@gmail.com>
2014-11-05 15:38:30 -0500

    Extract Cryptography functionality for re-use.

--- Website/Conditions/CookieCryptography.cs
+++ Website/Conditions/CookieCryptography.cs
@@ -13,85 +13,21 @@ namespace StreamEnergy.MyStream.Conditions
     {
         #region Settings
 
-        private static int _iterations = 2;
-        private static int _keySize = 128;
-
-        private static string _hash = "SHA1";
         private static string _salt = "8f9huif345utnfkj"; // Random
         private static string _vector = "542jknvzx654fjks"; // Random
+
+        private static readonly StreamEnergy.Cryptography cryptography = new StreamEnergy.Cryptography(_salt, _vector);
 
         #endregion
 
         public static string Encrypt(string value, string password)
         {
-            byte[] vectorBytes = Encoding.ASCII.GetBytes(_vector);
-            byte[] saltBytes = Encoding.ASCII.GetBytes(_salt);
-            byte[] valueBytes = Encoding.UTF8.GetBytes(value);
-
-            byte[] encrypted;
-            using (AesManaged cipher = new AesManaged())
-            {
-                PasswordDeriveBytes _passwordBytes =
-                    new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
-                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
-
-                cipher.Mode = CipherMode.CBC;
-
-                using (ICryptoTransform encryptor = cipher.CreateEncryptor(keyBytes, vectorBytes))
-                {
-                    using (MemoryStream to = new MemoryStream())
-                    {
-                        using (CryptoStream writer = new CryptoStream(to, encryptor, CryptoStreamMode.Write))
-                        {
-                            writer.Write(valueBytes, 0, valueBytes.Length);
-                            writer.FlushFinalBlock();
-                            encrypted = to.ToArray();
-                        }
-                    }
-                }
-                cipher.Clear();
-            }
-            return Convert.ToBase64String(encrypted);
+            return cryptography.Encrypt(value, password);
         }
 
         public static string Decrypt(string value, string password)
         {
-            byte[] vectorBytes = ASCIIEncoding.ASCII.GetBytes(_vector);
-            byte[] saltBytes = ASCIIEncoding.ASCII.GetBytes(_salt);
-            byte[] valueBytes = Convert.FromBase64String(value);
-
-            byte[] decrypted;
-            int decryptedByteCount = 0;
-
-            using (AesManaged cipher = new AesManaged())
-            {
-                PasswordDeriveBytes _passwordBytes = new PasswordDeriveBytes(password, saltBytes, _hash, _iterations);
-                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
-
-                cipher.Mode = CipherMode.CBC;
-
-                try
-                {
-                    using (ICryptoTransform decryptor = cipher.CreateDecryptor(keyBytes, vectorBytes))
-                    {
-                        using (MemoryStream from = new MemoryStream(valueBytes))
-                        {
-                            using (CryptoStream reader = new CryptoStream(from, decryptor, CryptoStreamMode.Read))
-                            {
-                                decrypted = new byte[valueBytes.Length];
-                                decryptedByteCount = reader.Read(decrypted, 0, decrypted.Length);
-                            }
-                        }
-                    }
-                }
-                catch (Exception)
-                {
-                    return String.Empty;
-                }
-
-                cipher.Clear();
-            }
-            return Encoding.UTF8.GetString(decrypted, 0, decryptedByteCount);
+            return cryptography.Decrypt(value, password);
         }
 
     }
