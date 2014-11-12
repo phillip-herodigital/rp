577537cb577d2bc5daf009415f56ff48ca1686a1
Adam Powell <adam.powell@responsivepath.com>
2014-11-12 14:17:20 -0800

    Progress on responsive styles

--- Website/Controllers/ApiControllers/MobileEnrollmentController.cs
+++ Website/Controllers/ApiControllers/MobileEnrollmentController.cs
@@ -77,7 +77,7 @@ namespace StreamEnergy.MyStream.Controllers.ApiControllers
                 { "SignatoryRelation", context.SignatoryRelation ?? "" },    
                 { "AgreeToTerms", context.AgreeToTerms.ToString() },    
                 { "TcpaPreference", context.TcpaPreference.ToString() },    
-                { "PdfUrl", new Uri(Request.RequestUri, "/api/MobileEnrollment/w9/token/" + await mobileEnrollment.CreatePdfToken(result)).ToString() }/**/
+                { "PdfUrl", new Uri(Request.RequestUri, "/api/MobileEnrollment/w9/token/" + Server.(await mobileEnrollment.CreatePdfToken(result))).ToString() }/**/
             });
 
             if (result != Guid.Empty)
