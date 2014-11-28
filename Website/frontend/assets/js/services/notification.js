f3c0e988dfbda98a295241e79da56332b0625c9f
morgan.todd@responsivepath.com <morgan.todd@responsivepath.com>
2014-11-28 13:36:50 -0800

    basic notification service

---
+++ Website/frontend/assets/js/services/notification.js
@@ -0,0 +1,24 @@
+﻿ngApp.factory('notificationService', ['$document', function ($document) {
+
+    var Levels = {INFO: 0, WARN: 1, ERROR: 2, CRITICAL: 3};
+    var className = 'notification-area';
+
+    function showNotification(subject, message, level) {
+        var $elems = $document.find('.' + className);
+        decorateElement($elems, level);
+        $elems.html("<h1>" + subject + "</h1><p>" + message + "</p>");
+    }
+
+    function decorateElement($elements, level) { };
+
+    return {
+        notify: function (subject, message, level) {
+            if (!level && level !== 0) {
+                return showNotification(subject, message, Levels.INFO);
+            } else {
+                return showNotification(subject, message, level);
+            }
+        },
+        level: Levels
+    };
+}]);
