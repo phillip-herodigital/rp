de10bcaa4e23162311c1627b63e6e64813070e3d
jkporter <mail@j.porter.name>
2014-12-09 22:20:02 -0800

    Rev 2. re-work and reskin for redesign

--- Website/Utility/NavigationSiteMapProvider.cs
+++ Website/Utility/NavigationSiteMapProvider.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using RestSharp.Extensions;
+using Sitecore;
+using Sitecore.Configuration;
 using Sitecore.ContentSearch.Utilities;
 using Sitecore.Data;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
+using Sitecore.Data.Managers;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.IO;
 using Sitecore.Links;
 using Sitecore.Mvc.Presentation;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore.SecurityModel;
 using Sitecore.Shell.Applications.ContentEditor;
 using Sitecore.Shell.Framework.Commands;
+using Sitecore.Sites;
+using Sitecore.Web;
+using Sitecore.Xml.Xsl;
+using Version = Sitecore.Data.Version;
 
 namespace StreamEnergy.MyStream.Utility
 {
@@ -17,7 +32,135 @@ namespace StreamEnergy.MyStream.Utility
     {
         public override SiteMapNode FindSiteMapNode(string rawUrl)
         {
-            return FindSiteMapNode(Sitecore.Context.Item);
+            return FindSiteMapNode(Context.Item);
+
+            /* var args = new HttpRequestArgs(HttpContext.Current, HttpRequestType.PostRequestHandlerExecuted);
+
+            var url = RequestUrl.Parse(HttpContext.Current.Request);
+            url.QueryString = HtmlSpecialChars(url.QueryString);
+
+            var path = MainUtil.DecodeName(url.ItemPath);
+            var obj = args.GetItem(path);
+            if (obj == null)
+            {
+                path = url.ItemPath;
+                obj = args.GetItem(path);
+            }
+            if (obj == null)
+            {
+                path = args.LocalPath;
+                obj = args.GetItem(path);
+            }
+            if (obj == null)
+            {
+                path = MainUtil.DecodeName(args.LocalPath);
+                obj = args.GetItem(path);
+            }
+            var site = Context.Site;
+            var part1 = site != null ? site.RootPath : string.Empty;
+            if (obj == null)
+            {
+                path = FileUtil.MakePath(part1, args.LocalPath, '/');
+                obj = args.GetItem(path);
+            }
+            if (obj == null)
+            {
+                path = MainUtil.DecodeName(FileUtil.MakePath(part1, args.LocalPath, '/'));
+                obj = args.GetItem(path);
+            }
+            if (obj == null)
+                obj = ResolveUsingDisplayName(args);
+            if (obj == null && args.UseSiteStartPath && site != null)
+                obj = args.GetItem(site.StartPath);
+
+            return obj == null ? null : FindSiteMapNode(obj); */
+        }
+
+        private static string HtmlSpecialChars(string html)
+        {
+            return html.Replace("(", "&#40;").Replace(")", "&#41;").Replace("<", "&lt;").Replace(">", "&gt;");
+        }
+
+        private static Item GetChild(Item item, string itemName)
+        {
+            foreach (Item obj in item.Children)
+            {
+                if (obj.DisplayName.Equals(itemName, StringComparison.OrdinalIgnoreCase) || obj.Name.Equals(itemName, StringComparison.OrdinalIgnoreCase))
+                    return obj;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Gets a sub item.
+        /// 
+        /// </summary>
+        /// <param name="path">The path.</param><param name="root">The root.</param>
+        /// <returns/>
+        private static Item GetSubItem(string path, Item root)
+        {
+            var obj = root;
+            var str = path;
+            var chArray = new[] { '/' };
+            foreach (var itemName in str.Split(chArray).Where(itemName => itemName.Length != 0))
+            {
+                obj = GetChild(obj, itemName);
+                if (obj == null)
+                    return null;
+            }
+            return obj;
+        }
+
+        /// <summary>
+        /// Resolves the full path.
+        /// 
+        /// </summary>
+        /// <param name="args">The args.</param>
+        /// <returns/>
+        private static Item ResolveFullPath(HttpRequestArgs args)
+        {
+            var itemPath = args.Url.ItemPath;
+            if (string.IsNullOrEmpty(itemPath) || itemPath[0] != 47)
+                return null;
+            var num = itemPath.IndexOf('/', 1);
+            if (num < 0)
+                return null;
+            var root = ItemManager.GetItem(itemPath.Substring(0, num), Language.Current, Version.Latest, Context.Database, SecurityCheck.Disable);
+            return root == null ? null : GetSubItem(MainUtil.DecodeName(itemPath.Substring(num)), root);
+        }
+
+        /// <summary>
+        /// Resolves the local path.
+        /// 
+        /// </summary>
+        /// <param name="args">The args.</param>
+        /// <returns/>
+        private static Item ResolveLocalPath(HttpRequestArgs args)
+        {
+            var site = Context.Site;
+            if (site == null)
+                return null;
+            var root = ItemManager.GetItem(site.RootPath, Language.Current, Version.Latest, Context.Database, SecurityCheck.Disable);
+            return root == null ? null : GetSubItem(MainUtil.DecodeName(args.LocalPath), root);
+        }
+
+        /// <summary>
+        /// Resolves the display name of the using.
+        /// 
+        /// </summary>
+        /// <param name="args">The args.</param>
+        /// <returns/>
+        private Item ResolveUsingDisplayName(HttpRequestArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Item obj;
+            using (new SecurityDisabler())
+            {
+                obj = ResolveLocalPath(args) ?? ResolveFullPath(args);
+                if (obj == null)
+                    return null;
+            }
+            return args.ApplySecurity(obj);
         }
 
         public SiteMapNode FindSiteMapNode(Item item)
@@ -34,15 +177,15 @@ namespace StreamEnergy.MyStream.Utility
 
         public override SiteMapNodeCollection GetChildNodes(SiteMapNode node)
         {
-            return new SiteMapNodeCollection(Sitecore.Context.Database.GetItem(ID.Parse(node.Key)).Children.Select(i => (SiteMapNode)new ItemSiteMapNode(this, i)).ToArray());
+            return new SiteMapNodeCollection(Context.Database.GetItem(ID.Parse(node.Key)).Children.Select(i => (SiteMapNode)new ItemSiteMapNode(this, i)).ToArray());
         }
 
         public override SiteMapNode GetParentNode(SiteMapNode node)
         {
-            var item = Sitecore.Context.Database.GetItem(ID.Parse(node.Key));
+            var item = Context.Database.GetItem(ID.Parse(node.Key));
             if (item.TemplateName != "Navigation Item" || item.Parent == null)
                 return null;
-            return new ItemSiteMapNode(this, Sitecore.Context.Database.GetItem(ID.Parse(node.Key)).Parent);
+            return new ItemSiteMapNode(this, Context.Database.GetItem(ID.Parse(node.Key)).Parent);
         }
 
         protected override SiteMapNode GetRootNodeCore()
@@ -52,34 +195,40 @@ namespace StreamEnergy.MyStream.Utility
         }
     }
 
-    public class ItemSiteMapNode : SiteMapNode
+    public sealed class ItemSiteMapNode : SiteMapNode
     {
         public ItemSiteMapNode(SiteMapProvider provider, Item item)
             : base(provider, item.ID.ToString())
         {
             Item = item;
-            if (item.TemplateName != "Navigation Item")
-                return;
-
             LinkField linkField = item.Fields["Navigation Link"];
-            if (linkField == null) return;
-
-            Title = linkField.Text;
-            Url = linkField.Url;
-            Description = linkField.Title;
-            Attributes = new NameValueCollection
+            if (linkField != null)
             {
-                {"Anchor", linkField.Anchor},
-                {"Class", linkField.Class},
-                {"Target", linkField.Target}
-            };
+                Description = linkField.Title;
+                Attributes = new NameValueCollection
+                {
+                    {"Class", linkField.Class},
+                    {"Target", linkField.Target}
+                };
+            }
 
-            if (!linkField.IsInternal) return;
-            Title = string.IsNullOrEmpty(linkField.Text) ? (linkField.TargetItem != null ? linkField.TargetItem.DisplayName : null) : linkField.Text;
-            Url = linkField.TargetItem != null ? LinkManager.GetItemUrl(linkField.TargetItem) : null;
+            Title = linkField == null ? string.Empty : linkField.Text;
+            if (string.IsNullOrEmpty(Title) && linkField != null && linkField.TargetItem != null)
+                Title = linkField.TargetItem.DisplayName ?? string.Empty;
+              
+            Url = GetUrl(linkField) ?? string.Empty;
+            if (linkField != null && linkField.LinkType == "javascript")
+                Url = "javascript:" + HttpUtility.UrlEncode(Url).Replace("+", "%20");
         }
 
         public Item Item { get; set; }
+
+        private string GetUrl(XmlField field)
+        {
+            var defaultUrlOptions = LinkManager.GetDefaultUrlOptions();
+            defaultUrlOptions.SiteResolving = Settings.Rendering.SiteResolving;
+            return field != null ? new LinkUrl().GetUrl(field, Item.Database) : LinkManager.GetItemUrl(Item, defaultUrlOptions);
+        }
     }
 
     public static class SiteMapNodeExtensions
