50651365b9005c5394e8bd9f114abbb4aadc4d40
Ryan Brill <ryanbrill@gmail.com>
2014-12-29 16:29:51 -0800

    Working on mobile marketing pages

---
+++ Website/frontend/assets/js/controllers/marketing/mobilePlansDetails.js
@@ -0,0 +1,12 @@
+﻿/* Mobile Plans Details Controller
+ *
+ */
+ngApp.controller('MobilePlansDetailsCtrl', ['$scope', function ($scope) {
+
+    $scope.numLines = 2;
+
+    $scope.groupPlanCost = function (planCost, extraLineCost) {
+        return planCost + (($scope.numLines - 1) * extraLineCost);
+    };
+
+}]);
