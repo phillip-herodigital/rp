ec8710797b9ab006f2c7ded6e84b745c1a45bd1f
Ryan Brill <ryanbrill@gmail.com>
2015-01-03 11:20:26 -0800

    cleanup

--- Website/frontend/assets/js/directives/state-selector.js
+++ Website/frontend/assets/js/directives/state-selector.js
@@ -1,5 +1,5 @@
 // State Selector
-ngApp.directive('stateSelector', ['breakpoint', '$filter', '$window', function (breakpoint, $filter, $window) {
+ngApp.directive('stateSelector', ['breakpoint', '$filter', function (breakpoint, $filter) {
 	return {
 		restrict: 'A',
 		scope: true,
@@ -10,23 +10,9 @@ ngApp.directive('stateSelector', ['breakpoint', '$filter', '$window', function (
 				scope.selectedStateKey = attrs.stateSelector;
 			}
 
-			/*scope.selectPane = function(pane) {
-				scope.pane = pane;
-
-				//Calculate the height of the tab items and window scroll offset also accounting for the fixed nav height
-				if(breakpoint.breakpoint.name == 'phone') {
-				    $window.scrollTo(0, $tabs.offset().top + $tabs.height() - jQuery('.site-header').height());
-				}
-			};*/
-
 			scope.$watch('selectedStateKey', function(newVal, oldVal) {
 				scope.selectedState = $filter('filter')(scope.stateEnrollData, {'key': newVal})[0];
 			});
-
-
-			scope.goToEnrollUrl = function () {
-			    $window.location = selectedState.enrollURL;
-			}
 
 		}
 	};
