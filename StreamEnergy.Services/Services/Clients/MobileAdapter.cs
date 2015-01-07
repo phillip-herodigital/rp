66cc75c8212fb54b4b32c51a581f118f629c2028
Adam Powell <adam.powell@responsivepath.com>
2015-01-06 18:01:55 -0800

    Minor updates to style Verify Identity

--- StreamEnergy.Services/Services/Clients/MobileAdapter.cs
+++ StreamEnergy.Services/Services/Clients/MobileAdapter.cs
@@ -102,13 +102,12 @@ namespace StreamEnergy.Services.Clients
                               },
                               MobileInventory = (from inventoryType in (IEnumerable<dynamic>)product.MobileInventory
                                                  let inventoryData = sitecoreProductData.GetMobileInventoryData((string)inventoryType.Id)
-                                                 where inventoryData != null
                                                  select new Mobile.MobileInventory
                                                  {
                                                      Id = (string)inventoryType.Id,
                                                      TypeId = (string)inventoryType.TypeId,
                                                      Price = Convert.ToDecimal(inventoryType.Price.ToString()),
-                                                     InstallmentPlan = GetInstallmentPlanIds(inventoryData, supportedInventoryTypes: product.MobileInventory),
+                                                     InstallmentPlan = (inventoryData != null) ? GetInstallmentPlanIds(inventoryData, supportedInventoryTypes: product.MobileInventory) : null,
                                                  }).ToArray(),
 
                               Footnotes = productData.Footnotes,
