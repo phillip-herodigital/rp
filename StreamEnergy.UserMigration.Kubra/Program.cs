75520c4f35b3146f811183377e76bcf45c528ec9
Matt DeKrey <matt.dekrey@responsivepath.com>
2015-01-08 12:54:25 -0500

    A few more improvements to the Kubra import.

--- StreamEnergy.UserMigration.Kubra/Program.cs
+++ StreamEnergy.UserMigration.Kubra/Program.cs
@@ -16,6 +16,13 @@ namespace StreamEnergy.UserMigration.Kubra
     class Program
     {
         const string prefix = "extranet\\";
+        private readonly MembershipBuilder membership;
+
+        public Program(MembershipBuilder membership)
+        {
+            this.membership = membership;
+        }
+
         static void Main(string[] args)
         {
             var options = new Options();
@@ -34,34 +41,50 @@ namespace StreamEnergy.UserMigration.Kubra
             ((IContainerSetupStrategy)new StreamEnergy.Services.Clients.StreamConnectContainerSetup()).SetupUnity(container);
             ((IContainerSetupStrategy)new StreamEnergy.Services.Clients.ClientContainerSetup()).SetupUnity(container);
 
-            var membership = container.Resolve<MembershipBuilder>();
+            container.Resolve<Program>().Run();
+        }
 
+        private void Run()
+        {
             var users = LoadRecords().ToArray();
-
 
             foreach (var entry in users.Select((e, index) => new { index, e }))
             {
-                if (Membership.GetUser(prefix + entry.e.Username) != null)
-                    continue;
-
-                var customerIdTask = membership.CreateUser(prefix + entry.e.Username, email: entry.e.EmailAddress, globalCustomerId: entry.e.GlobalCustomerId)
-                    .ContinueWith(profileTask =>
-                    {
-                        profileTask.Result.ImportSource = StreamEnergy.DomainModels.Accounts.ImportSource.KubraAccounts;
-                        profileTask.Result.Save();
-                        return profileTask.Result;
-                    });
-
-                customerIdTask.Wait();
+                ImportUser(entry.e).Wait();
 
                 Console.WriteLine("{0}% of {2} - {1}", (entry.index * 100 / users.Length), entry.e.Username, users.Length);
             }
         }
 
-        private static IEnumerable<UserRecord> LoadRecords()
+        private async Task ImportUser(UserRecord userRecord)
+        {
+            if (Membership.GetUser(prefix + userRecord) != null)
+            {
+                await MarkComplete(userRecord, usernameCollision: true);
+            }
+            else
+            {
+
+                var userProfile = await membership.CreateUser(prefix + userRecord.Username, email: userRecord.EmailAddress, globalCustomerId: userRecord.GlobalCustomerId);
+
+                userProfile.ImportSource = StreamEnergy.DomainModels.Accounts.ImportSource.KubraAccounts;
+                userProfile.Save();
+
+                await MarkComplete(userRecord, usernameCollision: false);
+            }
+        }
+
+        private IEnumerable<UserRecord> LoadRecords()
         {
             // TODO - load from database
             throw new NotImplementedException();
         }
+
+        private Task MarkComplete(UserRecord userRecord, bool usernameCollision)
+        {
+            // TODO - load from database
+            throw new NotImplementedException();
+        }
+
     }
 }
