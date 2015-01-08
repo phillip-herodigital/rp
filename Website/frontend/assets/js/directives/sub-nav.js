a82da0eb4d08d3f1530cfe56656e50802be39d1f
Ryan Brill <ryanbrill@gmail.com>
2015-01-07 17:18:55 -0800

    Tweaking subnav for Firefox.

--- Website/frontend/assets/js/directives/sub-nav.js
+++ Website/frontend/assets/js/directives/sub-nav.js
@@ -11,16 +11,20 @@ ngApp.directive('subNav', ['jQuery', 'breakpoint', '$timeout', function (jQuery,
                     return;
                 }
                 var mainNavItem = jQuery('.main-nav .nav-' + attrs.subNav),
-                mainNavItemCenter = mainNavItem.position().left + parseInt(jQuery(".site-header .main-nav > .wrapper").position().left, 10) + mainNavItem.width() / 2, // Need to account for the .wrapper padding in mainNavItem.position().left
+                mainNavItemCenter = Math.floor(mainNavItem.position().left + parseInt(jQuery(".site-header .main-nav > .wrapper").position().left, 10) + mainNavItem.width() / 2), // Need to account for the .wrapper padding in mainNavItem.position().left
                 listItem = element.find('ul'),
                 listItemWidth = listItem.width(),
-                marginLeft = mainNavItemCenter - listItemWidth / 2;
+                marginLeft = Math.ceil(mainNavItemCenter - listItemWidth / 2);
+
+                //console.log(mainNavItem);
 
                 if (marginLeft < 0) {
                     marginLeft = 0;
                 } else if (marginLeft + listItemWidth > element.width()) {
-                    marginLeft = jQuery('.sub-nav .wrapper').width() - listItemWidth;
+                    marginLeft = element.width() - listItemWidth;
                 }
+
+                marginLeft = marginLeft - 1; // Exact widths was causing a bug in Firefox, for some reason.
 
                 scope.marginLeft = Math.floor(marginLeft);
             };
