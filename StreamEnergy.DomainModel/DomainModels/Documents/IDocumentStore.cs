3eef47414457ae441a329ea34ea293a318f3a01a
Matt DeKrey <matt.dekrey@responsivepath.com>
2015-01-12 14:59:20 -0500

    Create the initial document store classes

---
+++ StreamEnergy.DomainModel/DomainModels/Documents/IDocumentStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamEnergy.DomainModels.Documents
+{
+    public interface IDocumentStore
+    {
+        Task<Guid> UploadNew(byte[] document, Guid customerId, string documentType, string mimeType);
+
+        // TODO - access the document
+    }
+}
