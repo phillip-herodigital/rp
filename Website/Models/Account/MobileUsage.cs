b494e109e7fa750f2baec0b7c21b7d4c646ad29c
Adam Brill <adambrill@gmail.com>
2015-01-11 19:08:13 -0800

    Fixed the mobile usage components to handle no usage.

--- Website/Models/Account/MobileUsage.cs
+++ Website/Models/Account/MobileUsage.cs
@@ -10,8 +10,8 @@ namespace StreamEnergy.MyStream.Models.Account
         public string Name { get; set; }
         public string Number { get; set; }
         public string Id { get; set; }
-        public decimal DataUsage { get; set; }
-        public decimal MessagesUsage { get; set; }
-        public decimal MinutesUsage { get; set; }
+        public decimal? DataUsage { get; set; }
+        public decimal? MessagesUsage { get; set; }
+        public decimal? MinutesUsage { get; set; }
     }
 }
