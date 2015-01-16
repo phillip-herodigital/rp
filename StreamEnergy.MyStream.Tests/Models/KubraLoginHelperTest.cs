788076e55d5d1bcd56570984d8e3bb0a9b4ea9fb
Matt DeKrey <matt.dekrey@responsivepath.com>
2015-01-16 13:45:54 -0500

    Fix build

--- StreamEnergy.MyStream.Tests/Models/KubraLoginHelperTest.cs
+++ StreamEnergy.MyStream.Tests/Models/KubraLoginHelperTest.cs
@@ -11,7 +11,7 @@ namespace StreamEnergy.MyStream.Models.Tests
         public void KubraLogin()
         {
             // Arrange
-            var kubraLogin = new KubraLoginHelper();
+            var kubraLogin = new KubraLoginHelper(null);
 
             // Act
             var loginSuccess = kubraLogin.Login(new MyStream.Models.Authentication.LoginRequest { Username = "chrishayden", Password = "not-correct" }).Result;
