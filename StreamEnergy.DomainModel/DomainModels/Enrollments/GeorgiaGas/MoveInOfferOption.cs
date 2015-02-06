20d2c030ed2acb1b73a733e4042df58b5496180c
Adam Powell <adam.powell@responsivepath.com>
2015-02-06 14:51:36 -0800

    QC 1644 - fixed start date on multi-premise, multi-device enrollment

--- StreamEnergy.DomainModel/DomainModels/Enrollments/GeorgiaGas/MoveInOfferOption.cs
+++ StreamEnergy.DomainModel/DomainModels/Enrollments/GeorgiaGas/MoveInOfferOption.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,8 @@ namespace StreamEnergy.DomainModels.Enrollments.GeorgiaGas
     {
         public const string Qualifier = "GeorgiaGasMoveIn";
 
-        public DateTime ConnectDate { get; set; }
+        [Required]
+        public DateTime? ConnectDate { get; set; }
 
         // Note - do not use this fee other than for display; it can be affected by the client
         public decimal ConnectionFee { get; set; }
