1311e9a5917a90b7828ecfe3cd638736898289e9
Matt DeKrey <matt.dekrey@responsivepath.com>
2015-02-17 11:30:16 -0500

    Add EsnActivation front-end.

---
+++ Website/Controllers/ApiControllers/MobileActivationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using StreamEnergy.DomainModels.Enrollments;
+
+namespace StreamEnergy.MyStream.Controllers.ApiControllers
+{
+    public class MobileActivationController : ApiController
+    {
+        private IEnrollmentService enrollmentService;
+
+        public MobileActivationController(IEnrollmentService enrollmentService)
+        {
+            this.enrollmentService = enrollmentService;
+        }
+
+        [HttpPost]
+        [Caching.CacheControl(MaxAgeInMinutes = 0)]
+        public async Task<bool> ActivateEsn([FromBody]string esn)
+        {
+            // TODO - check if esn is an activation code somehow
+            return await enrollmentService.ActivateEsn(esn);
+        }
+    }
+}
---
+++ Website/frontend/assets/js/controllers/activation/esnActivationController.js
@@ -0,0 +1,27 @@
+﻿ngApp.controller('esnActivationController', ['$scope', '$http', function ($scope, $http) {
+    var activationCtrl = this;
+    this.activationCode = null;
+
+    $scope.$watch(function () { return activationCtrl.activationCode; }, function () {
+        activationCtrl.success = undefined;
+        activationCtrl.failure = undefined;
+    })
+
+    this.activate = function () {
+        activationCtrl.lastCode = activationCtrl.activationCode;
+        activationCtrl.isLoading = true;
+        $http.post('/api/mobileActivation/activateEsn', activationCtrl.activationCode)
+            .success(function (data) {
+                activationCtrl.isLoading = false;
+                if (JSON.parse(data))
+                    activationCtrl.success = true;
+                else
+                    activationCtrl.failure = true;
+            })
+            .error(function (data) {
+                activationCtrl.isLoading = false;
+                activationCtrl.failure = true;
+            });
+        activationCtrl.activationCode = null;
+    }
+}]);
