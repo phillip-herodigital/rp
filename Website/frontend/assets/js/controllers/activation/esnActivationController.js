c61a36040f94121e2278f72c31dbc4831422465a
Matt DeKrey <matt.dekrey@responsivepath.com>
2015-02-17 17:55:30 -0500

    Ensure the activation code is transformed to JSON.

--- Website/frontend/assets/js/controllers/activation/esnActivationController.js
+++ Website/frontend/assets/js/controllers/activation/esnActivationController.js
@@ -10,7 +10,7 @@
     this.activate = function () {
         activationCtrl.lastCode = activationCtrl.activationCode;
         activationCtrl.isLoading = true;
-        $http.post('/api/mobileActivation/activateEsn', activationCtrl.activationCode)
+        $http.post('/api/mobileActivation/activateEsn', activationCtrl.activationCode, { transformRequest: JSON.stringify })
             .success(function (data) {
                 activationCtrl.isLoading = false;
                 if (JSON.parse(data))
