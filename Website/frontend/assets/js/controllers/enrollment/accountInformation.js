1194b22313472ba9380d33d9e069c0f606db7b25
Adam Powell <adam.powell@responsivepath.com>
2015-02-20 12:47:09 -0800

    use cleansed address as previous for mobile

--- Website/frontend/assets/js/controllers/enrollment/accountInformation.js
+++ Website/frontend/assets/js/controllers/enrollment/accountInformation.js
@@ -122,9 +122,6 @@ ngApp.controller('EnrollmentAccountInformationCtrl', ['$scope', 'enrollmentServi
     * Complete Enrollment Section
     */
     $scope.completeStep = function () {
-        if ($scope.cartHasMobile() && typeof $scope.accountInformation.previousAddress == 'undefined') {
-            $scope.accountInformation.previousAddress = $scope.accountInformation.mailingAddress;
-        }
 
         var addresses = [$scope.accountInformation.mailingAddress];
         if ($scope.hasMoveIn && $scope.customerType != 'commercial') {
@@ -133,6 +130,10 @@ ngApp.controller('EnrollmentAccountInformationCtrl', ['$scope', 'enrollmentServi
 
 
         var continueWith = function () {
+            // update the cleansed address for mobile
+            if ($scope.cartHasMobile() && typeof $scope.accountInformation.previousAddress == 'undefined') {
+                $scope.accountInformation.previousAddress = $scope.accountInformation.mailingAddress;
+            }
             enrollmentService.setAccountInformation().then(function (data) {
                 $scope.validations = data.validations;
             });
@@ -161,6 +162,7 @@ ngApp.controller('EnrollmentAccountInformationCtrl', ['$scope', 'enrollmentServi
                         if (addressOptions.previousAddress && $scope.modal.previousAddress != 'original') {
                             $scope.accountInformation.previousAddress = $scope.addressOptions.previousAddress[1];
                         }
+
                         continueWith();
                     });
                 }
