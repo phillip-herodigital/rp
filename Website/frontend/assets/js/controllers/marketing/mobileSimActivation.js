e57b1233ce939cc957cfab2ddc8143d682ebfac4
Adam Powell <adam.powell@responsivepath.com>
2015-04-13 15:26:48 -0700

    Finished content styles and JS

--- Website/frontend/assets/js/controllers/marketing/mobileSimActivation.js
+++ Website/frontend/assets/js/controllers/marketing/mobileSimActivation.js
@@ -2,6 +2,30 @@
  *
  */
 ngApp.controller('MobileSimActivationCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-	$scope.selectedPhone = undefined;
+    $scope.selectedPhone = null;
+    $scope.isLoading = false;
+    $scope.activeState = 'step1';
+
+    $scope.$watch("selectedPhone", function(newVal) { 
+        if (newVal == 'gsm') {
+            $scope.activeState = 'step2';
+        } 
+        if (newVal == 'cdma') {
+            $scope.activeState = 'step2CDMA';
+        }
+    }); 
+
+    $scope.cancelActivation = function () {
+        $scope.selectedPhone = null;
+        $scope.activeState = 'step1';
+    };
+
+    $scope.lookupAccount = function () {
+        $scope.activeState = 'step3';
+    };
+
+    $scope.activateService = function () {
+        $scope.activeState = 'step4';
+    };
 
 }]);
