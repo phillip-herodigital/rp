d00eda65891ad78f80e8f9f18c1d6bde0f89d3e5
Adam Powell <adam.powell@responsivepath.com>
2015-04-15 12:18:51 -0700

    Call the api from the frontend

--- Website/frontend/assets/js/controllers/marketing/mobileSimActivation.js
+++ Website/frontend/assets/js/controllers/marketing/mobileSimActivation.js
@@ -23,11 +23,61 @@ ngApp.controller('MobileSimActivationCtrl', ['$scope', '$http', '$location', fun
     };
 
     $scope.lookupAccount = function () {
-        $scope.activeState = 'step3';
+        $scope.isLoading = true;
+        $scope.lookupAccountError = false;
+
+        $http({
+            method: 'POST',
+            url: '/api/MobileActivation/LookupAccountByEsn',
+            data: {  
+                activationCode: $scope.activation.code, 
+                lastName: $scope.activation.lastName
+            },
+            headers: {
+                'Content-Type': 'application/JSON'
+            } 
+        })  
+        .success(function (data, status, headers, config) {
+            $scope.isLoading = false;
+            if (data.accountNumber) {
+                $scope.account = data;
+                $scope.activeState = 'step3';
+            } else {
+                $scope.lookupAccountError = true;
+            }
+        }).error(function(data, status, headers, config) {
+            $scope.isLoading = false;
+            $scope.lookupAccountError = true;
+        });
     };
 
     $scope.activateService = function () {
-        $scope.activeState = 'step4';
+        $scope.isLoading = true;
+        $scope.activateServiceError = false;
+
+        $http({
+            method: 'POST',
+            url: '/api/MobileActivation/ActivateEsn',
+            data: {  
+                activationCode: $scope.activation.code, 
+                accountNumber: $scope.account.accountNumber
+            },
+            headers: {
+                'Content-Type': 'application/JSON'
+            } 
+        })  
+        .success(function (data, status, headers, config) {
+            $scope.isLoading = false;
+            if (data) {
+                $scope.account = data;
+                $scope.activeState = 'step4';
+            } else {
+                $scope.activateServiceError = true;
+            }
+        }).error(function(data, status, headers, config) {
+            $scope.isLoading = false;
+            $scope.activateServiceError = true;
+        });
     };
 
 }]);
