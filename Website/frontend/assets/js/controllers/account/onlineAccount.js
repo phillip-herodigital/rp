cbdf821be70b3089bc3561230d8e9963858fbfc1
Adam Powell <adam.powell@responsivepath.com>
2015-05-26 17:06:27 -0700

    QC 1944 - use the scroll service for success messages

--- Website/frontend/assets/js/controllers/account/onlineAccount.js
+++ Website/frontend/assets/js/controllers/account/onlineAccount.js
@@ -1,96 +1,94 @@
 /* My Online Account Controller
  *
  */
-ngApp.controller('AcctOnlineAccountCtrl', ['$scope', '$rootScope', '$http', '$document', '$timeout', function ($scope, $rootScope, $http, $document, $timeout) {
-	// create a blank object to hold the form information
-	$scope.formData = {};
+ngApp.controller('AcctOnlineAccountCtrl', ['$scope', '$rootScope', '$http', 'scrollService', '$timeout', function ($scope, $rootScope, $http, scrollService, $timeout) {
+    // create a blank object to hold the form information
+    $scope.formData = {};
 
-	// initialize the challenges
-	$scope.formData.challenges = [{},{}];
+    // initialize the challenges
+    $scope.formData.challenges = [{},{}];
 
-	$scope.isLoading = true;
-	$scope.streamConnectError = false;
+    $scope.isLoading = true;
+    $scope.streamConnectError = false;
 
-	// get the current data
-	$http.get('/api/account/getOnlineAccount').success(function (data, status, headers, config) {
-		while (data.challenges.length < 2) {
-			data.challenges.push({});
-		}
-		$scope.formData = data;
-		$scope.formDataOriginal = angular.copy($scope.formData);
-		$scope.languagePreference = data.languagePreference;
-		$scope.isLoading = false;
-		$scope.streamConnectError = false;
-		$scope.selectedIds = [$scope.formData.challenges[0].selectedQuestion != null ? $scope.formData.challenges[0].selectedQuestion.id : "", $scope.formData.challenges[1].selectedQuestion != null ? $scope.formData.challenges[1].selectedQuestion.id : ""];
-	}).error(function() {
-		$scope.isLoading = false;
+    // get the current data
+    $http.get('/api/account/getOnlineAccount').success(function (data, status, headers, config) {
+        while (data.challenges.length < 2) {
+            data.challenges.push({});
+        }
+        $scope.formData = data;
+        $scope.formDataOriginal = angular.copy($scope.formData);
+        $scope.languagePreference = data.languagePreference;
+        $scope.isLoading = false;
+        $scope.streamConnectError = false;
+        $scope.selectedIds = [$scope.formData.challenges[0].selectedQuestion != null ? $scope.formData.challenges[0].selectedQuestion.id : "", $scope.formData.challenges[1].selectedQuestion != null ? $scope.formData.challenges[1].selectedQuestion.id : ""];
+    }).error(function() {
+        $scope.isLoading = false;
         $scope.streamConnectError = true; 
-	});
+    });
 
-	// create a filter so that the same security question can't be selected twice
-	$scope.filter1 = function(item){
-		return (!($scope.formData.challenges.length > 0 && $scope.formData.challenges[0].selectedQuestion && $scope.formData.challenges[0].selectedQuestion.id) || item.id != $scope.formData.challenges[0].selectedQuestion.id);
-	};
+    // create a filter so that the same security question can't be selected twice
+    $scope.filter1 = function(item){
+        return (!($scope.formData.challenges.length > 0 && $scope.formData.challenges[0].selectedQuestion && $scope.formData.challenges[0].selectedQuestion.id) || item.id != $scope.formData.challenges[0].selectedQuestion.id);
+    };
 
-	$scope.filter2 = function(item){
-		return (!($scope.formData.challenges.length > 1 && $scope.formData.challenges[1].selectedQuestion && $scope.formData.challenges[1].selectedQuestion.id) || item.id != $scope.formData.challenges[1].selectedQuestion.id);
-	};
+    $scope.filter2 = function(item){
+        return (!($scope.formData.challenges.length > 1 && $scope.formData.challenges[1].selectedQuestion && $scope.formData.challenges[1].selectedQuestion.id) || item.id != $scope.formData.challenges[1].selectedQuestion.id);
+    };
 
-	// hack to fix IE so the filters work when the select options are changed
-	$scope.fixIE = function(){
-	    $scope.selectedIds = [$scope.formData.challenges[0].selectedQuestion != null ? $scope.formData.challenges[0].selectedQuestion.id : "", $scope.formData.challenges[1].selectedQuestion != null ? $scope.formData.challenges[1].selectedQuestion.id : ""];
-		$timeout(function () {
-			angular.forEach($('select'), function (currSelect) {
-				currSelect.options[currSelect.selectedIndex].text += " ";
-			});
-		});
-	};
+    // hack to fix IE so the filters work when the select options are changed
+    $scope.fixIE = function(){
+        $scope.selectedIds = [$scope.formData.challenges[0].selectedQuestion != null ? $scope.formData.challenges[0].selectedQuestion.id : "", $scope.formData.challenges[1].selectedQuestion != null ? $scope.formData.challenges[1].selectedQuestion.id : ""];
+        $timeout(function () {
+            angular.forEach($('select'), function (currSelect) {
+                currSelect.options[currSelect.selectedIndex].text += " ";
+            });
+        });
+    };
 
-	// process the form
-	$scope.updateOnlineAccount = function() {
-		$scope.isLoading = true;
-		$scope.successMessage = false;
+    // process the form
+    $scope.updateOnlineAccount = function() {
+        $scope.isLoading = true;
+        $scope.successMessage = false;
 
-		// format the request data
-		var requestData = {};
-		requestData.challenges = $scope.formData.challenges;
+        // format the request data
+        var requestData = {};
+        requestData.challenges = $scope.formData.challenges;
 
-		if ($scope.formData.username && $scope.formData.username != '') {
-			requestData.username = $scope.formData.username;
-		}
-		requestData.email = $scope.formData.email;
-		if ($scope.formData.currentPassword) {
-			requestData.currentPassword = $scope.formData.currentPassword;
-			requestData.password = $scope.formData.password;
-			requestData.confirmPassword = $scope.formData.confirmPassword;
-		}
+        if ($scope.formData.username && $scope.formData.username != '') {
+            requestData.username = $scope.formData.username;
+        }
+        requestData.email = $scope.formData.email;
+        if ($scope.formData.currentPassword) {
+            requestData.currentPassword = $scope.formData.currentPassword;
+            requestData.password = $scope.formData.password;
+            requestData.confirmPassword = $scope.formData.confirmPassword;
+        }
 
-		requestData.languagePreference = $scope.formData.languagePreference;
+        requestData.languagePreference = $scope.formData.languagePreference;
 
-		// sent the update
-		$http({
-			method  : 'POST',
-			url     : '/api/account/updateOnlineAccount',
-			data    : requestData,
-			headers : { 'Content-Type': 'application/JSON' } 
-		})
-			.success(function (data, status, headers, config) {
-				if (data.validations) {
-					// if not successful, bind errors to error variables
-					$scope.isLoading = false;
-					$scope.validations = data.validations;
+        // sent the update
+        $http({
+            method  : 'POST',
+            url     : '/api/account/updateOnlineAccount',
+            data    : requestData,
+            headers : { 'Content-Type': 'application/JSON' } 
+        })
+            .success(function (data, status, headers, config) {
+                if (data.validations) {
+                    // if not successful, bind errors to error variables
+                    $scope.isLoading = false;
+                    $scope.validations = data.validations;
 
-				} else {
-					// if successful, alert the user
-					$scope.validations = [];
-					$scope.isLoading = false;
-					$scope.successMessage = true;
-					// scroll to the success message
-			        var offset = angular.element(document.querySelector('header.site-header'))[0].offsetHeight;
-			        var message = angular.element(document.querySelector('.success'))[0];
-                    $document.scrollToElement(message, offset);
-				}
-			});
-	};
-	
+                } else {
+                    // if successful, alert the user
+                    $scope.validations = [];
+                    $scope.isLoading = false;
+                    $scope.successMessage = true;
+                    // scroll to the success message
+                    scrollService.scrollTo('successMessage', 0, 0, angular.noop);
+                }
+            });
+    };
+    
 }]);
