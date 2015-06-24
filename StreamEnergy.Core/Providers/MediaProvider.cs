1efbd109b2820378764afc9bcb04446a12feb8f9
Adam Powell <adam.powell@responsivepath.com>
2015-06-24 14:29:17 -0700

    Fix the spaces in media URLs

---
+++ StreamEnergy.Core/Providers/MediaProvider.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamEnergy.Providers
+{
+    public class MediaProvider : Sitecore.Resources.Media.MediaProvider
+    {
+        public override string GetMediaUrl(Sitecore.Data.Items.MediaItem item, Sitecore.Resources.Media.MediaUrlOptions options)
+        {
+            var returnUrl = base.GetMediaUrl(item, options);
+            return returnUrl.Replace(" ", "%20");
+        }
+    }
+}
