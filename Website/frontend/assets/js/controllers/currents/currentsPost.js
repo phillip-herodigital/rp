08ad55e8f79001d20d24f29c4c3a7c07dd6b5bae
Adam Powell <adam.powell@responsivepath.com>
2015-06-25 11:46:35 -0700

    Add the image gallery on blog posts

---
+++ Website/frontend/assets/js/controllers/currents/currentsPost.js
@@ -0,0 +1,9 @@
+/* Currents Post Controller
+ *
+ */
+ngApp.controller('CurrentsPostCtrl', ['$scope', '$window', function ($scope, $window) {
+    $scope.setActive = function(idx) {
+        $scope.slides[idx].active=true;
+    }
+
+}]);
