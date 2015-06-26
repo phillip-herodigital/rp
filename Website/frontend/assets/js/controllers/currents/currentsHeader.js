6efc4c152d60ee94839823776fe3097c9c329b17
Adam Brill <adambrill@gmail.com>
2015-06-26 07:25:17 -0700

    Fixed a couple issues in currents

--- Website/frontend/assets/js/controllers/currents/currentsHeader.js
+++ Website/frontend/assets/js/controllers/currents/currentsHeader.js
@@ -3,6 +3,6 @@
  */
 ngApp.controller('CurrentsHeaderCtrl', ['$scope', '$window', function ($scope, $window) {
     $scope.searchCurrents = function () {
-        $window.location.href = '/currents/search?term=' +  encodeURIComponent($scope.searchTerm);
+        $window.location.href = '/search?term=' +  encodeURIComponent($scope.searchTerm);
     };
 }]);
