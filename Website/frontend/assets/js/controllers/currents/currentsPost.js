116d5f51c5204b886340c968c1a652cb037ee28f
Adam Brill <adambrill@gmail.com>
2015-06-29 12:54:13 -0700

    Revert "Add the image gallery on blog posts"

--- Website/frontend/assets/js/controllers/currents/currentsPost.js
+++
@@ -1,9 +0,0 @@
-/* Currents Post Controller
- *
- */
-ngApp.controller('CurrentsPostCtrl', ['$scope', '$window', function ($scope, $window) {
-    $scope.setActive = function(idx) {
-        $scope.slides[idx].active=true;
-    }
-
-}]);
