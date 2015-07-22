1492199f2639e9250829de1c69a8496a0b3865a8
Adam Powell <adam.powell@responsivepath.com>
2015-07-22 15:34:30 -0700

    Add ECM renderer config and environment specific settings

---
+++ Website/Pipelines/EcmRenderer.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Modules.EmailCampaign;
+using Sitecore.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StreamEnergy.MyStream.Pipelines
+{
+    public class EcmRenderer
+    {
+        public void Process(PipelineArgs args)
+        {
+            if (!string.IsNullOrEmpty(Settings.GetSetting("ECM.RendererUrl")))
+            {
+                GlobalSettings.RendererUrl = Settings.GetSetting("ECM.RendererUrl");
+            }
+        }
+    }
+}
