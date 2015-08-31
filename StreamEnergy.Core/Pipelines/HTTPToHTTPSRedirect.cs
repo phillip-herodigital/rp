d90c57d422b089908cb7a77fe74c262cf3a20c74
Chris Millican <chris.millican@responsivepath.com>
2015-08-31 11:53:55 -0700

    [update] Using DI and updating the redirect logic.

--- StreamEnergy.Core/Pipelines/HTTPToHTTPSRedirect.cs
+++ StreamEnergy.Core/Pipelines/HTTPToHTTPSRedirect.cs
@@ -5,20 +5,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.Practices.Unity;
 
 namespace StreamEnergy.Pipelines
 {
     public class HTTPToHTTPSRedirect : Sitecore.Pipelines.HttpRequest.HttpRequestProcessor
     {
+        public class Injection
+        {
+            [Dependency]
+            public HttpContextBase Context { get; set; }
+
+            [Dependency("SSLEnabled")]
+            public bool SSLEnabled { get; set; }
+        }
+        private Injection dependencies;
+        public HTTPToHTTPSRedirect()
+        {
+            dependencies = StreamEnergy.Unity.Container.Instance.Unity.Resolve<Injection>();
+        }
+        public HTTPToHTTPSRedirect(Injection injectedValue)
+        {
+            dependencies = injectedValue;
+        }
+
         public override void Process(Sitecore.Pipelines.HttpRequest.HttpRequestArgs args)
         {
-            var URL = HttpContext.Current.Request.Url;
-            var SSLEnabled = !string.IsNullOrEmpty(System.Configuration.ConfigurationManager.AppSettings["SSLEnabled"]) &&
-                bool.Parse(System.Configuration.ConfigurationManager.AppSettings["SSLEnabled"]);
-
-            if (SSLEnabled && URL.OriginalString.Contains("http:"))
+            if (!dependencies.Context.Request.IsSecureConnection && dependencies.SSLEnabled)
             {
-                HttpContext.Current.Response.Redirect(URL.OriginalString.Replace("http:", "https:"), false);
+                string url = dependencies.Context.Request.Url.ToString().StartsWith("http:") ? 
+                    "https:" + dependencies.Context.Request.Url.ToString().Substring(5) : dependencies.Context.Request.Url.ToString();
+                dependencies.Context.Response.Redirect(url);
             }
         }
     }
