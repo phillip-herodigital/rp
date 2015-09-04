c57b623e9ae67ac5d7a06f6c50ac3ed84b6680db
kizzy89 <kayne.alexander@responsivepath.com>
2015-09-04 13:09:19 -0700

    fix

--- Website/frontend/assets/js/services/analytics.js
+++ Website/frontend/assets/js/services/analytics.js
@@ -1,4 +1,4 @@
-﻿ngApp.factory('analytics', [function () {
+﻿ngApp.factory('analytics', ['$timeout', function ($timeout) {
 
     return {
         sendVariables: function () {
@@ -10,6 +10,23 @@
                         tracker.set('dimension' + arguments[i], arguments[i + 1]);
                     }
                     tracker.send('event');
+                } else { 
+                    var maxLoop=5; var count=0;
+                    while (count <= 5) {
+                        $timeout(function () {
+                            if (window.ga && window.ga.getAll().length >= 1) {
+                                var tracker = ga.getAll()[0];
+                                for (var i = 0; i < arguments.length; i += 2) {
+                                    tracker.set('dimension' + arguments[i], arguments[i + 1]);
+                                }
+                                tracker.send('event');
+                                count = 5;
+                            }
+                            
+                        }, 1000);
+                        count++;
+                    }
+
                 }
             } catch(e) {} //just eat any errors;
         }
