64103b4504bea1e9b577306dcda5ebbd3d44be79
Adam Powell <adam.powell@responsivepath.com>
2015-09-03 17:26:18 -0700

    Revert "Remove HTTPS redirect code for 9/3 release"
    This reverts commit 9f2aecfe06e3aff56eee381fd7af29778ee2e4c4.

---
+++ StreamEnergy.Core/Pipelines/HTTPToHTTPSRedirect.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.SecurityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace StreamEnergy.Pipelines
+{
+    public class HTTPToHTTPSRedirect : Sitecore.Pipelines.HttpRequest.HttpRequestProcessor
+    {
+        public override void Process(Sitecore.Pipelines.HttpRequest.HttpRequestArgs args)
+        {
+            var URL = HttpContext.Current.Request.Url;
+            var SSLEnabled = !string.IsNullOrEmpty(System.Configuration.ConfigurationManager.AppSettings["SSLEnabled"]) &&
+                bool.Parse(System.Configuration.ConfigurationManager.AppSettings["SSLEnabled"]);
+
+            if (SSLEnabled && URL.OriginalString.Contains("http:"))
+            {
+                HttpContext.Current.Response.Redirect(URL.OriginalString.Replace("http:", "https:"), false);
+            }
+        }
+    }
+}
