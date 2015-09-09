4207d6e0dd9630c5acc40dd8525263072f07afa1
kizzy89 <kayne.alexander@responsivepath.com>
2015-09-09 13:29:23 -0700

    code update

--- Website/frontend/assets/js/services/analytics.js
+++ Website/frontend/assets/js/services/analytics.js
@@ -10,21 +10,22 @@
                         tracker.set('dimension' + arguments[i], arguments[i + 1]);
                     }
                     tracker.send('event');
-                } else { 
-                    var count=0;
-                    while (count <= 5) {
-                        $timeout(function () {
-                            if (window.ga && window.ga.getAll().length >= 1) {
-                                var tracker = ga.getAll()[0];
-                                for (var i = 0; i < arguments.length; i += 2) {
-                                    tracker.set('dimension' + arguments[i], arguments[i + 1]);
-                                }
-                                tracker.send('event');
-                                count = 5;
-                            }                            
-                        }, 1000);
-                        count++;
-                    }
+                } else {
+                    var tries = 0;
+                    var cancel = $timeout(function() {
+                        if (window.ga && window.ga.getAll().length >= 1) {
+                            var tracker = ga.getAll()[0];
+                            for (var i = 0; i < arguments.length; i += 2) {
+                                tracker.set('dimension' + arguments[i], arguments[i + 1]);
+                            }
+                            tracker.send('event');
+                            $timeout.cancel(cancel);
+                        }
+                        if (tries > 100) {
+                            $timeout.cancel(cancel);
+                        }
+                        tries++;
+                    }, 2500);
                 }
             } catch(e) {} //just eat any errors;
         }
