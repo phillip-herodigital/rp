0943b2a851428a66f1e480ad2a32b800da6fcf23
kizzy89 <kayne.alexander@responsivepath.com>
2015-09-14 14:06:22 -0700

    Revert "[update] HTTPS Logging put in place."
    This reverts commit e03da2e856e35e9d1eb14ea8dd12ed52097949bf.

--- StreamEnergy.Core/Pipelines/HTTPToHTTPSRedirect.cs
+++ StreamEnergy.Core/Pipelines/HTTPToHTTPSRedirect.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using System.Web;
 using Microsoft.Practices.Unity;
 using System.Text.RegularExpressions;
-using ResponsivePath.Logging;
 
 namespace StreamEnergy.Pipelines
 {
@@ -20,8 +19,6 @@ namespace StreamEnergy.Pipelines
 
             [Dependency("SSLEnabled")]
             public bool SSLEnabled { get; set; }
-            [Dependency]
-            public ILogger logger { get; set; }
         }
         private Injection dependencies;
         public HTTPToHTTPSRedirect()
@@ -35,29 +32,17 @@ namespace StreamEnergy.Pipelines
 
         public override void Process(Sitecore.Pipelines.HttpRequest.HttpRequestArgs args)
         {
-            dependencies.logger.Record("HTTPToHTTS Process Initiated", Severity.Debug);
-            dependencies.logger.Record(string.Format("HTTPS Initial Condtion - !IsSecureConnection: {0}  -  SSLEnabled: {1}   -   Full Condition: {2}",
-                !HttpContext.Current.Request.IsSecureConnection, dependencies.SSLEnabled, !HttpContext.Current.Request.IsSecureConnection && dependencies.SSLEnabled),
-                Severity.Debug);
-
             if (!HttpContext.Current.Request.IsSecureConnection && dependencies.SSLEnabled)
             {
                 string url = dependencies.Context.Request.Url.ToString();
-                dependencies.logger.Record(string.Format("HTTPS Check URL: {0}", url), Severity.Debug);
                 Regex reg = new Regex("http:");
                 url = reg.Replace(url, "https:", 1);
 
-                dependencies.logger.Record(string.Format("HTTPS url after replacement: {0}  -  Dependencies URL: {1}  ---  Is different from dependencies url:{2}", 
-                    url, dependencies.Context.Request.Url.ToString(), url != dependencies.Context.Request.Url.ToString()), Severity.Debug);
-
                 if (url != dependencies.Context.Request.Url.ToString()) //In case HTTPS url returns non-secure connection for whatever reason
                 {
-                    dependencies.logger.Record(string.Format("Attempting redirect to: {0}", url), Severity.Debug);
                     dependencies.Context.Response.Redirect(url);
                 }
             }
-
-            dependencies.logger.Record("HTTPToHTTS Process Exiting", Severity.Debug);
         }
     }
 }
