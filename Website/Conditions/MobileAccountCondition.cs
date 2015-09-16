84c61064be7df77fa0b99da1d450dab9475786ab
Adam Powell <adam.powell@responsivepath.com>
2015-09-16 11:58:36 -0700

    Add condition to check for a mobile account

---
+++ Website/Conditions/MobileAccountCondition.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Practices.Unity;
+using Sitecore.Rules.Conditions;
+using System.Threading.Tasks;
+using StreamEnergy.DomainModels.Accounts;
+using StreamEnergy.DomainModels.Payments;
+
+namespace StreamEnergy.MyStream.Conditions
+{
+    public class MobileAccountCondition<T> : WhenCondition<T>
+        where T : Sitecore.Rules.RuleContext
+    {
+        private readonly Injection dependencies;
+
+        public class Injection
+        {
+            [Dependency]
+            public ISettings Settings { get; set; }
+
+            [Dependency]
+            public ICurrentUser currentUser { get; set; }
+
+            [Dependency]
+            public IAccountService accountService { get; set; }
+        }
+        
+        public MobileAccountCondition()
+        {
+            dependencies = StreamEnergy.Unity.Container.Instance.Unity.Resolve<Injection>();
+        }
+
+        public MobileAccountCondition(Injection injectedValue)
+        {
+            dependencies = injectedValue;
+        }
+
+        protected override bool Execute(T ruleContext)
+        {
+            var result = AsyncHelper.RunSync<bool>(() => hasMobileAccount());
+
+            return result;
+        }
+
+        public async Task<bool> hasMobileAccount()
+        {
+            ICurrentUser currentUser = dependencies.currentUser;
+            IAccountService accountService = dependencies.accountService;
+
+            currentUser.Accounts = await accountService.GetAccounts(currentUser.StreamConnectCustomerId);
+            if (currentUser.Accounts != null)
+            {
+                return currentUser.Accounts.Where(a => a.AccountType == "Mobile").Any();
+            }
+            else
+            { 
+                return false;
+            }
+        }
+    }
+}
