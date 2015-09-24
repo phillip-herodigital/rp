0ec4081e38c63a80caef100c296d90e0082806ab
NelsonMike <michael.nelson@herodigital.com>
2015-09-24 14:47:45 -0700

    Updated Mobile Plans Selection Page

--- Website/frontend/assets/js/controllers/marketing/mobilePlansDetails.js
+++ Website/frontend/assets/js/controllers/marketing/mobilePlansDetails.js
@@ -2,11 +2,22 @@
  *
  */
 ngApp.controller('MobilePlansDetailsCtrl', ['$scope', function ($scope) {
-
+   
     $scope.numLines = 2;
 
     $scope.groupPlanCost = function (planCost, extraLineCost) {
-        return parseFloat(planCost) + (($scope.numLines - 1) * extraLineCost);
+        var cost = parseFloat(planCost) + (($scope.numLines - 1) * extraLineCost);
+        return $scope.superCents(cost.toFixed(2));
     };
 
+    $scope.superCents = function (planCost) {
+        var array = planCost.toString().split('.');
+        var planCostFinal = "";
+        if (array.length > 1 && array[1] != "00") {
+            planCostFinal = '$' + array[0] + "<sup>." + array[1] + "</sup>";
+        } else {
+            planCostFinal = '$' + array[0];
+        }
+        return planCostFinal;
+    }
 }]);
