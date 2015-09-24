1d878637b341e7d3abbb9ee34c5f7d0f17955afd
Chris Millican <chris.millican@responsivepath.com>
2015-09-24 12:57:37 -0700

    [update] Updating project for the correct JS files.

---
+++ Website/frontend/assets/js/controllers/enrollment/mobile/mobileEnrollmentProgressBar.js
@@ -0,0 +1,20 @@
+﻿ngApp.controller('MobileEnrollmentProgressBarCtrl', ['$scope', '$filter', '$modal', '$http', '$sce', 'enrollmentService', 'mobileEnrollmentService', 'enrollmentStepsService', 'enrollmentCartService', 'scrollService', 'analytics', function ($scope, $filter, $modal, $http, $sce, enrollmentService, mobileEnrollmentService, enrollmentStepsService, enrollmentCartService, scrollService, analytics) {
+    $scope.referral = {
+        'Name': "John Smith",
+        'JobTitle': "Some Job"
+    }
+    $scope.mobileEnrollmentService = mobileEnrollmentService;
+    $scope.mobileEnrollmentService.cartItemCountText = '';
+    $scope.enrollmentCartService = enrollmentCartService;
+    $scope.showModal = function (templateUrl) {
+        $modal.open({
+            'scope': $scope,
+            'templateUrl': templateUrl
+        })
+    };
+
+    $scope.openCartOverlay = function () {
+        $scope.enrollmentCartService.cartOverlayOpenFromProgressBar = true;
+        enrollmentCartService.toggleCart()
+    }
+}]);
