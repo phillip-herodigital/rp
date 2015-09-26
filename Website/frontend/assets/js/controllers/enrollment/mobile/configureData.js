e984be426959953d5343c36d92a777849ce1107c
Chris Millican <chris.millican@responsivepath.com>
2015-09-26 09:57:08 -0700

    [update] Coverage Map Overlay

--- Website/frontend/assets/js/controllers/enrollment/mobile/configureData.js
+++ Website/frontend/assets/js/controllers/enrollment/mobile/configureData.js
@@ -1,7 +1,11 @@
-﻿ngApp.controller('MobileEnrollmentConfigureDataCtrl', ['$scope', '$filter', '$modal', 'enrollmentService', 'mobileEnrollmentService', 'enrollmentStepsService', 'enrollmentCartService', 'analytics', function ($scope, $filter, $modal, enrollmentService, mobileEnrollmentService, enrollmentStepsService, enrollmentCartService, analytics) {
+﻿ngApp.controller('MobileEnrollmentConfigureDataCtrl', ['$scope', '$filter', '$modal', 'uiGmapGoogleMapApi','uiGmapIsReady', '$location', 'jQuery', '$http', 'enrollmentService', 'mobileEnrollmentService', 'enrollmentStepsService', 'enrollmentCartService', 'analytics', function ($scope, $filter, $modal, uiGmapGoogleMapApi,uiGmapIsReady, $location, jQuery, $http, enrollmentService, mobileEnrollmentService, enrollmentStepsService, enrollmentCartService, analytics) {
 
     $scope.mobileEnrollmentService = mobileEnrollmentService;
     $scope.currentMobileLocationInfo = enrollmentCartService.getActiveService;
+    $scope.data = { serviceState: 'TX' };
+    $scope.data.serviceLocation = {};
+    
+    var coverageMap = $(jQuery.find(".coverage-map-container"));
 
     $scope.formFields = {
         chosenPlanId: undefined
@@ -171,4 +175,160 @@
         })
     };
 
+    $scope.showCoverageMapOverlay = function () {
+        coverageMap.removeClass("hidden-map");
+
+        var test = $scope.mapInstance;
+        var test2 = enrollmentService.mapInstance;
+        var test3 = $scope.$parent.mapInstance;
+
+        var center = $scope.mapInstance.getCenter();
+        google.maps.event.trigger($scope.mapInstance, 'resize');
+        $scope.mapInstance.setCenter(center);
+        
+    }
+    $scope.lookupZip = function () {
+        enrollmentService.isLoading = true;
+        analytics.sendVariables(1, $scope.postalCode5);
+        $http.get('/api/addresses/lookupZip/' + $scope.postalCode5)
+        .success(function (data) {
+            enrollmentService.isLoading = false;
+            showChangeLocation = false;
+            if (data.length != 0) {
+                mobileEnrollmentService.state = data[0];
+                mobileEnrollmentService.postalCode5 = $scope.postalCode5;
+
+                $scope.data.serviceLocation.address = {
+                    line1: 'Line1',
+                    city: 'City',
+                    stateAbbreviation: data[0],
+                    postalCode5: $scope.postalCode5
+                };
+                $scope.data.serviceLocation.capabilities = [{ "capabilityType": "ServiceStatus", "enrollmentType": "moveIn" }];
+                $scope.data.serviceLocation.capabilities.push({ "capabilityType": "CustomerType", "customerType": (mobileEnrollmentService.planType == 'Business') ? "commercial" : "residential" });
+                $scope.data.serviceLocation.capabilities.push({ "capabilityType": "Mobile" });
+
+                /*
+               
+
+                $scope.$parent.customerType = (mobileEnrollmentService.planType == 'Business') ? "commercial" : "residential";
+                */
+
+                var activeService = enrollmentCartService.getActiveService();
+                if (activeService) {
+                    activeService.location = $scope.data.serviceLocation;
+                    enrollmentService.setSelectedOffers(true);
+                }
+                else {
+                    enrollmentCartService.addService({ location: $scope.data.serviceLocation });
+                    enrollmentService.setServiceInformation(true);
+                    activeService = enrollmentCartService.getActiveService();
+                }
+
+                // if the state is in the excluded list, show the "no plans available" dialog
+                if (_($scope.mobileEnrollmentSettings.excludedStates).contains(data[0])) {
+                    $scope.showNetworks = false;
+                }
+                else {
+                    $scope.showNetworks = true;
+                }
+            }
+            else {
+                $scope.showNetworks = false;
+            }
+
+            
+        })
+    };
+
+    var ARCOverlay = function (key, layer, opacity) {
+        return new google.maps.ImageMapType({
+            isPng: true,
+            opacity: opacity,
+            tileSize: new google.maps.Size(256, 256),
+            getTileUrl: function (coord, zoom) {
+                return "http://api.cellmaps.com/tiles/cellmap/" + zoom + "/" + coord.x + "/" + coord.y + ".png?key=" + key + "&map=stream" + ((layer != "") ? "&llist=" + layer : "");
+            }
+        });
+    };
+
+    $scope.searchbox = {
+        template: 'searchbox.tpl.html',
+        events: {
+            places_changed: function (searchBox) {
+                $scope.mapInstance.setCenter(searchBox.getPlaces()[0].geometry.location);
+                if (searchBox.getPlaces()[0].geometry.viewport) {
+                    $scope.mapInstance.fitBounds(searchBox.getPlaces()[0].geometry.viewport);
+                } else {
+                    $scope.mapInstance.setZoom(15);
+                }
+            }
+        }
+    };
+
+    $scope.selectedNetwork = $location.search().carrier || 'att';
+
+    $scope.layers = {
+        att: {
+            att_voice_roam: true,
+            att_data_roam: true,
+            att_lte: true
+        },
+        sprint: {
+            sprint_voice_roam: true,
+            sprint_data_roam: true,
+            sprint_lte: true
+        }
+    };
+
+    $scope.map = {
+        center: { latitude: 38.850033, longitude: -98.6500523 },
+        zoom: 4,
+        options: {
+            disableDefaultUI: false,
+            scrollwheel: false,
+            panControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            styles: [{ "featureType": "poi", "stylers": [{ "visibility": "off" }] }, { "stylers": [{ "saturation": -70 }, { "lightness": 37 }, { "gamma": 1.15 }] }, { "elementType": "labels", "stylers": [{ "gamma": 0.26 }, { "visibility": "off" }] }, { "featureType": "road", "stylers": [{ "lightness": 0 }, { "saturation": 0 }, { "hue": "#ffffff" }, { "gamma": 0 }] }, { "featureType": "road", "elementType": "labels.text.stroke", "stylers": [{ "visibility": "off" }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "lightness": 50 }, { "saturation": 0 }, { "hue": "#ffffff" }] }, { "featureType": "administrative.province", "stylers": [{ "visibility": "on" }, { "lightness": -50 }] }, { "featureType": "administrative.province", "elementType": "labels.text.stroke", "stylers": [{ "visibility": "off" }] }, { "featureType": "administrative.province", "elementType": "labels.text", "stylers": [{ "lightness": 20 }] }] // https://snazzymaps.com/style/19/subtle
+        },
+        events: {
+            tilesloaded: function (map) {
+                $scope.$apply(function () {
+                    $scope.mapInstance = map;
+                    $scope.$parent.mapInstance = map;
+                });
+            }
+        }
+    };
+
+    $scope.$watch('mapInstance', function (newVal, oldVal) {
+        if (newVal) {
+            $scope.updateMapLayers();
+        }
+    });
+
+    $scope.$watch('selectedNetwork', function (newVal, oldVal) {
+        $scope.updateMapLayers();
+    }, true);
+
+    $scope.$watch('layers', function (newVal, oldVal) {
+        $scope.updateMapLayers();
+    }, true);
+
+    $scope.updateMapLayers = function () {
+        var layers = [];
+        angular.forEach($scope.layers[$scope.selectedNetwork], function (value, key) {
+            if (value == true) {
+                layers.push(key);
+            }
+        });
+        if ($scope.mapInstance) {
+            if (layers.length) {
+                $scope.mapInstance.overlayMapTypes.setAt(0, new ARCOverlay('d8bfd6a09d07263c52ecb75b5a470a90', layers.join(','), 0.75));
+            } else {
+                $scope.mapInstance.overlayMapTypes.clear();
+            }
+        }
+    };
 }]);
