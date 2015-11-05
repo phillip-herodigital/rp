90eb8e8d5a213225ff92cc033901855e7d1c12c3
Adam Powell <adam.powell@responsivepath.com>
2015-11-05 11:51:36 -0800

    Start to add condition to check if it's the first login

---
+++ Website/Conditions/CustomerFirstLoginCondition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Practices.Unity;
+using Sitecore.Rules.Conditions;
+using System.Threading.Tasks;
+using StreamEnergy.DomainModels.Accounts;
+using System.Web.Security;
+
+namespace StreamEnergy.MyStream.Conditions
+{
+    public class CustomerFirstLoginCondition<T> : WhenCondition<T>
+        where T : Sitecore.Rules.RuleContext
+    {
+        private readonly Injection dependencies;
+
+        public class Injection
+        {
+            [Dependency]
+            public ICurrentUser currentUser { get; set; }
+        }
+
+        public CustomerFirstLoginCondition()
+        {
+            dependencies = StreamEnergy.Unity.Container.Instance.Unity.Resolve<Injection>();
+        }
+
+        public CustomerFirstLoginCondition(Injection injectedValue)
+        {
+            dependencies = injectedValue;
+        }
+
+        protected override bool Execute(T ruleContext)
+        {
+            var user = Membership.GetUser(dependencies.currentUser.Customer.Username);
+
+            return true;
+        }
+
+    }
+}
