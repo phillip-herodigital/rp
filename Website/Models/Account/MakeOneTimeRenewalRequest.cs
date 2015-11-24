f090e0dc561283ec8666980c939341ba93eb595f
phillip-herodigital <phillip.jacobs@herodigital.com>
2015-11-23 17:41:42 -0800

    saving progress

---
+++ Website/Models/Account/MakeOneTimeRenewalRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ResponsivePath.Validation;
+using StreamEnergy.DomainModels;
+
+namespace StreamEnergy.MyStream.Models.Account
+{
+    public class MakeOneTimeRenewalRequest
+    {
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        public string Last4OfSocial { get; set; }
+    }
+}
