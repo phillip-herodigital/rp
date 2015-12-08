c74dc5c2255c1b0eada21b2cd98be4dfa6d86d33
phillip-herodigital <phillip.jacobs@herodigital.com>
2015-12-07 16:25:25 -0800

    saving progress

--- Website/frontend/assets/js/controllers/marketing/voice.js
+++ Website/frontend/assets/js/controllers/marketing/voice.js
@@ -2,4 +2,21 @@
 
     $scope.showHubFeatures = false;
 
+    $scope.toggleHubFeatures = function () {
+        if ($scope.showHubFeatures) {
+            $scope.showHubFeatures = false;
+            document.getElementById('slidingText').classList.remove('slideIn');
+            document.getElementById('slidingText').classList.add('slideOut');
+            document.getElementById('dropDownImage').classList.remove('rotate180');
+            document.getElementById('dropDownImage').classList.add('rotate0');
+
+        }
+        else {
+            $scope.showHubFeatures = true;
+            document.getElementById('slidingText').classList.remove('slideOut');
+            document.getElementById('slidingText').classList.add('slideIn');
+            document.getElementById('dropDownImage').classList.add('rotate180');
+            document.getElementById('dropDownImage').classList.remove('rotate0');
+        }
+    }
 }]);
