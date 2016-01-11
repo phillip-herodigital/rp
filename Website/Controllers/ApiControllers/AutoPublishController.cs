cae2380100f0c0a9cb35d52d3bcb4c174d61d18b
phillip.jacobs@herodigital.com <phillip.jacobs@herodigital.com>
2016-01-11 14:33:23 -0800

    add AutoPublishController

---
+++ Website/Controllers/ApiControllers/AutoPublishController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Form.Core.Configuration;
+using Sitecore.Data;
+using Sitecore.Globalization;
+using Sitecore.Publishing;
+
+namespace StreamEnergy.MyStream.Controllers.ApiControllers
+{
+    public class AutoPublishController : Controller
+    {
+        private void PerformSmartPublish()
+        {
+            if (true == true)
+            {
+                Database dbSource = Sitecore.Configuration.Factory.GetDatabase("master");
+                Database dbTarget = Sitecore.Configuration.Factory.GetDatabase("web");
+
+                Language[] dbLanguages = dbSource.Languages;
+
+                Database[] dbTargets = new Database[] { dbTarget };
+
+                PublishManager.PublishSmart(dbSource, dbTargets, dbLanguages);
+            }
+        }
+    }
+}
