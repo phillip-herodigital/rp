aee137f467f38c3c8d12d262b72f62498e428a3e
phillip.jacobs@herodigital.com <phillip.jacobs@herodigital.com>
2016-01-28 11:21:50 -0800

    changed dropdown text to ng-class and fixed typo on Get More cshtml

--- Website/frontend/assets/js/controllers/marketing/voice.js
+++ Website/frontend/assets/js/controllers/marketing/voice.js
@@ -1,22 +1,4 @@
 ﻿ngApp.controller('voiceCtrl', ['$scope', '$http', function ($scope, $http) {
-
     $scope.showHubFeatures = false;
     $scope.bundleLink = "";
-    $scope.toggleHubFeatures = function () {
-        if ($scope.showHubFeatures) {
-            $scope.showHubFeatures = false;
-            document.getElementById('slidingText').classList.remove('slideIn');
-            document.getElementById('slidingText').classList.add('slideOut');
-            document.getElementById('dropDownImage').classList.remove('rotate180');
-            document.getElementById('dropDownImage').classList.add('rotate0');
-
-        }
-        else {
-            $scope.showHubFeatures = true;
-            document.getElementById('slidingText').classList.remove('slideOut');
-            document.getElementById('slidingText').classList.add('slideIn');
-            document.getElementById('dropDownImage').classList.add('rotate180');
-            document.getElementById('dropDownImage').classList.remove('rotate0');
-        }
-    }
 }]);
