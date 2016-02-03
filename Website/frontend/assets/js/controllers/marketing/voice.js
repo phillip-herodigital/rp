ceb58cb13bc31dcfd6dcd1fa7943b02ee1ddce22
Adam Powell <adam.powell@responsivepath.com>
2016-02-03 15:23:42 -0800

    Style updates and make the controller context the whole page

--- Website/frontend/assets/js/controllers/marketing/voice.js
+++ Website/frontend/assets/js/controllers/marketing/voice.js
@@ -1,4 +1,7 @@
 ﻿ngApp.controller('voiceCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.showHubFeatures = false;
     $scope.bundleLink = "";
+    $scope.hubLink = "https://shop-stream-peck.ooma.com/cart?offer=STREAM_HUB&sales_source=xyz&associate_a_number=1234&free_sponsorship_id=8r8r8";
+    $scope.airLink = "https://shop-stream-peck.ooma.com/cart?offer=STREAM_HUB_AIR&sales_source=xyz&associate_a_number=1234&free_sponsorship_id=8r8r8";
+    $scope.bridgeLink = "https://shop-stream-peck.ooma.com/cart?offer=STREAM_HUB_BRIDGE&sales_source=xyz&associate_a_number=1234&free_sponsorship_id=8r8r8";
 }]);
