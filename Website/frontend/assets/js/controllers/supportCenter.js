62776b0380aaa545bfdd6c1b6584a6bfbba5cbaa
phillip.jacobs@herodigital.com <phillip.jacobs@herodigital.com>
2016-04-28 16:10:03 -0700

    wip

--- Website/frontend/assets/js/controllers/supportCenter.js
+++ Website/frontend/assets/js/controllers/supportCenter.js
@@ -1,11 +1,66 @@
-﻿ngApp.controller('supportCenter', ['$scope', '$http', function ($scope, $http) {
+﻿ngApp.controller('supportCenterCtrl', ['$scope', '$http', function ($scope, $http) {
 
-    $scope.states = [{
-        name: Texas,
-        abbreviation: TX
+    $scope.services = [
+        {
+            title: "Utility",
+            name: "Utility",
+            states: [{
+                name: "Texas",
+                abbreviation: "TX"
+            },
+            {
+                name: "Georgia",
+                abbreviation: "GA"
+            }],
+            link: "/"
+        },
+        {
+            title: "Mobile",
+            name: "Mobile",
+            link: "/"
+        },
+        {
+            title: "Protective",
+            name: "Protective",
+            link: "/"
+        }
+    ];
+
+    $scope.faqs = [{
+        name: "faq1",
+        answer: "because",
+        question: "why",
+        description: "description1",
+        link: "/link1",
+        related: "faq2",
+        furtherSupport: "/",
+        categories: ["things", "objects"],
+        states: ["Texas", "Georgia"],
+        keywords: ["utility", "stuff"]
     },
     {
-        name: Georgia,
-        abbreviation: GA
-    }];
+        name: "faq2",
+        answer: "because",
+        question: "why",
+        description: "description2",
+        link: "/link3",
+        related: "faq1",
+        furtherSupport: "/",
+        categories: ["things", "objects"],
+        states: ["Texas", "Georgia"],
+        keywords: ["utility", "stuff"]
+    }]
+
+    $scope.selectService = function (service, state) {
+        if (service.states) {
+            if (state) {
+                $scope.selectedService = service;
+                $scope.selectedState = state;
+            }
+        }
+        else {
+            $scope.selectedService = service;
+            $scope.selectedState = state;
+        }
+    }
 }]);
