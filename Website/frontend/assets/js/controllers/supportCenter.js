e31ec4173233ed9bfb6addf0d0ea83e8d7a9e6e8
phillip.jacobs@herodigital.com <phillip.jacobs@herodigital.com>
2016-04-28 11:57:22 -0700

    start

---
+++ Website/frontend/assets/js/controllers/supportCenter.js
@@ -0,0 +1,11 @@
+﻿ngApp.controller('supportCenter', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.states = [{
+        name: Texas,
+        abbreviation: TX
+    },
+    {
+        name: Georgia,
+        abbreviation: GA
+    }];
+}]);
