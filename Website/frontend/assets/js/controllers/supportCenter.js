d84deedddfb7ed3286585e2d3784d9c90e2db0ff
phillip.jacobs@herodigital.com <phillip.jacobs@herodigital.com>
2016-05-02 14:52:37 -0700

    popular FAQ and search wip

--- Website/frontend/assets/js/controllers/supportCenter.js
+++ Website/frontend/assets/js/controllers/supportCenter.js
@@ -29,27 +29,49 @@
     $scope.faqs = [{
         name: "faq1",
         answer: "because",
-        question: "why",
+        question: "why1",
         description: "description1",
         link: "/link1",
-        related: "faq2",
+        related: ["faq2", "faq3"],
         furtherSupport: "/",
         categories: ["things", "objects"],
         states: ["Texas", "Georgia"],
-        keywords: ["utility", "stuff"]
+        keywords: ["utility", "stuff"],
+        popular: true,
+        helpful: null,
+        helpfulCount: 0
     },
     {
         name: "faq2",
         answer: "because",
-        question: "why",
+        question: "why2",
         description: "description2",
-        link: "/link3",
-        related: "faq1",
+        link: "/link2",
+        related: ["faq1", "faq3"],
         furtherSupport: "/",
         categories: ["things", "objects"],
         states: ["Texas", "Georgia"],
-        keywords: ["utility", "stuff"]
-    }]
+        keywords: ["utility", "stuff"],
+        popular: true,
+        helpful: null,
+        helpfulCount: 0
+    },
+    {
+        name: "faq3",
+        answer: "because",
+        question: "why3",
+        description: "description3",
+        link: "/link3",
+        related: ["faq1", "faq2"],
+        furtherSupport: "/",
+        categories: ["things", "objects"],
+        states: ["Texas", "Georgia"],
+        keywords: ["utility", "stuff"],
+        popular: false,
+        helpful: null,
+        helpfulCount: 0
+    }];
+    $scope.selectedFaqIndex = null;
 
     $scope.selectService = function (service, state) {
         if (service.states) {
@@ -64,5 +86,54 @@
             $scope.selectedState = state;
             $scope.dropDown = false;
         }
+    };
+    
+    $scope.selectFaq = function (index) {
+        //select new popFaq
+        if ($scope.selectedFaqIndex == null) {
+            $scope.faqs[index].selected = true;
+            $scope.selectedFaqIndex = index;
+        }
+        else {
+            // save helpful info
+            var oldFaq = $scope.faqs[$scope.selectedFaqIndex];
+            if (oldFaq.helpful != null) {
+                if (oldFaq.helpful) {
+                    oldFaq.helpfulCount++;
+                }
+                if (!oldFaq.helpful && oldFaq.helpfulCount > 0) {
+                    oldFaq.helpfulCount--;
+                }
+                oldFaq.helpful = null;
+            }
+
+            //deselect popFaq
+            if ($scope.selectedFaqIndex == index) {
+                $scope.faqs[index].selected = false;
+                $scope.selectedFaqIndex = null;
+            }
+            //select different popFaq
+            else {
+                angular.forEach($scope.faqs, function (faq) {
+                    faq.selected = false;
+                });
+                $scope.faqs[index].selected = true;
+                $scope.selectedFaqIndex = index;
+            }
+        }
     }
+
+    $scope.selectRelated = function(parentIndex, index) {
+        if ($scope.faqs[index].popular) {
+            $scope.selectFaq(index);
+        }
+        else {
+            window.open($scope.faqs[index].link);
+        }
+    };
+
+    $scope.related = function (parentIndex, index) {
+        return _.contains($scope.faqs[parentIndex].related, $scope.faqs[index].name);
+    };
+    
 }]);
