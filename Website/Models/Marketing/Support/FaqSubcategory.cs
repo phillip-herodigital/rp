b0e8152cd8ee5213f2478f9910f546078b25d36d
Chris Millican <chris.millican@responsivepath.com>
2016-05-05 16:24:11 -0700

    [update] Adding categories to subcategories

--- Website/Models/Marketing/Support/FaqSubcategory.cs
+++ Website/Models/Marketing/Support/FaqSubcategory.cs
@@ -7,7 +7,7 @@ namespace StreamEnergy.MyStream.Models.Marketing.Support
 {
     public class FaqSubcategory
     {
-        public List<FAQCategory> Categories;
+        public List<FAQCategory> Categories = new List<FAQCategory>();
         public string Guid;
         public string DisplayTitle;
 
@@ -25,6 +25,16 @@ namespace StreamEnergy.MyStream.Models.Marketing.Support
             this.Guid = SitecoreItem.ID.ToString();
 
             DisplayTitle = SitecoreItem.Fields["Display Title"].Value;
+
+            if (SitecoreItem.Fields["Categories"] != null && 
+                !string.IsNullOrEmpty(SitecoreItem.Fields["Categories"].Value))
+            {
+                var cats = SitecoreItem.Fields["Categories"].Value.Split("|".ToCharArray());
+                foreach (string c in cats)
+                {
+                    Categories.Add(new FAQCategory(Sitecore.Context.Database.GetItem(c)));
+                }
+            }
         }
     }
 }
