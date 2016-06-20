a933fa66cd3587213af24e3b942c1e8563df7fae
phillip.jacobs@herodigital.com <phillip.jacobs@herodigital.com>
2016-06-20 08:39:36 -0700

    add state for duplicate FAQ import

--- Website/Controllers/ApiControllers/MarketingController.cs
+++ Website/Controllers/ApiControllers/MarketingController.cs
@@ -319,15 +319,23 @@ namespace StreamEnergy.MyStream.Controllers.ApiControllers
             TemplateItem FAQTemplate = Sitecore.Context.Database.GetTemplate("User Defined/Components/Support/State FAQ");
             Item EnergySubcategoryFolder = Sitecore.Context.Database.GetItem("/sitecore/content/Data/Components/Support/Subcategories/Energy");
             String EnergyCategoryGuid = "{D009C153-3D1F-4C58-A984-7C4AC60612B3}";
+            string GeorgiaGuid = "{CF5BAF5B-35B4-4AE0-8F3A-F9F10F332A55}";
+            string MarylandGuid = "{62775FBA-8066-4D70-B575-8BA70A9E26D0}";
+            string NewJerseyGuid = "{AF7168F9-D030-4598-B5D2-7F5459DE3808}";
+            string NewYorkGuid = "{FD9206D6-844E-4476-AC18-FF833A36F99A}";
+            string PennsylvaniaGuid = "{7224504C-1989-4410-B8E4-A3D536958E35}";
+            string TexasGuid = "{C524C6E6-7BEE-402E-BC3F-ACC22E90A8CC}";
+            string DCGuid = "{55CAD77F-9736-4D3B-94E6-502252A81D9A}";
+
             List<Item> EnergySubcategoryItems = new List<Item>();
             Item FAQItem;
 
             using (new Sitecore.SecurityModel.SecurityDisabler())
             {
-                foreach (Item child in EnergyFAQFolder.Children)
-                {
-                    child.Delete();
-                }
+                //foreach (Item child in EnergyFAQFolder.Children)
+                //{
+                //    child.Delete();
+                //}
 
                 foreach (Item child in EnergySubcategoryFolder.Children)
                 {
@@ -354,6 +362,10 @@ namespace StreamEnergy.MyStream.Controllers.ApiControllers
                     string faqQuestion = fields[2];
                     string faqAnswer = fields[3];
                     string faqQuestionItemName = rgx.Replace(faqQuestion, "");
+                    if (faqQuestionItemName.Length > 99)
+                    {
+                        faqQuestionItemName = faqQuestionItemName.Substring(0, 99);
+                    }
 
                     FAQItem = Sitecore.Context.Database.GetItem("/sitecore/content/Data/Components/Support/FAQs/Energy FAQs/" + faqQuestionItemName);
                     if (FAQItem == null)
@@ -364,8 +376,14 @@ namespace StreamEnergy.MyStream.Controllers.ApiControllers
                         FAQItem.Fields["FAQ Answer"].Value = faqAnswer;
                         FAQItem.Fields["FAQ Subcategories"].Value = faqSubcategory;
                         FAQItem.Fields["FAQ Categories"].Value = EnergyCategoryGuid;
-                        FAQItem.Fields["FAQ States"].Value = "{C524C6E6-7BEE-402E-BC3F-ACC22E90A8CC}"; //texas
+                        FAQItem.Fields["FAQ States"].Value = NewYorkGuid;
                         FAQItem.Appearance.Sortorder = sortOrder;
+                        FAQItem.Editing.EndEdit();
+                    }
+                    else
+                    {
+                        FAQItem.Editing.BeginEdit();
+                        FAQItem.Fields["FAQ States"].Value = FAQItem.Fields["FAQ States"].ToString() + "|" + NewYorkGuid;
                         FAQItem.Editing.EndEdit();
                     }
                 }
