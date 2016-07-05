a97e669024dfede110892b479953dd5790a180b0
Chris Millican <chris.millican@responsivepath.com>
2016-07-04 20:50:35 -0700

    [update] Payment Location API

---
+++ Website/Models/Marketing/PaymentLocation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StreamEnergy.DomainModels;
+
+namespace StreamEnergy.MyStream.Models.Marketing
+{
+    public class PaymentLocation
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public string Hours { get; set; }
+        public string Name { get; set; }
+        public double ID { get; set; }
+        public string Vender { get; set; }
+        public string Agent { get; set; }
+        //public Address Address { get; set; }
+        public string City { get; set; }
+        public string StateAbbreviation { get; set; }
+        public string PostalCode5 { get; set; }
+        public string PostalCodePlus4 { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+
+        public string ContactName { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool Fee { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public String Status { get; set; }
+        public List<string> PaymentMethods { get; set; }
+        public double Rank { get; set; }
+
+        public double Distance { get; set; }
+    }
+}
