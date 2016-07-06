05dbc0d8a7b42ea964d74aabe84c46e0d365cb43
phillip.jacobs@herodigital.com <phillip.jacobs@herodigital.com>
2016-07-06 09:17:14 -0700

    api frontend integration

--- Website/Controllers/ApiControllers/MarketingController.cs
+++ Website/Controllers/ApiControllers/MarketingController.cs
@@ -25,6 +25,10 @@ using Sitecore.Data.Items;
 using System.Text.RegularExpressions;
 using Microsoft.VisualBasic.FileIO;
 using Newtonsoft.Json;
+using StackExchange.Redis;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Device.Location;
 
 namespace StreamEnergy.MyStream.Controllers.ApiControllers
 {
@@ -310,5 +314,157 @@ namespace StreamEnergy.MyStream.Controllers.ApiControllers
                 parser.Close();
             }
         }
+
+
+
+        [HttpGet]
+        [Route("paymentlocations/{lat}/{lng}/{maxLat}/{maxLon}/{minLat}/{minLon}/{maxResults}/{useCache}")]
+        public List<PaymentLocation> Get(string lat, string lng, string maxLat,
+            string maxLon, string minLat, string minLon, int maxResults, string useCache)
+        {
+            ConnectionMultiplexer Connection;
+            IDatabase cache;
+
+            List<PaymentLocation> results = new List<PaymentLocation>();
+            string key = string.Format("{0}|{1}|{2}", lat + lng, maxLat + maxLon + minLat + minLon,
+                maxResults.ToString());
+
+            Connection = ConnectionMultiplexer.Connect(ConfigurationManager.ConnectionStrings["redisCache"].ConnectionString);
+            cache = Connection.GetDatabase();
+
+            //Add request logic here
+
+            bool cacheExists = false;
+
+            if (bool.Parse(useCache))
+            {
+                results = getCachedResults(key, cache, out cacheExists);
+
+                if (cacheExists)
+                {
+                    return results;
+                }
+            }
+
+            GeoCoordinate centerLoc = new GeoCoordinate(double.Parse(lat), double.Parse(lng));
+
+            string connectionString = Sitecore.Configuration.Settings.GetConnectionString("core");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    /*command.CommandText = @"
+Select * from [paymentLocations] 
+where 
+lat=@lat and lon =@lon
+";*/
+
+                    command.CommandText = @"
+Select * from [paymentLocations] 
+        Where Lat Between @minLat And @maxLat
+          And Lon Between @minLon And @maxLon
+";
+                    /*command.Parameters.AddWithValue("@lat", lat);
+                    command.Parameters.AddWithValue("@lon", lng);*/
+
+                    command.Parameters.AddWithValue("@minLat", minLat);
+                    command.Parameters.AddWithValue("@minLon", minLon);
+                    command.Parameters.AddWithValue("@maxLat", maxLat);
+                    command.Parameters.AddWithValue("@maxLon", maxLon);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        var userContext = new DomainModels.Enrollments.UserContext();
+
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                string ed = (string)reader["stop_date"];
+
+                                PaymentLocation location = new PaymentLocation()
+                                {
+                                    ID = (double)reader["id"],
+                                    Vender = (string)reader["vendor"],
+                                    Agent = (string)reader["agent"],
+                                    Name = (string)reader["location_name"],
+                                    AddressLine1 = (string)reader["address1"],
+                                    AddressLine2 = (string)reader["address2"],
+                                    City = (string)reader["city"],
+                                    StateAbbreviation = (string)reader["state"],
+                                    PostalCode5 = (string)reader["zip"],
+                                    PostalCodePlus4 = (string)reader["zip4"],
+                                    PhoneNumber = String.Format("{0:(###) ###-####}", ((string)reader["phone"]).Replace("-", "").Replace("(", "").Replace(")", "").Trim()),
+                                    ContactName = (string)reader["contact_name"],
+                                    Hours = (string)reader["hours"],
+                                    Fee = ((double)reader["fee"]) > 0,
+                                    StartDate = DateTime.Parse((string)reader["start_date"]),
+                                    EndDate = string.IsNullOrEmpty(ed) || ed == "0000-00-00" ? DateTime.MinValue : DateTime.Parse(ed),
+                                    Status = (string)reader["status"],
+                                    PaymentMethods = ((string)reader["pay_methods"]).Split(" ".ToCharArray()).ToList(),
+                                    Lat = (double)reader["lat"],
+                                    Lon = (double)reader["lon"],
+                                    Rank = (double)reader["rank"],
+                                    Distance = centerLoc.GetDistanceTo(new GeoCoordinate((double)reader["lat"], (double)reader["lon"]))
+                                };
+
+                                results.Add(location);
+                            }
+                        }
+                    }
+                }
+            }
+
+            //Sort by distance
+            results.Sort((a, b) => a.Distance.CompareTo(b.Distance));
+
+            if (results.Count() > maxResults)
+            {
+                results = results.Take(maxResults).ToList();
+            }
+
+            if (results != null && results.Count() > 0)
+            {
+                setLocationCache(results, cache, key);
+            }
+
+            return results;
+        }
+
+        private List<PaymentLocation> getCachedResults(string key, IDatabase cache, out bool cacheExisted)
+        {
+            List<PaymentLocation> results = new List<PaymentLocation>();
+
+            string result = cache.StringGet(key);
+            cacheExisted = !string.IsNullOrEmpty(result);
+
+            if (string.IsNullOrEmpty(result))
+            {
+                return new List<PaymentLocation>();
+            }
+
+            results = JsonConvert.DeserializeObject<List<PaymentLocation>>(result);
+
+            return results;
+        }
+
+        private bool setLocationCache(List<PaymentLocation> locations, IDatabase cache, string key)
+        {
+            try
+            {
+                cache.StringSet(key, JsonConvert.SerializeObject(locations), TimeSpan.FromDays(7));
+            }
+            catch (Exception)
+            {
+                //Add logging here
+                return false;
+            }
+            return true;
+        }
+
+
+
+
     }
 }
--- Website/frontend/assets/js/controllers/paycenterCtrl.js
+++ Website/frontend/assets/js/controllers/paycenterCtrl.js
@@ -25,7 +25,7 @@ ngApp.controller('PaycenterCtrl', ['$scope', '$http', '$window', '$location', 'o
                     $scope.isLoading = false;
                 });
             },
-            drag: function (map) {
+            dragend: function (map) {
                 $scope.getMarkers({
                     location: map.center,
                     viewport: map.getBounds()
@@ -63,17 +63,18 @@ ngApp.controller('PaycenterCtrl', ['$scope', '$http', '$window', '$location', 'o
                     $scope.search = getPlaces.name;
                     $scope.searchType = "zipcode";
                 }
-                $scope.getMarkers(getPlaces.geometry);
-                //calculateDistances();
-                $scope.showMap = false;
-                updateMarkers = false;
-                $scope.mapInstance.setCenter(getPlaces.geometry.location);
-                $scope.bounds.extend(getPlaces.geometry.location);
-                $scope.bounds.extend(new google.maps.LatLng(
-                    $scope.markers[0].coords.latitude,
-                    $scope.markers[0].coords.longitude));
-                $scope.mapInstance.fitBounds($scope.bounds);
-                updateMarkers = true;
+                var promise = $scope.getMarkers(getPlaces.geometry);
+                promise.then(function (response) {
+                    $scope.showMap = false;
+                    updateMarkers = false;
+                    $scope.mapInstance.setCenter(getPlaces.geometry.location);
+                    $scope.bounds.extend(getPlaces.geometry.location);
+                    $scope.bounds.extend(new google.maps.LatLng(
+                        $scope.markers[0].coords.latitude,
+                        $scope.markers[0].coords.longitude));
+                    $scope.mapInstance.fitBounds($scope.bounds);
+                    updateMarkers = true;
+                });
             }
         }
     };
@@ -110,122 +111,82 @@ ngApp.controller('PaycenterCtrl', ['$scope', '$http', '$window', '$location', 'o
     $scope.markers = [];
 
     $scope.getMarkers = function (data) {
-
-        if ($scope.search && updateMarkers) {
-
-            var lat = data.location.lat();
-            var lng = data.location.lng();
-            var topLat = data.viewport.b.b;
-            var bottomLat = data.viewport.b.f;
-            var leftLng = data.viewport.f.b;
-            var rightLng = data.viewport.f.f;
-
-            //$scope.isLoading = true;
-            //$http({
-            //    method: 'get',
-            //    url: '/api/paymentlocations/' + lat + '/' + lng + '/' + topLat + '/' + bottomLat + '/' + leftLng + '/' + rightLng
-            //}).then(function successs(response) {
-            //    $scope.markers = response.data;
-            //    $scope.isLoading = false;
-            //}
-            //, function error(response) {
-            //});
-
-
-            $scope.markers = [
-              {
-                  id: 1,
-                  icon: pinImage,
-                  coords: { latitude: 32.64, longitude: -97.03 },
-                  locationInfo: {
-                      name: "Fiesta Store #57a",
-                      address: {
-                          line1: "2951 S. Buckner Blvd.",
-                          line2: "Dallas, TX 75227",
-                          phone: "(214) 275-3020"
-                      },
-                      hours: "9-5",
-                      paymentMethods: ["card", "cash"],
-                  },
-                  onClicked: function () {
-                      $scope.openWindow(0);
-                  },
-                  distance: 0,
-                  selected: false
-              },
-              {
-                  id: 2,
-                  icon: pinImage,
-                  coords: { latitude: 32.84, longitude: -97.13 },
-                  locationInfo: {
-                      name: "Fiesta Store #58",
-                      address: {
-                          line1: "2951 S. Buckner Ave.",
-                          line2: "Dallas, TX 12345",
-                          phone: "(214) 275-2030"
-                      },
-                      hours: "9-5",
-                      paymentMethods: ["card", "cash"],
-                  },
-                  onClicked: function () {
-                      $scope.openWindow(1);
-                  },
-                  distance: 0,
-                  selected: false
-              },
-              {
-                  id: 3,
-                  icon: pinImage,
-                  coords: { latitude: 32.94, longitude: -97.50 },
-                  locationInfo: {
-                      name: "Azle Store",
-                      address: {
-                          line1: "713 W Main St",
-                          line2: "Azle, TX 76020",
-                          phone: "(214) 275-2030"
-                      },
-                      hours: "9-5",
-                      paymentMethods: ["card", "cash"],
-                  },
-                  onClicked: function () {
-                      $scope.openWindow(2);
-                  },
-                  distance: 0,
-                  selected: false
-              }
-            ];
-        }
-    }
-
-    var calculateDistances = function () {
-        angular.forEach($scope.markers, function (marker, index) {
-            var toRadians = function (degrees) {
-                return (degrees / 360) * 2 * Math.PI;
-            };
-            var lat1 = marker.coords.latitude;
-            var lat2 = $scope.searchMarker.coords.latitude;
-            var lon1 = marker.coords.longitude;
-            var lon2 = $scope.searchMarker.coords.longitude;
-
-            var R = 3959; // miles
-            var φ1 = toRadians(lat1);
-            var φ2 = toRadians(lat2);
-            var Δφ = toRadians(lat2 - lat1);
-            var Δλ = toRadians(lon2 - lon1);
-
-            var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-                    Math.cos(φ1) * Math.cos(φ2) *
-                    Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-            $scope.markers[index].distance = (R * c).toFixed(1) * 1;
+        var promise = new Promise(function (resolve, reject) {
+            if ($scope.search && updateMarkers) {
+                var lat = data.location.lat();
+                var lng = data.location.lng();
+                var topLat = data.viewport.f.b;
+                var bottomLat = data.viewport.f.f;
+                var leftLng = data.viewport.b.f;
+                var rightLng = data.viewport.b.b;
+                $scope.isLoading = true;
+                $http({
+                    method: 'GET',
+                    url: '/api/paymentlocations/' + lat + '/' + lng + '/' + topLat + '/' + leftLng + '/' + bottomLat + '/' + rightLng + '/100/true'
+                }).then(function successCallback (response) {
+                    $scope.isLoading = false;
+                    $scope.markers = [];
+                    angular.forEach(response.data, function (place, index) {
+                        $scope.markers.push({
+                            id: index + 1,
+                            icon: pinImage,
+                            coords: {
+                                latitude: place.lat,
+                                longitude: place.lon
+                            },
+                            locationInfo: {
+                                name: place.name,
+                                address: {
+                                    line1: place.addressLine1,
+                                    line2: place.addressLine2
+                                },
+                                hours: place.hours,
+                                paymentMethods: place.paymentMethods
+                            },
+                            onClicked: function() {
+                                $scope.openWindow(index)
+                            },
+                            distance: place.distance,
+                            selected: false
+                        })
+                    });
+                    resolve(response.statusText);
+                }, function errorCallback (response) {
+                    reject(response.statusText);
+                });
+            }
+            else {
+                resolve();
+            }
         });
-        $scope.markers = orderBy($scope.markers, 'distance');
-
+        return promise;
     }
 
-    //$scope.sortedMarkers = function() {
-    //    return orderBy($scope.markers, 'distance');
+    //var calculateDistances = function () {
+    //    angular.forEach($scope.markers, function (marker, index) {
+    //        var toRadians = function (degrees) {
+    //            return (degrees / 360) * 2 * Math.PI;
+    //        };
+    //        var lat1 = marker.coords.latitude;
+    //        var lat2 = $scope.searchMarker.coords.latitude;
+    //        var lon1 = marker.coords.longitude;
+    //        var lon2 = $scope.searchMarker.coords.longitude;
+
+    //        var R = 3959; // miles
+    //        var φ1 = toRadians(lat1);
+    //        var φ2 = toRadians(lat2);
+    //        var Δφ = toRadians(lat2 - lat1);
+    //        var Δλ = toRadians(lon2 - lon1);
+
+    //        var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    //                Math.cos(φ1) * Math.cos(φ2) *
+    //                Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    //        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    //        $scope.markers[index].distance = (R * c).toFixed(1) * 1;
+    //    });
+    //    $scope.markers = orderBy($scope.markers, 'distance');
+
     //}
 
     $scope.openWindow = function (i) {
@@ -267,8 +228,6 @@ ngApp.controller('PaycenterCtrl', ['$scope', '$http', '$window', '$location', 'o
             windowMarkerIndex = -1;
         }
     }
-
-    calculateDistances();
 
     //mobile view
     $scope.isMobile = function () {
