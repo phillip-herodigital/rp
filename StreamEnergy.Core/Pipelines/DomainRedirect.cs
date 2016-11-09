1ec6c484b5356b3c44a31571f92889b230045423
phillip.jacobs@herodigital.com <phillip.jacobs@herodigital.com>
2016-11-09 10:24:02 -0800

    implement domain redirect setting

---
+++ StreamEnergy.Core/Pipelines/DomainRedirect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web;
+using Microsoft.Practices.Unity;
+using Sitecore.Data.Fields;
+
+namespace StreamEnergy.Pipelines
+{
+    public class DomainRedirect : Sitecore.Pipelines.HttpRequest.HttpRequestProcessor
+    {
+        private ISettings settings;
+        public DomainRedirect()
+        {
+            this.settings = StreamEnergy.Unity.Container.Instance.Unity.Resolve<ISettings>();
+        }
+
+        public override void Process(Sitecore.Pipelines.HttpRequest.HttpRequestArgs args)
+        {
+            try
+            {
+                Sitecore.Data.Items.Item settingsItem = settings.GetSettingsItem("Domain Redirect");
+                if (!string.IsNullOrEmpty(settingsItem.Fields["Redirect Enabled"].Value))
+                {
+                    string domain = HttpContext.Current.Request.Url.Host.Replace(".", "_");
+                    var redirects = ((NameValueListField)settingsItem.Fields["Redirects"]).NameValues;
+                    if (!string.IsNullOrEmpty(redirects[domain]))
+                    {
+                        HttpContext.Current.Response.RedirectPermanent(HttpUtility.UrlDecode(redirects[domain]));
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Eat errors
+            }
+        }
+    }
+}
